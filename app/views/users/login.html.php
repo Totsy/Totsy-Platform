97079effa83c9ccee002576db356e804319162e6
fitzagard <fhagard@lightcube.us>
2010-05-18 12:40:57 -0400

    Added Improved Registration/Login functionality, Refs #100, 101 Added to tmp cache to gitignore

---
+++ app/views/users/login.html.php
@@ -0,0 +1,10 @@
+<?php
+	if ($message){
+		echo $message;
+	}
+?>
+<?=$this->form->create();?>
+<?=$this->form->field('email', array('class'=>'required'))?>
+<?=$this->form->field('password', array('type'=>'password','class'=>'required'));?>
+<?=$this->form->submit('Login');?>
+<?=$this->form->end();?>
