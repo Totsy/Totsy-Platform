9db2d417e89c45ea070ee285add1fa8a6efde1d2
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-06-04 15:42:20 -0400

    Finishing up autoimport script for task #108

--- data/updatemongo.sh
+++ data/updatemongo.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 # Helper script to reload MongoDB database and reindex fields
+#
 
-echo "ok, getting this party started."
+# set some environment shizzle
+DBNAME="totsy"
+FILES="*.json"
+EXTENSION="json"
+INDEXES="indexes.js"
+
+# fetch a list of data files for import
+
+for i in $FILES
+do
+	COLL="${i%%.$EXTENSION}"
+	#echo "We got a datafile called $i for collection $COLL"
+	mongoimport -d $DBNAME -c $COLL --drop $i
+done
+
+# shell example:
+#    "mongoimport -d totsy -c lists --drop lists.json"
+
+# now apply the indexes with the following syntax:
+#   "mongo totsy indexes.js"
+
+mongo totsy indexes.js
+
+
+# done
+echo "Done."
