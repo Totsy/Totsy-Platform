393c27b237a79baa1f062c096ff7c450bc0398ed
Jonathan Lackey <jon@zunostudios.com>
2010-06-10 18:29:57 -0700

    created new button styles and finished login form

--- app/views/users/login.html.php
+++ app/views/users/login.html.php
@@ -39,27 +39,48 @@
 				
 				</div>
 				
-				<div id="sign-in-box">
-					
-					<h2>Member Sign In</h2>
-					
-					<?=$this->form->create(null,array('id'=>'loginForm'));?>
-					<?=$this->form->field('email', array('class'=>"validate['required','email']"));?>
-					<?=$this->form->field('password', array(
-							'class'=>"validate['required']", 
-							'name' => 'password', 
-							'id' => 'password',
-							'type' => 'password'));?>
-					<?=$this->form->submit('Login');?>
-					<?=$this->form->end();?>
-					<?=$this->html->link('Request Membership','/register')?>
-				
+				<div class="sign-in-container">
+					<div id="sign-in-box" class="r-container">
+						<div class="tl"></div>
+						<div class="tr"></div>
+						<div class="r-box lt-gradient-1">
+							<h2>Member Sign In</h2>
+							
+							<?=$this->form->create(null,array('id'=>'loginForm'));?>
+							<?=$this->form->field('email', array('class'=>"validate['required','email']  inputbox", 'id'=>"email"));?>
+							<?=$this->form->field('password', array(
+									'class'=>"validate['required'] inputbox", 
+									'name' => 'password', 
+									'id' => 'password',
+									'type' => 'password'));?>
+							<?=$this->form->field('remember', array('id'=>"remember", 'type'=>"checkbox", 'class'=>"fl checkbox"));?>
+							
+							<?=$this->html->link('Forgot your password?','/remind', array('class'=>"md", 'title'=>"Forgot your password?"))?>
+							<br />
+							<?=$this->form->submit('Login', array('class'=>"flex-btn-2"));?>
+							<?=$this->form->end();?>
+							
+						</div>
+						<div class="bl"></div>
+						<div class="br"></div>
+					</div>
 				</div>
 				
-				<div id="register-box">
-				
-					<h2>Become a Member</h2>
-				
+				<div class="register-container">
+					<div id="register-box" class="r-container">
+						<div class="tl"></div>
+						<div class="tr"></div>
+						<div class="r-box lt-gradient-1">
+							<h2>Become a Member</h2>
+							
+							<p>Become a member today for access to brand-specific sales, up to 70% off retail, just for you and the kids, ages 0-7. Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, and educational materials &mdash; and that's just the start.</p>
+							
+							<?=$this->html->link('Request Membership','/register', array('class'=>"flex-btn-2")); ?>
+						</div>
+						<div class="bl"></div>
+						<div class="br"></div>
+					
+					</div>
 				</div>
 			
 				<p class="login-sig clear">Exclusive access, Top brands. Great deals. <span class="red">The savvy mom shops at Totsy.com</span></p>
@@ -69,6 +90,8 @@
 		</div>
 		
 	</div>
+	
+	
 	
 	<div id="bg">
 		<img id="bg-img" class="activeslide" src="img/home-bg-1.jpg" alt="" />
