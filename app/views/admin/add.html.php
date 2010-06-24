d3910114c5475d32fe7fc32193b1610c7e8dae43
fitzagard <fhagard@lightcube.us>
2010-06-24 17:18:48 -0400

    Removing all admin specific functionality from app.

--- app/views/admin/add.html.php
+++
@@ -1,5 +0,0 @@
-<?=$this->form->create(); ?>
-    <?=$this->form->field('title');?>
-    <?=$this->form->field('body', array('type' => 'textarea'));?>
-    <?=$this->form->submit('Add Page'); ?>
-<?=$this->form->end(); ?>
--- app/views/admin/index.html.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($pages as $page): ?>
-<article>
-    <h1><?=$page->title ?></h1>
-    <p><?=$page->body ?></p>
-</article>
-<?php endforeach; ?>
