5e61438ff4247becf9e912b7a8a544c9b9d5e5d9
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-06-24 16:39:12 +0000

    migrating index definitions from navigations to menus

--- data/indexes.js
+++ data/indexes.js
@@ -1,3 +1,3 @@
-db.navigations.ensureIndex({title: 1}, {unique: true});
+db.menus.ensureIndex({title: 1}, {unique: true});
 db.lists.ensureIndex({name: 1}, {unique: true});
 db.users.ensureIndex({email: 1}, {unique: true, dropDups : true});
