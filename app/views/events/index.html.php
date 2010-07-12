eda034f7ed49da7be8d5b5b22f1036d629cca1f0
fitzagard <fhagard@lightcube.us>
2010-07-12 12:41:34 -0400

    More splash cleanup. Added seconds to splash.

--- app/views/events/index.html.php
+++ app/views/events/index.html.php
@@ -14,8 +14,11 @@
 		<div class="tl"></div>
 		<div class="tr"></div>
 		<div class="md-gray p-container">
-			<img src="/image/<?php echo $event->images->preview_image?>.jpg" width="355" height="410" title="Product Title" alt="Product Alt Text" />
-
+			<?php if ($x <= 1): ?>
+				<img src="/image/<?php echo $event->images->preview_image?>.jpg" width="355" height="410" title="Product Title" alt="Product Alt Text" />
+			<?php else: ?>
+				<img src="/image/<?php echo $event->images->preview_image?>.jpg" width="298" height="298" title="Product Title" alt="Product Alt Text" />
+			<?php endif ?>
 			<div class="splash-details">
 				<div class="table-cell left">
 					Events End In<br />
@@ -35,32 +38,35 @@
 		$date = $event->end_date->sec * 1000;
 		$splashid = "#todaysplash$x";
 		$todayJs[] = "<script type=\"text/javascript\">$(function () {var saleEnd = new Date();saleEnd = new Date($date);$(\"$splashid\").countdown({until: saleEnd, compact: true, description: ''});});</script>";?>
+		<?php if ($x == 1): ?>
+			<div class="invite-column r-container">
+
+				<div class="tl"></div>
+				<div class="tr"></div>
+				<div class="md-gray-gradient p-container">
+
+					<h2 class="invite-friends">Invite Friends Get $15</h2>
+
+					<a href="#" class="flex-btn" title="Invite Friends Now">Invite Now</a>
+
+					<div class="hor-div-line"><!-- --></div>
+
+					<h2 class="socialize-us">Socialize With Us</h2>
+
+					<div class="sm-icons">
+						<a href="http://facebook.com" title="Friend us on Facebook" class="sm-btn sm-facebook-md">Friend us on Facebook</a>
+						<a href="http://twitter.com" title="Follow us on Twitter" class="sm-btn sm-twitter-md">Follow us on Twitter</a>
+					</div>
+
+				</div>
+				<div class="bl"></div>
+				<div class="br"></div>
+
+			</div>
+		<?php endif ?>
 	<?php $x++; ?>
 <?php endforeach ?>
-<div class="invite-column r-container">
 
-	<div class="tl"></div>
-	<div class="tr"></div>
-	<div class="md-gray-gradient p-container">
-	
-		<h2 class="invite-friends">Invite Friends Get $15</h2>
-		
-		<a href="#" class="flex-btn" title="Invite Friends Now">Invite Now</a>
-		
-		<div class="hor-div-line"><!-- --></div>
-		
-		<h2 class="socialize-us">Socialize With Us</h2>
-		
-		<div class="sm-icons">
-			<a href="http://facebook.com" title="Friend us on Facebook" class="sm-btn sm-facebook-md">Friend us on Facebook</a>
-			<a href="http://twitter.com" title="Follow us on Twitter" class="sm-btn sm-twitter-md">Follow us on Twitter</a>
-		</div>
-			
-	</div>
-	<div class="bl"></div>
-	<div class="br"></div>
-
-</div>
 <?php $x = 0; ?>
 <?php foreach ($currentEvents as $event): ?>
 	
