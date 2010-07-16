2e7203a81098d5b3a71bdb019e9ac6aec342963e
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-07-16 19:54:44 -0400

    adding shipping and returns content to pages views

---
+++ app/views/pages/shipping.html.php
@@ -0,0 +1,38 @@
+<h1 class="p-header">Shipping &amp; Tax</h1> 
+
+<div id="middle" class="fullwidth">				
+			
+<div class="tl"><!-- --></div> 
+<div class="tr"><!-- --></div> 
+
+<div id="page">
+	
+	<h2>Shipping Time</h2>
+
+	<p>For delivery, orders will be processed, shipped, and delivered within 3-4 weeks. When your order is shipped, you will be able to access a tracking number on your personal account page.</p>
+
+	<h2>Sales Tax</h2>
+
+	<p>Sales tax will be charged for orders in Delaware, Pennsylvania, and New York. You will be able to view the sales tax for your order, if applicable, before confirming payment.</p>
+
+	<h2>Shipping Rates</h2>
+
+	<p>Shipping rates vary depending on the shipping preferences chosen (standard or express) and the location where the order will be shipped.</p>
+
+	<h2>Shipping Carrier</h2>
+
+	<p>Standard delivery orders are shipped via USPS Priority Mail. Express delivery orders are shipped via UPS.</p>
+	
+	
+	
+	
+
+</div> 
+<div class="bl"><!-- --></div> 
+<div class="br"><!-- --></div> 
+
+		</div> 
+
+	</div> 
+
+</div>
