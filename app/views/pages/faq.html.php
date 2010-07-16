e38425b25cf4ec85e4dda79d450093807cb0453b
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-07-16 17:00:14 -0400

    Adding FAQ content copy

--- app/views/pages/faq.html.php
+++ app/views/pages/faq.html.php
@@ -1,4 +1,4 @@
-<h1 class="p-header">FAQ</h1> 
+<h1 class="p-header">Frequently Asked Questions</h1> 
 				
 				
 		<div id="middle" class="fullwidth">				
@@ -13,16 +13,70 @@
 <div class="tr"><!-- --></div> 
 <div id="page"> 
  
- 
- 
- 
- 
-<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi.</p> 
-<p>Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. Maecenas vitae nulla consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.</p> 
-<p>Quisque facilisis erat a dui. Nam malesuada ornare dolor. Cras gravida, diam sit amet rhoncus ornare, erat elit consectetuer erat, id egestas pede nibh eget odio. Proin tincidunt, velit vel porta elementum, magna diam molestie sapien, non aliquet massa pede eu diam. Aliquam iaculis.</p> 
-<p>Fusce et ipsum et nulla tristique facilisis. Donec eget sem sit amet ligula viverra gravida. Etiam vehicula urna vel turpis. Suspendisse sagittis ante a urna. Morbi a est quis orci consequat rutrum. Nullam egestas feugiat felis. Integer adipiscing semper ligula. Nunc molestie, nisl sit amet cursus convallis.</p> 
-<p>Sapien lectus pretium metus, vitae pretium enim wisi id lectus. Donec vestibulum. Etiam vel nibh. Nulla facilisi. Mauris pharetra. Donec augue. Fusce ultrices, neque id dignissim ultrices, tellus mauris dictum elit, vel lacinia enim metus eu nunc.</p> 
-<p>Proin at eros non eros adipiscing mollis. Donec semper turpis sed diam. Sed consequat ligula nec tortor. Integer eget sem. Ut vitae enim eu est vehicula gravida. Morbi ipsum ipsum, porta nec, tempor id, auctor vitae, purus. Pellentesque neque. Nulla luctus erat vitae libero. Integer nec enim.</p> 
+	<p><strong>What is Totsy?</strong><br />
+	Totsy is a private shopping network of exclusive, brand-specific sales, up to 70% off retail, for moms on-the-go, moms-to-be, and kids, ages 0-7. Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, DVDs, and educational materials are just a sampling of a selection that promises only the best in quality and designer brands. Each sale lasts 48 to 72 hours.</p>
+
+	<p><strong>How do I become a member of Totsy?</strong><br />
+	Membership is free. You can join either by accepting an invitation offer from a friend or by requesting your membership via Totsy's sign-in page.</p>
+
+	<h2>Sales and Payment</h2>
+
+	<p><strong>When does a Totsy sale start?</strong><br />
+	Sales start at 10 AM (EST). Exceptions may be made for one-hour sales.</p>
+
+	<p><strong>How long does a Totsy sale stay open?</strong><br />
+	Unless otherwise specified, a sale lasts 48 to 72 hours.</p>
+
+	<p><strong>Is there a limit to how many items I can put in my shopping cart?</strong><br />
+	No, but stock is limited.</p>
+
+	<p><strong>Can I put items from two different events in my shopping cart?</strong><br />
+	Yes.</p>
+
+	<p><strong>How can I pay for my items?</strong><br />
+	Payment can be made either via credit card or PayPal.</p>
+
+	<p><strong>Can I cancel an order?</strong><br />
+	Generally no, especially on final sales, but you always have the option to call customer service to explain the situation.</p>
+
+	<p><strong>Does Totsy charge sales tax?</strong><br />
+	Yes, but only for orders delivered to Delaware, Pennsylvania, and New York.</p>
+
+	<h2>Shipping Information</h2>
+
+	<p><strong>What are my shipping options?</strong><br />
+	For standard delivery, we ship via USPS Priority Mail. For express delivery, we ship via UPS.</p>
+
+	<p><strong>How long will it take for me to get my order?</strong><br />
+	Depending on delivery options, the general length of time is 3 to 4 weeks.</p>
+
+	<p><strong>Can I track my order?</strong><br />
+	Yes, you can track your order online via your account page.</p>
+
+	<p><strong>Does Totsy ship to Canada?</strong><br />
+	Yes.</p>
+
+	<p><strong>Does Totsy ship internationally?</strong><br />
+	We do not yet have international delivery rates set up, but if there is a specific request, you can contact us either via email or phone.</p>
+
+	<h2>Returns</h2>
+
+	<p><strong>What is Totsy's policy on returns and exchanges?</strong><br />
+	Please see our Return Policy page for more information.</p>
+
+	<h2>Account Maintenance</h2>
+
+	<p><strong>What happens if I forget my password?</strong><br />
+	If you forget your password, you can reset it via the "Forgot Your Password?" link on Totsy's sign-in page. You'll be directed to a new page and asked to input your email address. We will then email you a link to reset your password and create a new one.</p>
+
+	<p><strong>How do I change my password, my shipping and/or billing address, email address, and/or credit card information?</strong><br />
+	All of these preferences can be edited on your personal account page.</p>
+
+	<p><strong>Can I have multiple shipping addresses on file?</strong><br />
+	You can only store one address, but you will have the option to change the shipping address during checkout.</p>
+
+	<p><strong>How do I unsubscribe to email notices?</strong><br />
+	To unsubscribe from our email notices, just click on the unsubscribe link found at the bottom of every email. You'll be taken directly to a page that will ask you to confirm that you want to be removed from our database. Once you confirm, we'll automatically remove you from our lists.</p>
  
  
 </div> 
