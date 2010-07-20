8183d15df0bb2850385d5a82c8f30974be13bb97
Nate Abele <nate.abele@gmail.com>
2010-07-20 09:41:19 -0400

    Fixing line wrapping, because this is just killing me.

--- app/views/pages/aboutus.html.php
+++ app/views/pages/aboutus.html.php
@@ -8,20 +8,36 @@
 <div id="page">
 	
 	<h2>Where the savvy mom shops.</h2>
-	<p>Totsy offers moms on-the-go and moms-to-be access to brand-specific sales, up to 70% off retail, just for them and the kids, ages 0-7.</p>
+	<p>
+		Totsy offers moms on-the-go and moms-to-be access to brand-specific sales, up to 70% off
+		retail, just for them and the kids, ages 0-7.
+	</p>
 	
 	<h2>Top brands for mom, baby, and child.</h2>
-	<p>Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, DVDs, and educational materials are just a sampling of a selection that promises only the best in quality and designer brands.</p>
+	<p>
+		Prenatal care products, baby gear, travel accessories, bedding and bath, children's
+		clothing, toys, DVDs, and educational materials are just a sampling of a selection that
+		promises only the best in quality and designer brands.
+	</p>
 	
 	<h2>By invitation only.</h2>
-	<p>Membership is by invitation or request only. But the sooner you join, the better. Each sale lasts only 48 to 72 hours.</p>
+	<p>
+		Membership is by invitation or request only. But the sooner you join, the better. Each sale
+		lasts only 48 to 72 hours.
+	</p>
 	
 	<h2>100% eco-friendly.</h2>
-	<p>Totsy is the first company in private sales to take on sustainable and socially responsible initiatives in all areas of business.</p>
+	<p>
+		Totsy is the first company in private sales to take on sustainable and socially responsible
+		initiatives in all areas of business.
+	</p>
 	
 	<h2>One baby, one tree.</h2>
-	<p>With every purchase you make through Totsy, we will plant one tree in the name of your child to help reduce the effects of deforestation. Who says shopping can't save the world?</p>
-	
+	<p>
+		With every purchase you make through Totsy, we will plant one tree in the name of your child
+		to help reduce the effects of deforestation. Who says shopping can't save the world?
+	</p>
+
 </div> 
 <div class="bl"><!-- --></div> 
 <div class="br"><!-- --></div> 
