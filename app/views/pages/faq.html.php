718ab5478c8cd16515be3c6b536bb436c7fa6f1a
Nate Abele <nate.abele@gmail.com>
2010-07-20 04:49:57 -0400

    Fixing up a straggler page.

--- app/views/pages/faq.html.php
+++ app/views/pages/faq.html.php
@@ -1,11 +1,6 @@
-<?php
-	use app\models\Menu;
-	$this->title("Frequently Asked Questions");
-	$options = array('div' => array('id' => 'left'), 'ul' => array('class' => 'menu'));
-	echo $this->MenuList->build($menu, $options);
-?>
+<?=$this->menu->render('about'); ?>
 
-<h1 class="p-header">Frequently Asked Questions</h1> 
+<h1 class="p-header"><?=$this->title('Frequently Asked Questions'); ?></h1>
 
 <div class="tl"><!-- --></div> 
 <div class="tr"><!-- --></div> 
@@ -13,10 +8,15 @@
 <div id="page"> 
  
 	<p><strong>What is Totsy?</strong><br />
-	Totsy is a private shopping network of exclusive, brand-specific sales, up to 70% off retail, for moms on-the-go, moms-to-be, and kids, ages 0-7. Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, DVDs, and educational materials are just a sampling of a selection that promises only the best in quality and designer brands. Each sale lasts 48 to 72 hours.</p>
+	Totsy is a private shopping network of exclusive, brand-specific sales, up to 70% off retail,
+	for moms on-the-go, moms-to-be, and kids, ages 0-7. Prenatal care products, baby gear, travel
+	accessories, bedding and bath, children's clothing, toys, DVDs, and educational materials are
+	just a sampling of a selection that promises only the best in quality and designer brands. Each
+	sale lasts 48 to 72 hours.</p>
 
 	<p><strong>How do I become a member of Totsy?</strong><br />
-	Membership is free. You can join either by accepting an invitation offer from a friend or by requesting your membership via Totsy's sign-in page.</p>
+	Membership is free. You can join either by accepting an invitation offer from a friend or by
+	requesting your membership via Totsy's sign-in page.</p>
 
 	<h2>Sales and Payment</h2>
 
@@ -36,7 +36,8 @@
 	Payment can be made either via credit card or PayPal.</p>
 
 	<p><strong>Can I cancel an order?</strong><br />
-	Generally no, especially on final sales, but you always have the option to call customer service to explain the situation.</p>
+	Generally no, especially on final sales, but you always have the option to call customer service
+	to explain the situation.</p>
 
 	<p><strong>Does Totsy charge sales tax?</strong><br />
 	Yes, but only for orders delivered to Delaware, Pennsylvania, and New York.</p>
@@ -44,7 +45,8 @@
 	<h2>Shipping Information</h2>
 
 	<p><strong>What are my shipping options?</strong><br />
-	For standard delivery, we ship via USPS Priority Mail. For express delivery, we ship via UPS.</p>
+	For standard delivery, we ship via USPS Priority Mail. For express delivery, we ship via UPS.
+	</p>
 
 	<p><strong>How long will it take for me to get my order?</strong><br />
 	Depending on delivery options, the general length of time is 3 to 4 weeks.</p>
@@ -56,7 +58,8 @@
 	Yes.</p>
 
 	<p><strong>Does Totsy ship internationally?</strong><br />
-	We do not yet have international delivery rates set up, but if there is a specific request, you can contact us either via email or phone.</p>
+	We do not yet have international delivery rates set up, but if there is a specific request, you
+	can contact us either via email or phone.</p>
 
 	<h2>Returns</h2>
 
@@ -66,16 +69,23 @@
 	<h2>Account Maintenance</h2>
 
 	<p><strong>What happens if I forget my password?</strong><br />
-	If you forget your password, you can reset it via the "Forgot Your Password?" link on Totsy's sign-in page. You'll be directed to a new page and asked to input your email address. We will then email you a link to reset your password and create a new one.</p>
+	If you forget your password, you can reset it via the "Forgot Your Password?" link on Totsy's
+	sign-in page. You'll be directed to a new page and asked to input your email address. We will
+	then email you a link to reset your password and create a new one.</p>
 
-	<p><strong>How do I change my password, my shipping and/or billing address, email address, and/or credit card information?</strong><br />
+	<p><strong>How do I change my password, my shipping and/or billing address, email address,
+	and/or credit card information?</strong><br />
 	All of these preferences can be edited on your personal account page.</p>
 
 	<p><strong>Can I have multiple shipping addresses on file?</strong><br />
-	You can only store one address, but you will have the option to change the shipping address during checkout.</p>
+	You can only store one address, but you will have the option to change the shipping address
+	during checkout.</p>
 
 	<p><strong>How do I unsubscribe to email notices?</strong><br />
-	To unsubscribe from our email notices, just click on the unsubscribe link found at the bottom of every email. You'll be taken directly to a page that will ask you to confirm that you want to be removed from our database. Once you confirm, we'll automatically remove you from our lists.</p>
+	To unsubscribe from our email notices, just click on the unsubscribe link found at the bottom
+	of every email. You'll be taken directly to a page that will ask you to confirm that you want
+	to be removed from our database. Once you confirm, we'll automatically remove you from our
+	lists.</p>
  
  
 </div> 
