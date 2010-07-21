bd521d6a78ab9453ba907404eb9a1925a78e9868
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-07-21 11:20:23 -0400

    attempting to rename to import_data.php

--- data/v1/export_customers.sql
+++ data/v1/export_customers.sql
@@ -1,23 +1,29 @@
-SELECT id AS "_id",
-first_name AS "firstname",
-last_name AS "lastname",
-login AS "username",
-"email",
+SELECT c.id AS "_id",
+c.first_name AS "firstname",
+c.last_name AS "lastname",
+0 AS "affiliate",
+c.login AS "username",
+c.email AS "email",
 1 AS "legacy",
-crypted_password AS "password",
-password_salt AS "salt",
-created_at AS "created",
-updated_at AS "updated",
-last_login_at AS "lastlogin",
-last_login_ip AS "lastip",
-login_count AS "logincounter",
-1 AS "active"
+c.crypted_password AS "password",
+c.password_salt AS "salt",
+c.created_at AS "created",
+c.updated_at AS "updated",
+c.last_login_at AS "lastlogin",
+c.last_login_ip AS "lastip",
+c.login_count AS "logincounter",
+1 AS "active",
+it.code AS "invitation_code"
 
-
-FROM customers
+FROM customers c,
+invitation_tokens it
 
 WHERE first_name != ''
+AND it.customer_id = c.id
+-- skip crap data
+AND c.id > 3000
 
-ORDER BY id
---LIMIT 10
+ORDER BY c.id
+-- limit output
+LIMIT 20
 ;
