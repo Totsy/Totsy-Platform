ff01193612d4f53be3fa1f135a1a7a74a8b10567
fitzagard <fhagard@lightcube.us>
2010-07-22 13:39:25 -0400

    Make sure to pull cart based on session and user.

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -33,10 +33,12 @@ class Cart extends \lithium\data\Model {
 	
 	public static function active($params = null, array $options = array()) {
 		$fields = $params['fields'];
+		$user = Session::read('userLogin');
 		return static::all(array(
 			'conditions' => array(
 				'session' => Session::key(),
-				'expires' => array('$gt' => static::dates('now'))),
+				'expires' => array('$gt' => static::dates('now')),
+				'user_id' => $user['_id']),
 			'fields' => $fields,
 			'order' => array('expires' => 'ASC') 
 		));
