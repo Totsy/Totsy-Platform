826df7215f65dd5f0fb5de9afc6ebcbebdbca0c2
fitzagard <fhagard@lightcube.us>
2010-07-23 12:38:39 -0400

    Modifying mailer to stop RFC error - I hope.

--- app/extensions/Mailer.php
+++ app/extensions/Mailer.php
@@ -36,24 +36,16 @@ class Mailer {
 			->setPassword($config['mail']['password'])
 			->setPort($config['mail']['port'])
 			;
-			
-		
+
 		// make a message
 		$message = Swift_Message::newInstance($transport);
 		$message->setFrom(array('noreply@totsy.com' => 'Totsy'));
-		$message->setSubject('testing');
-		$message->setTo(array("{$to['name']} <{$to['email']}>"));
+		$message->setSubject($subject);
+		$message->setTo(array("<{$to['email']}>" => "{$to['name']}"));
 		$message->setBody(static::_view()->render('all', $data, compact('template')));
 		
 		//Create the Mailer using your created Transport
 		$mailer = Swift_Mailer::newInstance($transport);
-
-		//Create a message
-		// $message = Swift_Message::newInstance('Wonderful Subject')
-		//  ->setFrom(array('noreply@totsy.com'))
-		//  ->setTo(array('mitch@spacemonkeylabs.com'))
-		//  ->setBody('Here is the message itself')
-		//  ;
 		
 		//Send the message
 		$result = $mailer->send($message);
