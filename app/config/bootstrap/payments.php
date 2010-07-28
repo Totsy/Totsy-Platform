3f06e68328449a5e2a0154d12c9092ec3ee0cc84
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-07-28 12:55:05 -0400

    updating payment bootstrap

--- app/config/bootstrap/payments.php
+++ app/config/bootstrap/payments.php
@@ -5,10 +5,10 @@ use li3_payments\extensions\Payments;
 Payments::config(array(
 	'default' => array(
 		'adapter' => 'AuthorizeNet',
-		'login' => '3Zuk5g64mFR',
-		'key' => '3DgFTdNk4q9342yr',
-		'debug' => true,
-		'gateway' => 'live',
+		'login' => '3mVJKk363dcz',
+		'key' => '3zr65H5abZ68H4Ye',
+		'debug' => false,
+		'gateway' => 'test',
 		'connection' => array('classes' => array('socket' => 'lithium\net\socket\Curl'))
 	)
 ));
