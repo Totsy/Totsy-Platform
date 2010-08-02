ee100e0423f05ce0fadfa6ca4ebc5b2258f9caa3
pierre davidoff <pierre.davidoff@gmail.com>
2010-08-02 16:35:58 -0400

    menu test for IE 7.0

--- app/views/layouts/main.html.php
+++ app/views/layouts/main.html.php
@@ -40,7 +40,7 @@
 	          ** For each div with class menu (i.e.,
 	          ** the thing we want to be on top),
 	          */
-	          $$("main-nav ul").each(function(menu) {
+	          $$("main-nav").each(function(menu) {
 	              // For each of its ancestors,
 	              menu.ancestors().each(function (a) {
 	                  var pos = a.getStyle("position");
