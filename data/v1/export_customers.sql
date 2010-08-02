db9730fd4d802bfa65840eadc04de225906fd8fc
Mitch Pirtle <mitch@spacemonkeylabs.com>
2010-08-02 19:45:31 -0400

    updating v1 customer import scripts

--- data/v1/export_customers.sql
+++ data/v1/export_customers.sql
@@ -1,18 +1,25 @@
-SELECT c.id AS "_id",
-c.first_name AS "firstname",
-c.last_name AS "lastname",
-0 AS "affiliate",
-c.login AS "username",
-c.email AS "email",
-1 AS "legacy",
-c.crypted_password AS "password",
-c.password_salt AS "salt",
-c.created_at AS "created_orig",
-c.updated_at AS "updated_orig",
-c.last_login_at AS "lastlogin",
-c.last_login_ip AS "lastip",
-c.login_count AS "logincounter",
-1 AS "active"
+-- MAKE SURE YOU GOT AN INDEX ON invitation_tokens.customer_id
+SELECT '{_id:"'||c.id||'"' AS "_id",
+'firstname:"'||c.first_name||'"' AS "firstname",
+'lastname:"'||c.last_name||'"' AS "lastname",
+'affiliate:'||0 AS "affiliate",
+'username:"'||c.login||'"' AS "username",
+'email:"'||c.email||'"' AS "email",
+'legacy:'||1 AS "legacy",
+'password:"'||c.crypted_password||'"' AS "password",
+'salt:"'||c.password_salt||'"' AS "salt",
+'created_orig:{ "$date" : '||round(COALESCE(EXTRACT(EPOCH FROM c.created_at),1))||'000}' AS "created_orig",
+'updated_orig:{ "$date" : '||round(COALESCE(EXTRACT(EPOCH FROM c.updated_at),1))||'000}' AS "updated_orig",
+'lastlogin:{ "$date" : '||round(COALESCE(EXTRACT(EPOCH FROM c.last_login_at),1))||'000}' AS "lastlogin",
+'lastip:"'||COALESCE(c.last_login_ip,'')||'"' AS "lastip",
+'logincounter:'||COALESCE(c.login_count,0) AS "logincounter",
+'active:'||1 AS "active",
+'invitation_codes:["'||
+array_to_string (
+  array(
+    SELECT code FROM invitation_tokens where customer_id = c.id
+  ), '","'
+)||'"]}' AS "invitation_codes"
 
 FROM customers c
 
@@ -20,5 +27,5 @@ WHERE first_name != ''
 
 ORDER BY c.id
 -- limit output
---LIMIT 200
+--LIMIT 2000
 ;
