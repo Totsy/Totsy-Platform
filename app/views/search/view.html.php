b827159597026daa1408e2fb441aa06a1058cd0c
pierre davidoff <pierre.davidoff@gmail.com>
2010-08-01 20:20:42 -0400

    remove the comment in the search view

--- app/views/search/view.html.php
+++ app/views/search/view.html.php
@@ -1,7 +1,7 @@
 <h1>Sorry, we couldn't find what you were looking for</h1>
 
 <?php if (!count($events)) { ?>
-	<p>Furthermore, we have no clue what you're talking about.
+	
 	<?php return; ?>
 <?php } ?>
 <?php var_dump($events->data()); ?>
