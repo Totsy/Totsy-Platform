cc2e7fe9f0d54cdd4f165ab30fbd8dbd599e3c31
fitzagard <fhagard@lightcube.us>
2010-08-02 22:09:10 -0400

    More javascript cleanup.

--- app/views/layouts/main.html.php
+++ app/views/layouts/main.html.php
@@ -11,63 +11,6 @@
 	<?=$this->html->script(array('jquery-1.4.2.min.js','jquery-ui-1.8.2.custom.min.js', 'jquery.countdown.min')); ?>
 	<?=$this->scripts(); ?>
 	<?=$this->html->link('Icon', null, array('type' => 'icon')); ?>
-
-	<script type="text/javascript">
-		$.base = '<?=rtrim(Router::match("/", $this->_request)); ?>';
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-675412-15']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	  function isIE()
-	  {
-	      if(navigator.userAgent.match(/MSIE \d\.\d+/))
-	          return true;
-	      return false;
-	  }
-	  
-	  function zIndexWorkaround()
-	  {
-	      // If the browser is IE,
-	      if(isIE())
-	      {
-	          /*
-	          ** For each div with class menu (i.e.,
-	          ** the thing we want to be on top),
-	          */
-	          $$("main-nav").each(function(menu) {
-	              // For each of its ancestors,
-	              menu.ancestors().each(function (a) {
-	                  var pos = a.getStyle("position");
-
-	                  // If it's positioned,
-	                  if(pos == "relative" ||
-	                     pos == "absolute" ||
-	                     pos == "fixed")
-	                  {
-	                      /*
-	                      ** Add the "on-top" class name when the
-	                      ** mouse is hovering over it,
-	                      */
-	                      Event.observe(a, "mouseover", function() {
-	                          a.addClassName("on-top");
-	                      });
-	                      // And remove it when the mouse leaves.
-	                      Event.observe(a, "mouseout", function() {
-	                          a.removeClassName("on-top");
-	                      });
-	                  }
-	              });
-	          });
-	      }
-	  }
-	  	  
-	</script>
 </head>
 
 <body class="app">
@@ -129,6 +72,18 @@
 		});
 		$("#cart-modal").dialog('open');
 	});
+	</script>
+	<script type="text/javascript">
+		$.base = '<?=rtrim(Router::match("/", $this->_request)); ?>';
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-675412-15']);
+	  _gaq.push(['_trackPageview']);
+
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
 	</script>
 	</body>	
 </html>
