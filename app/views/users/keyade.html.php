2b03f5951bf247333b0fdfd59be14a25c19d8db1
Jonathan Lackey <jon@zunostudios.com>
2010-08-12 15:25:03 -0700

    Added new mark-up for IE7 fixes

--- app/views/users/keyade.html.php
+++ app/views/users/keyade.html.php
@@ -1,32 +1,54 @@
 <div id="fullscreen">
+
 	<div id="login-box">
+
 		<div id="login-box-border">
+
 			<div id="login-box-container">
-				<h1 id="logo">Totsy</h1>
-				<div id="intro-copy">
-					<h2 style="margin-top:30px"><span>Welcome to Totsy!!!</span></h2>
+				
+				<div class="tt">
+					<div></div>
+				</div>			
+				
+				<div class="tm">
+					<div class="ti">		
+					
+						<div class="tc login-inner">
+				
+							<h1 id="logo">Totsy</h1>
+							<div id="intro-copy">
+								<h2 style="margin-top:30px"><span>Welcome to Totsy!!!</span></h2>
+							</div>
+							<div class="r-container clear reg-list">
+							<div class="tl"></div>
+							<div class="tr"></div>
+							<div class="r-box lt-gradient-1">
+								<strong class="red">Why you will love Totsy</strong>
+								<ul class="bugs columns-2">
+									<li>Exclusive sales for moms, children &amp; babies.</li>
+									<li>Sales last up to 3 days, plenty of time to shop.</li>
+									<li>Savings of up to 70% off retail.</li>
+									<li>For every purchase, one tree is planted.</li>
+									<li>Membership is free</li>
+									<li>We are 100% green.</li>
+								</ul>
+							</div>
+							<div class="bl"></div>
+							<div class="br"></div>
+							</div>
+							<br><br><br>
+							<center><p><strong class='red'>Thank you for registering with Totsy.<br>
+								In a few seconds you will be redirected to see all our latest sales.
+							</strong></p></center>
+						
+						</div>
+					</div>
 				</div>
-				<div class="r-container clear reg-list">
-				<div class="tl"></div>
-				<div class="tr"></div>
-				<div class="r-box lt-gradient-1">
-					<strong class="red">Why you will love Totsy</strong>
-					<ul class="bugs columns-2">
-						<li>Exclusive sales for moms, children &amp; babies.</li>
-						<li>Sales last up to 3 days, plenty of time to shop.</li>
-						<li>Savings of up to 70% off retail.</li>
-						<li>For every purchase, one tree is planted.</li>
-						<li>Membership is free</li>
-						<li>We are 100% green.</li>
-					</ul>
+				
+				<div class="tb">
+					<div></div>
 				</div>
-				<div class="bl"></div>
-				<div class="br"></div>
-			</div>
-			<br><br><br>
-			<center><p><strong class='red'>Thank you for registering with Totsy.<br>
-				In a few seconds you will be redirected to see all our latest sales.
-			</strong></p></center>
+
 		</div>
 	</div>
 </div>
