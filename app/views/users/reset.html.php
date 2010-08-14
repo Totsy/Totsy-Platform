c4f7838b430ebca8e4e4dcb060d37543d7e1d0f6
Jonathan Lackey <jon@zunostudios.com>
2010-08-13 17:42:22 -0700

    fix modal window for IE7

--- app/views/users/reset.html.php
+++ app/views/users/reset.html.php
@@ -37,57 +37,77 @@ $image = $images[array_rand($images)];
 	
 	<div id="login-box">
 	
-		<div id="login-box-border">
+		<div id="login-box-border" class="login-modal">
 			
 			<div id="login-box-container">
-			
-				<h1 id="logo">Totsy</h1>
 				
-				<div id="intro-copy">
+				<div class="tt">
+					<div></div>
+				</div>
 				
-					<p class="red">You have places to be, things to do, and little ones in tow.</p>
+				<div class="tm">
+					<div class="ti">		
 					
-					<p>At Totsy, moms on the go and moms to be experience the shopping they desire at prices they can't resist.</p>
+						<div class="tc login-inner">
 				
-				</div>
-<div class="sign-in-container">
-	<div id="sign-in-box" class="r-container">
-		<div class="tl"></div>
-		<div class="tr"></div>
-		<div class="r-box lt-gradient-1">		
-				<?php if (!empty($message)): ?>
-					<?=$message;?>
-				<?php endif ?>
-				<?=$this->form->create(null, array('id'=>'loginForm')); ?>
-						<h2>Password Reset</h2>
-						
-						<p>Please enter your email below and more instructions will be sent to you.</p>
-					<br><br>
-					<?=$this->form->text('email', array('label' => 'Email Address')); ?>
-					<br><br>
-					<?=$this->form->submit('Reset Password', array('class'=>"flex-btn-2"));?>
-				<?=$this->form->end(); ?>
-		</div>
-		<div class="bl"></div>
-		<div class="br"></div>
-	</div>
-</div>				
-			<div class="register-container">
-				<div id="register-box" class="r-container">
-					<div class="tl"></div>
-					<div class="tr"></div>
-					<div class="r-box lt-gradient-1">
-						<h2>Become a Member</h2>
-						
-						<p>Become a member today for access to brand-specific sales, up to 70% off retail, just for you and the kids, ages 0-7. Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, and educational materials &mdash; and that's just the start.</p>
-						
-						<?=$this->html->link('Request Membership','/register', array('class'=>"flex-btn-2")); ?>
+							<h1 id="logo">Totsy</h1>
+							
+							<div id="intro-copy">
+							
+								<p class="red">You have places to be, things to do, and little ones in tow.</p>
+								
+								<p>At Totsy, moms on the go and moms to be experience the shopping they desire at prices they can't resist.</p>
+							
+							</div>
+			
+							<div class="sign-in-container">
+								<div id="sign-in-box" class="r-container">
+									<div class="tl"></div>
+									<div class="tr"></div>
+									<div class="r-box lt-gradient-1">		
+											<?php if (!empty($message)): ?>
+												<?=$message;?>
+											<?php endif ?>
+											<?=$this->form->create(null, array('id'=>'loginForm')); ?>
+													<h2>Password Reset</h2>
+													
+													<p>Please enter your email below and more instructions will be sent to you.</p>
+												<br><br>
+												<?=$this->form->text('email', array('label' => 'Email Address')); ?>
+												<br><br>
+												<?=$this->form->submit('Reset Password', array('class'=>"flex-btn-2"));?>
+											<?=$this->form->end(); ?>
+									</div>
+									<div class="bl"></div>
+									<div class="br"></div>
+								</div>
+							</div>				
+			
+							<div class="register-container">
+								<div id="register-box" class="r-container">
+									<div class="tl"></div>
+									<div class="tr"></div>
+									<div class="r-box lt-gradient-1">
+										<h2>Become a Member</h2>
+										
+										<p>Become a member today for access to brand-specific sales, up to 70% off retail, just for you and the kids, ages 0-7. Prenatal care products, baby gear, travel accessories, bedding and bath, children's clothing, toys, and educational materials &mdash; and that's just the start.</p>
+										
+										<?=$this->html->link('Request Membership','/register', array('class'=>"flex-btn-2")); ?>
+									</div>
+									<div class="bl"></div>
+									<div class="br"></div>
+								
+								</div>
+							</div>
+				
+						</div>
 					</div>
-					<div class="bl"></div>
-					<div class="br"></div>
-				
 				</div>
-			</div>
+				
+				<div class="tb">
+					<div></div>
+				</div>	
+				
 			</div>
 			
 		</div>
