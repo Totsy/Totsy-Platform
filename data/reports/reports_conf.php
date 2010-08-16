7f64040648a5059b3f23d278ce93338cd8273244
Mitch Pirtle <mpirtle@totsy.com>
2010-08-16 17:09:41 +0000

    Unbustomicating reports for production

--- data/reports/reports_conf.php
+++ data/reports/reports_conf.php
@@ -2,9 +2,9 @@
 
 // hostname or ip of mongodb server
 // DEVELOPMENT
-$mhost = 'localhost';
+//$mhost = 'localhost';
 // PRODUCTION
-//$mhost = '172.20.8.33';
+$mhost = '172.20.8.33';
 
 // database name
 $mdb = 'totsy';
