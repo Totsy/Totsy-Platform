b131f6e0ee6dbeaed61b68ff7ab04b8dc692c82f
Mitch Pirtle <mpirtle@totsy.com>
2010-08-24 15:08:11 +0000

    Updating FAQ text

--- app/views/pages/faq.html.php
+++ app/views/pages/faq.html.php
@@ -101,14 +101,10 @@ $(document).ready(function() {
 		<h2>Site Issues</h2>
 		
 		<p><strong>Where did my credits go?</strong><br />
-		We are in the process of migrating all customer credit history to the new platform. Rest assured 
-		that if you have earned credits in the past, your credits are not lost. Your credits will be 
-		here within the next few days, and new credits are already functional so you won't miss out on 
-		any opportunities while we are migrating the data from our old site.</p>
-		
-		<p><strong>My order history is not there. either.</strong><br />
-		Like credits, order data is in the process of being massaged and imported to the new platform. 
-		This is expected to be up shortly.</p>
+		We've migrated your credits from the old system, and are now hooking them into checkout so you
+		can select how much you want to apply at the time of purchase. This should be available shortly, 
+		and new credits are already functional so you won't miss out on 
+		any opportunities while we finish up the migration from our old site.</p>
 		
 		<p><strong>How about invitations, did I lose my invitations from the old site?</strong><br />
 		Your invitations are safely stored in the database, and we're working to hook the new platform 
