32bfe2a6fe5b6982d1c89d0f2ae3e8a149a5ca1b
fitzagard <fhagard@lightcube.us>
2010-09-13 17:36:31 -0400

    Adding more functionality to TincyMCE.

--- admin/views/items/edit.html.php
+++ admin/views/items/edit.html.php
@@ -14,9 +14,19 @@
 
 <script type="text/javascript">
 tinyMCE.init({
-	mode : "textareas",
-	theme : "simple",
-	width : "500"
+	// General options
+		mode : "textareas",
+		theme : "advanced",
+		plugins :
+"safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,filemanager",
+		// Theme options
+		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,help,code,|preview,|,forecolor,backcolor",	
+		theme_advanced_buttons3 : "tablecontrols",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
 });
 
 </script>
