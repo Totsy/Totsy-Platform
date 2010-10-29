b72fe1046ad44bce24995209537862b72ac2e630
Eric The Robot <eric@p2136406>
2010-10-29 14:30:49 +0000

    sliming down tinymce plugin

--- admin/views/items/edit.html.php
+++ admin/views/items/edit.html.php
@@ -14,22 +14,13 @@
 
 <script type="text/javascript">
 tinyMCE.init({
-	// General options
-		mode : "textareas",
-		theme : "advanced",
-		plugins :
-"safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,filemanager",
-		// Theme options
-		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,help,code,|preview,|,forecolor,backcolor",	
-		theme_advanced_buttons3 : "tablecontrols",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : true,
+	mode : "textareas",
+	theme : "simple",
+	width : "600"
 });
 
 </script>
+
 <div class="grid_16">
 	<h2 id="page-heading">Editing Item - <?=$item->description?></h2>
 </div>
