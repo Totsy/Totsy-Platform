be06f430db6b1fe8cc2bf58fff5ec570310709f0
fitzagard <fhagard@lightcube.us>
2010-11-04 10:31:30 -0400

    Refs #310 - Adding Reports Model.

---
+++ admin/models/Report.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace admin\models;
+
+use admin\models\Base;
+
+class Report extends Base {
+	
+	protected $_meta = array('source' => 'reports');
+	public $validates = array();
+	
+}
+
+?>
