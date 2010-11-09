2d219f656947e73ee239fb74795acc2781ee23b5
Eric The Robot <eric@p2136406>
2010-11-09 17:53:31 +0000

    Refs #218 - adding style to blog

--- app/views/blog/index.html.php
+++ app/views/blog/index.html.php
@@ -1,7 +1,13 @@
-<?php foreach ($rss->item as $item): ?>
-	<div>
-		<h2><?php echo $item->title?></h2>
-		<div><?=$item->pubDate?></div>
-		<?php echo $item->description?>
-	</div>
-<?php endforeach ?>
+<h1 class="p-header">Totsy Blog</h1>
+
+	<?php foreach ($rss->item as $item): ?>
+        <div style="margin:15px 35px;">
+          <h3 style="font-size:16px;"><?php echo $item->title?>
+              <span style="font-size:12px!important; color:#CCC; float:right; font-style:italic;">
+                <?=$item->pubDate?>
+              </span>
+          </h3>
+          <hr style="color:#CCC;"/>
+          <p><?php echo $item->description?></p>
+        </div>
+    <?php endforeach ?>
