539872f2e1201eb8562bd1522d8d6bee703ee645
Eric The Robot <eric@p2136406>
2010-11-11 22:47:35 +0000

    adding min-height property since there isnt any content. making things pretty.

--- app/views/search/view.html.php
+++ app/views/search/view.html.php
@@ -1,6 +1,7 @@
 <h1>Sorry, we couldn't find what you were looking for</h1>
 
-<?php if (!count($events)) { ?>
-	
-	<?php return; ?>
-<?php } ?>
+<div style="min-height:500px;"> 
+	<?php if (!count($events)) { ?>
+        <?php return; ?>
+    <?php } ?>
+</div>
