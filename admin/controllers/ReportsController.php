ac795b1a8f69b165b42b3270e868f9c4d7082323
fitzagard <fhagard@lightcube.us>
2010-11-15 16:58:19 -0500

    Refs - #562 Ooops.

--- admin/controllers/ReportsController.php
+++ admin/controllers/ReportsController.php
@@ -256,7 +256,7 @@ class ReportsController extends BaseController {
 							foreach ($orderData as $order) {
 								$items = $order['items'];
 								foreach ($items as $item) {
-									if (($item['item_id'] == $eventItem['_id']) && (string) $key == $item['size'])){
+									if (($item['item_id'] == $eventItem['_id']) && ((string) $key == $item['size'])){
 										$purchaseOrder[$inc]['Product Name'] = $eventItem['description'];
 										$purchaseOrder[$inc]['Product Color'] = $eventItem['color'];
 										$purchaseOrder[$inc]['SKU'] = $eventItem['vendor_style'];
