3009d8e8713b0495ad5d5d63fc4dffaedab041c6
fitzagard <fhagard@lightcube.us>
2010-11-24 15:27:54 -0500

    Hooking up view for Event table generator.

---
+++ admin/views/base/selectEvent.html.php
@@ -0,0 +1,19 @@
+<?=$this->html->script('jquery-1.4.2.min.js');?>
+<?=$this->html->script('jquery.dataTables.js');?>
+<?=$this->html->style('table');?>
+
+<div class="grid_16">
+	<h2 id="page-heading">Select Event for <?=$type?> Administration</h2>
+</div>
+<div class='clear'></div>
+<div class="grid_16">
+	<?=$this->events->build($events, array('type' => $type))?>
+</div>
+<div class='clear'></div>
+
+
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+		$('#itemTable').dataTable();
+	} );
+</script>
