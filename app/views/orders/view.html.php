5fd970bc58cbd7db24da4df07174e257271c3b46
fitzagard <fhagard@lightcube.us>
2010-11-30 17:03:56 -0500

    Refs #583 - Don't show old ship dates in order detail view.

--- app/views/orders/view.html.php
+++ app/views/orders/view.html.php
@@ -84,7 +84,7 @@
 -->
 <div style="clear:both;"></div>
 								<h2 style="margin:20px 0px 0px 0px;" class="gray mar-b">Order Summary
-									<?php if (!empty($shipDate) || ($shipDate > time())): ?>
+									<?php if (!empty($shipDate) && ($shipDate > time())): ?>
 										<span style="color:red; font-weight:bold; float:right;">Estimated Ship Date: <?=date('M d, Y', $shipDate); ?></span></h2>
 									<?php endif ?>
 <hr />
