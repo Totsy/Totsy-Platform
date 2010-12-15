95ac2875f9ae07449ddf1632d638ebfcac501c34
lawren.hanson <lawren.hanson@gmail.com>
2010-12-15 14:03:58 -0500

    Ref #646-Promocodes report and search features

--- admin/controllers/PromocodesController.php
+++ admin/controllers/PromocodesController.php
@@ -5,6 +5,7 @@ namespace admin\controllers;
 use \admin\models\Promocode;
 use \admin\models\Promotion;
 use MongoDate;
+use MongoRegex;
 
 class PromocodesController extends \lithium\action\Controller {
 
@@ -35,14 +36,50 @@ class PromocodesController extends \lithium\action\Controller {
 	}
 
 	public function view($code = null) {
-        var_dump($code);
-		$promocode = Promocode::all( array( 'conditions' => array( 'code'=> strtoupper($code) ) ) );
-        //var_dump($promocode);
-		return compact('promocode');
+       // var_dump($code);
+		$promocodes = Promocode::find( 'all', array( 
+                                                'conditions' =>array( 
+                                                                '$or' =>  array( 
+                                                                    array( 'code' => strtolower($code) ),
+                                                                    array( 'code' => strtoupper($code) )
+                                                    ) ) ) );
+      //  var_dump($promocode->data());
+        $promocodes= $promocodes->data();
+		return compact('promocodes');
 	}
     
     public function report() {
-		$promotions = Promotion::all();
+        
+        $data = $this->request->data;
+        
+        var_dump($data);
+        
+        if( empty($data) ){
+           
+            $promotions = Promotion::all();
+        
+        }else{
+           
+            $search = $data['search'];
+            if( !empty($data['search']) ) {
+                     
+                    $search = $data['search'];
+                    $promotions  = Promotion::find(  'all', array( 
+                                                    'conditions' =>array( 
+                                                                    '$or' =>  array( 
+                                                                        array( 'code' => strtolower($search) ),
+                                                                        array( 'code' => strtoupper($search) )
+                                                        ) ) ) );
+            }
+            
+            if( !empty( $data['start'] ) && !empty($data['end']) ) {
+                 
+                $start = new MongoDate( strtotime($data['start']) );
+                $end = new MongoDate( strtotime($data['end']) );
+                $promotions  = Promotion::find(  'all', array( 'conditions'=>array('date_created' =>array( '$gt' => $start, '$lte' => $end ) ) ) );
+            }
+            
+        }
         
         foreach($promotions as $promotion){
           
@@ -64,20 +101,14 @@ class PromocodesController extends \lithium\action\Controller {
 		$promoCode= Promocode::create();
         $code = $this->request->data;
          
-              
-          if( !empty($code) ) {
+        if( !empty($code) ) {
             
            $promocode = Promocode::all( array( 'conditions'=>array( 'code' => $code['code'] ) ) );
                 
-           // var_dump($promocode->data());
-                
-                
                 if ($promocode->data() ) {
-                    $message = 'Coupon Code Already Exists'  ;
+                    $message = 'Coupon Code Already Exists';
                     return compact('message');
-                    
                 }   
-                 
                   
                   if( $this->request->data['enabled'] !== '1' ){
                       
--- admin/views/promocodes/report.html.php
+++ admin/views/promocodes/report.html.php
@@ -2,11 +2,26 @@
 <br>
 <br>
 
+<?=$this->form->create(); ?>
+    <h5>Search code: </h5>
+        <?=$this->form->text('search'); ?>
+    <nbsp>
+        <h5> Date Range: </h5> <nbsp>
+        start:
+        <?=$this->form->text('start'); ?> 
+        end:
+        <?=$this->form->text('end'); ?>
+    
+   <?=$this->form->submit('search'); ?><br><br>
+<?=$this->form->end(); ?>
+
+<br>
 <?php foreach( $promotions as $promotion ): ?>
     
     Promocode: <?=$this->html->link($promotion->code, 'promocodes/view/'.$promotion->code ); ?> <br>
-    Userid: <?=$promotion->user_id; ?><br>
-    Amount Saved: $<?=$promotion->saved_amount; ?><br>
+    Userid: <?=$this->html->link($promotion->user_id, 'users/view/'.$promotion->user_id, array('target' => '_blank')); ?><br>
+    Order: <?=$this->html->link($promotion->order_id, 'orders/view/'.$promotion->order_id, array('target' => '_blank') );?><br>
+    Amount Saved: $<?php echo round($promotion->saved_amount, 2 ); ?><br>
     Created on: <?=$promotion->date_created; ?><br>
     <br>
     <br>
