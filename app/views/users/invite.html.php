0d779c98d9464c9a4601e45fb686805a752f7e48
fitzagard <fhagard@lightcube.us>
2010-12-27 12:49:20 -0500

    Refs #731 - Fixing check for invitation code.

--- app/views/users/invite.html.php
+++ app/views/users/invite.html.php
@@ -53,7 +53,7 @@
 										<?=$this->form->submit('Send', array('class' => 'send-btn fl')); ?>
 									<?=$this->form->end(); ?>
 									<br><br><br>
-									<?php if (is_array($user->invitation_codes)): ?>
+									<?php if (is_array($user->invitation_codes->data())): ?>
 										<?php foreach ($user->invitation_codes as $code): ?>
 											<?php $invite = "http://www.totsy.com/join/" . $code;?>
 										<?php endforeach ?>
