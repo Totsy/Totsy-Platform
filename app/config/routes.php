d8e8f05826cbe5b6a76ff3ba2018a12d23b89307
Mitch Pirtle <mitch@spacemonkeylabs.com>
2011-01-11 14:50:29 -0500

    Refs #803 - unbustomicating router and urls

--- app/config/routes.php
+++ app/config/routes.php
@@ -65,9 +65,9 @@ if(!Session::check('userLogin')) {
 Router::connect('/', 'Events::index');
 Router::connect('/sales', 'Events::index');
 Router::connect('/{:action:login|logout}', array('controller' => 'users'));
-Router::connect('/myaccount/addresses', 'Addresses::view');
-Router::connect('/myaccount/address/add', 'Addresses::add');
-Router::connect('/myaccount/addresses/edit{:args}', 'Addresses::edit');
+Router::connect('/myaccount/addresses/{:action}', array('controller' => 'Addresses'));
+//Router::connect('/myaccount/address/add', 'Addresses::add');
+//Router::connect('/myaccount/addresses/edit{:args}', 'Addresses::edit');
 Router::connect('/myaccount', 'Users::info');
 Router::connect('/myaccount/add/{:args}', 'Account::add');
 Router::connect('/myaccount/orders', 'Orders::index');
