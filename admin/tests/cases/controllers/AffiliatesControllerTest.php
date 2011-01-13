f1c9ac4d5fcb74ce50a4260c05755f4d3c8c4a70
Lawrenberg <lhanson@totsy.com>
2011-01-13 13:44:00 -0500

    Ref#590 - Affiliate Code Creator Phase I. Simple add/edit of affiliate.

---
+++ admin/tests/cases/controllers/AffiliatesControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace admin\tests\cases\controllers;
+
+use \admin\controllers\AffiliatesController;
+
+class AffiliatesControllerTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+	public function testIndex() {}
+	public function testView() {}
+	public function testAdd() {}
+	public function testEdit() {}
+}
+
+?>
