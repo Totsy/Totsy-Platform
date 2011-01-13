e4cd43b65111c6a931590b4685c187433815442a
Lawrenberg <lhanson@totsy.com>
2011-01-13 13:38:13 -0500

    changed Navigation for marketing admin panel

--- admin/models/Promocode.php
+++ admin/models/Promocode.php
@@ -2,19 +2,13 @@
 
 namespace admin\models;
 
-use lithium\storage\Session as LiSession;
+
 
 class Promocode extends \admin\models\Base {
-    
+
     protected $_meta = array('source' => 'promocodes');
-    
-	public static function createdBy($data){
-		$user = LiSession::read('userLogin');
-		$data['created_by']= $user['_id'];
-		
-		return $data;
-	}
-	    
+
+
 }
 
 
