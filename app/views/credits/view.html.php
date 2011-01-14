454c35b6764d61952efe2172f1bb0b927826a7e9
fitzagard <fhagard@lightcube.us>
2011-01-14 00:11:14 -0500

    Refs #618 - Giving view a little style.

--- app/views/credits/view.html.php
+++ app/views/credits/view.html.php
@@ -2,26 +2,36 @@
 <h1 class="p-header">My Credits</h1>
 <?=$this->menu->render('left'); ?>
 
-<div id="name">
-	Total Credit is:
-	<?php if (!empty($credit)): ?>
-		$<?=$credit?>
-	<?php endif ?>
-</div>
-<table border="0" cellspacing="5" cellpadding="5">
-	<tr>
-		<th>Date</th>
-		<th>Amount</th>
-		<th>Reason</th>
-	</tr>
-	<?php foreach ($credits as $credit): ?>
-		<?php if ($credit->created): ?>
+<div id="middle" class="noright">
+	<div class="tl"></div>
+	<div class="tr"></div>
+	<div id="page">
+		<h2 class="gray mar-b">My Orders</h2>
+		<div id="name">
+			<b>Credit Total:
+			<?php if (!empty($credit)): ?>
+				$<?=$credit?>
+			<?php endif ?>
+			</b>
+		</div>
+		<table border="0" cellspacing="5" cellpadding="5" width="100%" class="order-table">
 			<tr>
-				<td><?=date('Y-m-d', $credit->created->sec)?></td>
-				<td>$<?=$credit->credit_amount?></td>
-				<td><?=$credit->reason?></td>
+				<th>Date</th>
+				<th>Amount</th>
+				<th>Reason</th>
 			</tr>
-		<?php endif ?>
-	<?php endforeach ?>
+			<?php foreach ($credits as $credit): ?>
+				<?php if ($credit->created): ?>
+					<tr>
+						<td><?=date('Y-m-d', $credit->created->sec)?></td>
+						<td>$<?=$credit->credit_amount?></td>
+						<td><?=$credit->reason?></td>
+					</tr>
+				<?php endif ?>
+			<?php endforeach ?>
 	
-</table>
+		</table>
+	</div>
+	<div class="bl"></div>
+	<div class="br"></div>
+</div>
