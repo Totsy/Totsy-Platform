754f3c5c8dcfe82816f793778aa4e2e8d10b5964
fitzagard <fhagard@lightcube.us>
2011-01-13 21:37:51 -0500

    Refs #690 - Trying to optimize code so we don't look at invitations that are from affiliates.

--- admin/extensions/command/ApplyCredits.php
+++ admin/extensions/command/ApplyCredits.php
@@ -11,33 +11,41 @@ use MongoDate;
 use MongoRegex;
 use MongoId;
 
-
-
 /**
  * The credits command is executed to provide invitation credits to all users
- * who have been invited by someone else.
+ * who have accepted an invitation and have sucessfully placed an order.
  */
 class ApplyCredits extends \lithium\console\Command {
 
 	public function run() {
+		$this->header('Launching search for Invitations....');
 		$this->_credit();
+		$this->out('Finished Applying Credits');
 	}
 
 	/**
 	 * The _credit method loops through all the outstanding invitations and applies credit if applicable.
 	 *
-	 * The first step of this process is to find all the invitations that have not already been fulfilled.
+	 * The first step of this process is to find all the invitations that have not already recieved credit.
 	 * With the mongo cursor of invititations, use the inviation array to find the invitee email address.
 	 * After finding the user document based on email look for any order that has been fulfilled.
 	 * This is the part where we'll need to check order.shipped collection. Apply credits if there is an order.
 	 * @todo Change this method to utilize the orders.shipped collection.
 	 */
 	protected function _credit() {
-		$conditions = array('status' => 'Accepted', 'credited' => array('$ne' => true));
+		$affiliates = User::findAllByaffiliate(true, array('fields' => array('_id' => true)));
+		$affiliates = $affiliates->data();
+		foreach ($affiliates as $value) {
+			$ids[] = (string) $value['_id'];
+		}
+		$conditions = array(
+			'status' => 'Accepted',
+			'credited' => array('$ne' => true),
+			'user_id' => array('$nin' => $ids)
+		);
 		$invitations = Invitation::all($conditions);
 		$invitationCount = Invitation::count($conditions);
 		$this->out("There are currently $invitationCount outstanding invitations.");
-		$totalCredit = 0;
 		foreach ($invitations as $invitation) {
 			$user = User::findByemail($invitation->email);
 			$conditions = array(
@@ -47,7 +55,11 @@ class ApplyCredits extends \lithium\console\Command {
 			$order = Order::first($conditions);
 			if ($order) {
 				$user = User::find($invitation->user_id);
-				if (empty($user->affiliate)) {
+				$invitationCheck = User::find('first', array(
+					'email' => $invitation->email,
+					'credited' => true
+				));
+				if (empty($invitationCheck)) {
 					$this->out("Giving a credit to $invitation->user_id");
 					$data = array(
 						'user_id' => $invitation->user_id,
@@ -58,8 +70,6 @@ class ApplyCredits extends \lithium\console\Command {
 						$invitation->credited = true;
 						$invitation->save();
 					}
-				} else {
-					$this->out('Skipping affiliate user');
 				}
 			}
 		}
