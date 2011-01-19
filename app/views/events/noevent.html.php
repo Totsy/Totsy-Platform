ad8e7bca405f23094b22e0e88110b259b0aa5daa
Eric The Robot <erictherobot@gmail.com>
2011-01-19 18:42:42 -0500

    adding no event found page

--- app/views/events/noevent.html.php
+++ app/views/events/noevent.html.php
@@ -8,8 +8,9 @@
 
 <hr />
 
-<p>Please continue browsing <a href="/" title="Totsy's Sales">Totsy's Sales</a></p>
+<p>Please continue browsing <a href="/" title="Totsy's Sales">Today's Sales</a></p>
 <br/><br/>
+<!-- 
 <h3>Other Products You May Enjoy</h3>
 <hr/>
 <div style="height:400px;">
@@ -27,6 +28,6 @@
 	</div>
 	<div style="background:#f2f2f2; width:120px; height:120px; border:1px solid #ddd; display: block; float:left; margin:0px 0px 10px 0px;">
 	</div>
-
+-->
 </div>
 
