6121e78dde9104ddcada7539fc6ddf2d5f3d5710
Eric The Robot <erictherobot@gmail.com>
2011-01-19 17:42:39 -0500

    removing one home sale banner

--- app/views/events/noevent.html.php
+++ app/views/events/noevent.html.php
@@ -12,7 +12,7 @@
 <br/><br/>
 <h3>Other Products You May Enjoy</h3>
 <hr/>
-<div>
+<div style="height:400px;">
 	<div style="background:#f2f2f2; width:120px; height:120px; border:1px solid #ddd; display: block; float:left; margin:0px 10px 10px 0px;">
 	</div>
 	<div style="background:#f2f2f2; width:120px; height:120px; border:1px solid #ddd; display: block; float:left; margin:0px 10px 10px 0px;">
@@ -27,6 +27,6 @@
 	</div>
 	<div style="background:#f2f2f2; width:120px; height:120px; border:1px solid #ddd; display: block; float:left; margin:0px 0px 10px 0px;">
 	</div>
-	
+
 </div>
 
