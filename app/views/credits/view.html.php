39e21769feedd2f37175a12b84395e4f89cc91b8
Eric The Robot <erictherobot@gmail.com>
2011-01-19 00:25:51 -0500

    lightly styling up my credits page

--- app/views/credits/view.html.php
+++ app/views/credits/view.html.php
@@ -7,24 +7,27 @@
 	<div class="tr"></div>
 	<div id="page">
 		<h2 class="gray mar-b">My Credits</h2>
-		<div id="name">
-			<b>Credit Total:
+		<hr/>
+		<div id="name" style="padding:10px 10px 10px 5px; background:#f1f1f1; color:#009900;" class="order-table">
+			<strong class="fl">Total Credits</strong>
+			<strong class="fr">
 			<?php if (!empty($credit)): ?>
 				$<?=$credit?>
-			<?php endif ?>
-			</b>
+			<?php endif ?>sss
+			</strong>
+			<div style="clear:both;"></div>
 		</div>
 		<table border="0" cellspacing="5" cellpadding="5" width="100%" class="order-table">
 			<tr>
 				<th>Date</th>
-				<th>Amount</th>
+				<th>Credit Amount</th>
 				<th>Reason</th>
 			</tr>
 			<?php foreach ($credits as $credit): ?>
 				<?php if ($credit->created): ?>
 					<tr>
 						<td><?=date('Y-m-d', $credit->created->sec)?></td>
-						<td>$<?=$credit->credit_amount?></td>
+						<td class="price">$<?=$credit->credit_amount?></td>
 						<td><?=$credit->reason?></td>
 					</tr>
 				<?php endif ?>
