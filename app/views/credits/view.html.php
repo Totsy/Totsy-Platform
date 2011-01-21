36624a4c292069da19e800ad8dc3188507c6ee4c
Eric The Robot <erictherobot@gmail.com>
2011-01-21 16:48:47 -0500

    adding if else logic for when users do not have credits

--- app/views/credits/view.html.php
+++ app/views/credits/view.html.php
@@ -9,13 +9,11 @@
 		<h2 class="gray mar-b">My Credits</h2>
 		<hr/>
 		
-		<?php if (!empty($credit)): ?>
+		<?php if (!empty($credit)) { ?>
 		<div id="name" style="padding:10px 10px 10px 5px; color:#009900;" class="order-table">
 			<strong class="fl">Total Credits: $<?=$credit?></strong>
 			<div style="clear:both;"></div>
 		</div>
-		<?php endif ?>
-		
 		<table border="0" cellspacing="5" cellpadding="5" width="100%" class="order-table">
 			<tr>
 				<th>Date</th>
@@ -30,7 +28,12 @@
 			<tr>
 			<?php endforeach ?>
 		</table>
-		
+		<?php } else { ?>
+		<div id="name" style="padding:10px 10px 10px 5px;" class="order-table">
+			<strong class="fl">Earn credits by <a href="/users/invite" title="inviting your friends and family">inviting your friends and family.</a></strong>
+			<div style="clear:both;"></div>
+		</div>
+		<?php } ?>
 	</div>
 	<div class="bl"></div>
 	<div class="br"></div>
