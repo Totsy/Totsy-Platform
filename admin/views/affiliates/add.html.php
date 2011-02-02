72963cdb7c4389d52fe64b35a7c19e2dda5401e7
Lawrenberg <lhanson@totsy.com>
2011-02-02 16:28:28 -0500

    Enhancement to affiliate admin panel

--- admin/views/affiliates/add.html.php
+++ admin/views/affiliates/add.html.php
@@ -81,7 +81,7 @@
 			newPixelDiv.html("<label> Pixel #" +counter + "</label> <br> Enable:"+
 				'<?=$this->form->checkbox("pixel['+(counter-1)+'][enable]", array("value"=>"1", "checked"=>"checked")); ?> <br> Select:'+
 				'<?=$this->form->select("pixel['+(counter-1)+'][page]", $sitePages, array("multiple"=>"multiple", "size"=>5)); ?><br> Pixel<br>'+
-				'<?=$this->form->textarea("pixel['+(counter-1)+'][pixel]"); ?>'
+				'<?=$this->form->textarea("pixel['+(counter-1)+'][pixel]", array("rows"=>"5")); ?>'
 				);
 			newPixelDiv.appendTo('#pixel_panel');
 
