298d26dddde2ad1204da5800b2ea8778630e4f01
troyer <troyer@totsy.com>
2011-02-18 16:07:48 -0500

    Comeback to Date("Z")

--- admin/controllers/ReportsController.php
+++ admin/controllers/ReportsController.php
@@ -147,8 +147,8 @@ class ReportsController extends BaseController {
 			$affiliate = new MongoRegex("/$name/i");
 			if ($this->request->data['min_date'] && $this->request->data['max_date']) {
 				//Conditions with date converted to the right timezone
-				$min = new MongoDate(strtotime($this->request->data['min_date'] . " (EST)"));
-				$max = new MongoDate(strtotime($this->request->data['max_date'] . " (EST)"));
+				$min = new MongoDate(strtotime($this->request->data['min_date'])+date('Z'));
+				$max = new MongoDate(strtotime($this->request->data['max_date'])+date('Z'));
 				$date = array(
 					'created_date' => array(
 						'$gte' => $min,
@@ -531,8 +531,8 @@ class ReportsController extends BaseController {
 				//Conditions with date converted to the right timezone
 				$conditions = array(
 					'date_created' => array(
-						'$gt' => new MongoDate(strtotime($this->request->data['min_date'] . " (EST)")),
-						'$lte' => new MongoDate(strtotime($this->request->data['max_date'] . " (EST)"))
+						'$gt' => new MongoDate(strtotime($this->request->data['min_date'])+date('Z')),
+						'$lte' => new MongoDate(strtotime($this->request->data['max_date'])+date('Z'))
 				));
 				$orderCollection = Order::collection();
 				$orders = $orderCollection->find($conditions);
@@ -630,8 +630,8 @@ class ReportsController extends BaseController {
 				//Conditions with date converted to the right timezone
 				$conditions = array(
 					'date_created' => array(
-						'$gt' => new MongoDate(strtotime($this->request->data['min_date'] . " (EST)")),
-						'$lte' => new MongoDate(strtotime($this->request->data['max_date'] . " (EST)"))
+						'$gt' => new MongoDate(strtotime($this->request->data['min_date'])+date('Z')),
+						'$lte' => new MongoDate(strtotime($this->request->data['max_date'])+date('Z'))
 				));
 				$orderCollection = Order::collection();
 				$orders = $orderCollection->find($conditions);
@@ -699,15 +699,15 @@ class ReportsController extends BaseController {
 				$conditions = array(
 					'total' => $dollarLimit,
 					'date_created' => array(
-						'$gt' => new MongoDate(strtotime($search['min_date'] . " (EST)")),
-						'$lte' => new MongoDate(strtotime($search['max_date'] . " (EST)"))
+						'$gt' => new MongoDate(strtotime($search['min_date'])+date('Z')),
+						'$lte' => new MongoDate(strtotime($search['max_date'])+date('Z'))
 				));
 				$shippingLimit = ($search['state'] != 'All') ? array('shipping.state' => $search['state']) : array();
 				$categoryFields = array('items.category' => array('$nin' => array('accessories','apparel')));
 				$categoryLimit = ($search['include_category'] == false) ? $categoryFields : array();
 				$conditions = $conditions + $shippingLimit + $categoryLimit;
 				$orderCollection = Order::collection();
-				$orders = $orderCollection->find($conditions);
+				$orders = $orderCollection->find($conditions);∂
 				$reportId = substr(md5(uniqid(rand(),1)), 1, 15);
 				$collection = Report::collection();
 				if ($orders) {
