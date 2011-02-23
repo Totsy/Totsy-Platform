b88225bb48b1ae826a330b79be983bef85deb672
Eric The Robot <erictherobot@gmail.com>
2011-02-23 12:35:48 -0500

    adding new mom committee page and updating left menu to reflect new page

--- app/views/pages/press.html.php
+++ app/views/pages/press.html.php
@@ -1,41 +1,34 @@
 <?php $this->title("Press"); ?>
+
 <h1 class="p-header">About Us</h1>
-			
-	<div id="left" id="left">
-		<ul class="menu main-nav">
-		  <h3 style="color:#999;">About Us</h3>
-		  <hr />
-		  <li class="first item15"><a href="/pages/aboutus" title="About Totsy"><span>How Totsy Works</span></a></li>
-		  <li class="first item16 active"><a href="/pages/press" title="Press"><span>Totsy in the Press</span></a></li>
-		  <li class="first item17"><a href="/pages/being_green" title="Being Green"><span>Being Green</span></a></li>
-		  <br />
-		  <h3 style="color:#999;">Need Help?</h3>
-		  <hr />
-		  <li class="first item18"><a href="/tickets/add" title="Contact Us"><span>Help Desk</span></a></li>
-		  <li class="first item19"><a href="/pages/faq" title="Frequently Asked Questions"><span>FAQ's</span></a></li>
-		</ul>
-	</div>
-
+<div id="left">
+<ul class="menu main-nav">
+  <li class="first item15"><a href="/pages/aboutus" title="About Totsy"><span>How Totsy Works</span></a></li>
+  <li class="first item17"><a href="/pages/moms" title="Meet The Moms"><span>Meet The Moms</span></a></li>
+  <li class="first item16 active"><a href="/pages/press" title="Press"><span>Totsy in the Press</span></a></li>
+  <!-- <li class="first"><a href="/pages/testimonials" title="Video Testimonials"><span>Video Testimonials</span></a></li> -->
+  <li class="first item17"><a href="/pages/being_green" title="Being Green"><span>Being Green</span></a></li>
+  <br />
+  <h3 style="color:#999;">Need Help?</h3>
+  <hr />
+  <li class="first item18"><a href="/tickets/add" title="Contact Us"><span>Help Desk</span></a></li>
+  <li class="first item19"><a href="/pages/faq" title="Frequently Asked Questions"><span>FAQ's</span></a></li>
+</ul>
+</div>
 <div id="middle" class="noright">
-
-<div class="tl"><!-- --></div> 
-<div class="tr"><!-- --></div> 
-
-<div id="page">
-  <h2 class="gray mar-b">Press</h2>
-	<hr />
-  <p>Totsy is the leading website in private sales for moms. We are also the first company of its
-		kind to be <?=$this->html->link('100% green', array('Pages::being_green')); ?>. More than
-		just exclusive savings, we are a hub of information, expert advice, and quality products
-		perfect for mom, baby, and child.</p>
-  <p>For media inquiries, please contact us at <a href="mailto:press@totsy.com">press@totsy.com</a>.</p>
-
-</div>
-<div class="bl"><!-- --></div> 
-<div class="br"><!-- --></div> 
-
-</div>
-
-</div>
-
+  <div class="tl"><!-- --></div>
+  <div class="tr"><!-- --></div>
+  <div id="page">
+    <h2 class="gray mar-b">Press</h2>
+    <hr />
+    <p>Totsy is the leading website in private sales for moms. We are also the first company of its
+      kind to be
+      <?=$this->html->link('100% green', array('Pages::being_green')); ?>
+      . More than
+      just exclusive savings, we are a hub of information, expert advice, and quality products
+      perfect for mom, baby, and child.</p>
+    <p>For media inquiries, please contact us at <a href="mailto:press@totsy.com">press@totsy.com</a>.</p>
+  </div>
+  <div class="bl"><!-- --></div>
+  <div class="br"><!-- --></div>
 </div>
