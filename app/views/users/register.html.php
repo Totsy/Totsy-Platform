bbf68933511472c170bf5105011f911362b8e312
Lawrenberg <lhanson@totsy.com>
2011-02-28 17:37:03 -0500

    New Registration

--- app/views/users/register.html.php
+++ app/views/users/register.html.php
@@ -6,13 +6,13 @@
 				<div class="tt">
 					<div><!-- --></div>
 				</div>
-				
+
 				<div class="tm">
-					
-					<div class="ti">		
-						
+
+					<div class="ti">
+
 						<div class="tc login-inner register-inner">
-							
+
 							<div id='logo'>
                             <h1>
                               <a href='/' title="Totsy.com">
@@ -20,15 +20,15 @@
                               </h1>
                             </div>
 
-							
+
 							<div id="intro-copy">
 								<h2 style="margin-top:30px"><span>Become a</span> MEMBER</h2>
 							</div>
-							
+
 							<div class="message">
 								<?php if($message){echo "$message"; } ?>
 							</div>
-							
+
 							<div class="r-container clear reg-list">
 								<div class="tl"></div>
 								<div class="tr"></div>
@@ -51,24 +51,24 @@
 								<div class="tl"></div>
 								<div class="tr"></div>
 								<div class="r-box lt-gradient-1">
-								
+
 								<?=$this->form->create($user ,array('id'=>'registerForm')); ?>
-               
-               <!-- Commnented Firstname, Lastname and Zip code ---->
-								
+
+               <!-- Commnented Firstname, Lastname and Zip code --->
+
 									<!-- div class="form-row">
 										<?=$this->form->label('firstname', 'First Name <span>*</span>', array(
 											'escape' => false,
 											'class' => 'required'
 											));
 										?>
-										
-										
+
+
 										<?=$this->form->text('firstname', array('class' => 'inputbox')); ?>
 										<?=$this->form->error('firstname'); ?>
 									</div>
-									
-									
+
+
 									<div class="form-row">
 										<?=$this->form->label('lastname', 'Last Name <span>*</span>', array(
 											'escape' => false,
@@ -84,20 +84,20 @@
 											'class' => 'required'
 											));
 										?>
-										
+
 										<?=$this->form->text('zip', array('class' => 'inputbox', 'id' => 'zip')); ?>
 										<?=$this->form->error('zip'); ?>
 									</div -->
-									
+
 									<!-- ************************************************************** -->
-									
+
 									<div class="form-row">
 										<?=$this->form->label('email', 'Email <span>*</span>', array(
 											'escape' => false,
 											'class' => 'required'
-											)); 
+											));
 										?>
-										<?=$this->form->text('email', array('class' => 'inputbox')); ?> 
+										<?=$this->form->text('email', array('class' => 'inputbox')); ?>
 										<?=$this->form->error('email'); ?>
 									</div>
 									<div class="form-row">
@@ -119,7 +119,7 @@
 									?>
 									<?=$this->form->password('password', array(
 											'class'=>"inputbox",
-											'name' => 'password', 
+											'name' => 'password',
 											'id' => 'password'));
 									?>
 									<?=$this->form->error('password'); ?>
@@ -128,29 +128,29 @@
 										<?=$this->form->checkbox('terms', array('class'=>"", 'style'=>"float:left;margin-right:4px"));?>
 
 										<span class="sm reg-tos">
-												By requesting membership, I accept the 
-											<?=$this->html->link('Terms and Conditions','pages/terms')?> 
-											of Totsy, and accept to receive sale email newsletters. 
+												By requesting membership, I accept the
+											<?=$this->html->link('Terms and Conditions','pages/terms')?>
+											of Totsy, and accept to receive sale email newsletters.
 											Totsy will never sell or give my email to any outside party.
 										</span>
 										<?=$this->form->submit('Register', array('class' => 'register_button')); ?>
 										<?=$this->form->error('terms'); ?>
 									</div>
 								<?=$this->form->end(); ?>
-									
+
 								</div>
 								<div class="bl"></div>
 								<div class="br"></div>
 							</div>
-							
+
 						</div>
 					</div>
 				</div>
-				
+
 				<div class="tb">
 					<div><!-- --></div>
 				</div>
-			
+
 			</div>
 		</div>
 	</div>
@@ -170,7 +170,7 @@ jQuery(function($){
 		<li><a href="/pages/faq" title="FAQ">FAQ</a></li>
 		<li class="last"><a href="/pages/contact" title="Contact Us">Contact Us</a></li>
 	</ul>
-	
+
 	<span id="copyright">&copy; 2010 Totsy.com. All Rights Reserved.</span>
 
 </div>
