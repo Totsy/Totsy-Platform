a3930bff8fc64baf49891152e436e28fd2e4530d
fitzagard <fhagard@lightcube.us>
2011-03-08 13:20:11 -0500

    Adding description to Cart error message and some documentation.

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -7,6 +7,39 @@ use lithium\storage\Session;
 use app\models\Item;
 use MongoDate;
 
+/**
+* The Cart Class.
+*
+* Controls all the model methods needed to interact with the shopping cart.
+* The cart docuemnt has a one-to-one relationship with an item. What ties all the carts together
+* is the session of the user. They see all their carts together based on their user_id and session.
+* When the order is placed all the active carts are embedded into the Order Document as an array.
+* Carts have the following document structure in Mongo:
+* {{{
+*	"_id" : ObjectId("4d6dda33538926843a0026ad"),
+*	"category" : "Room Decor   ",
+*	"color" : "White",
+*	"created" : "Wed Mar 02 2011 00:48:35 GMT-0500 (EST)",
+*	"description" : "Baby Blanket Satin Stars",
+*	"discount_exempt" : false,
+*	"event" : [
+*		"4d6c216f12d4c9d022003c7b"
+*	],
+*	"expires" : "Wed Mar 02 2011 01:03:35 GMT-0500 (EST)",
+*	"item_id" : "4d6d1ae75389264724000aaa",
+*	"primary_image" : "4d6d5b2753892691130079f4",
+*	"product_weight" : 1,
+*	"quantity" : 1,
+*	"sale_retail" : 8.45,
+*	"session" : "p6cdij91661kvm6v95numaqe26",
+*	"size" : "no size",
+*	"url" : "baby-blanket-satin-stars-white",
+*	"user" : "4cacb5efce64e5a875220a00",
+*	"vendor_style" : "A22628H"
+*
+* }}}
+* @see app/models/Order::process()
+*/
 class Cart extends \lithium\data\Model {
 
 	const TAX_RATE = 0.08875;
@@ -17,6 +50,10 @@ class Cart extends \lithium\data\Model {
 
 	public $validates = array();
 
+	/**
+	 * List of common times we use.
+	 * @var array
+	 */
 	protected $_dates = array(
 		'now' => 0,
 		'-1min' => -60,
@@ -27,6 +64,11 @@ class Cart extends \lithium\data\Model {
 		'15min' => 900
 	);
 
+	/**
+	 * List of NYC Zip Codes
+	 * @todo This should be ripped out when we hook in 3rd party tax check.
+	 * @var array
+	 */
 	protected $_nyczips = array(
 		'100',
 		'104',
@@ -37,16 +79,25 @@ class Cart extends \lithium\data\Model {
 		'11005'
 	);
 
+	/**
+	 * Returns MongoDB collection object
+	 */
 	public static function collection() {
 		return static::_connection()->connection->carts;
 	}
 
 	protected $_nonTaxableCategories = array('apparel');
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function dates($name) {
 	     return new MongoDate(time() + static::_object()->_dates[$name]);
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function addFields($data, array $options = array()) {
 
 		$data->expires = static::dates('15min');
@@ -57,6 +108,9 @@ class Cart extends \lithium\data\Model {
 		return static::_object()->save($data);
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function active($params = null, array $options = array()) {
 		$fields = (!empty($params['fields'])) ? $params['fields'] : null;
 		$time = (!empty($params['time'])) ? $params['time'] : 'now';
@@ -71,6 +125,9 @@ class Cart extends \lithium\data\Model {
 		));
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function itemCount() {
 		$cart = Cart::active(array('fields' => array('quantity')));
 		$cartCount = 0;
@@ -133,6 +190,9 @@ class Cart extends \lithium\data\Model {
 		return $cart->sale_retail * $tax;
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public function weight($cart) {
 		$item = Item::first($cart->item_id);
 		$weight = $item->shipping_weight ?: $item->product_weight;
@@ -140,20 +200,22 @@ class Cart extends \lithium\data\Model {
 		return ($weight ?: 1) * $cart->quantity;
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function shipping($carts, $address) {
-		/**
-		THIS WORKED, BUT WE'RE GOING TO A FLAT RATE
-		$result = floatval(Ups::estimate(array(
-			'weight' => array_sum($carts->weight()),
-			'product' => "GND",
-			'origin' => static::ORIGIN_ZIP,
-			'dest' => $address->zip,
-			'rate' => "RDP",
-			'container' => "CP",
-			'rescom' => "RES"
-		)));
-		$cost =  $result ?: 7.95;
-		**/
+
+		// THIS WORKED, BUT WE'RE GOING TO A FLAT RATE
+		// $result = floatval(Ups::estimate(array(
+		// 	'weight' => array_sum($carts->weight()),
+		// 	'product' => "GND",
+		// 	'origin' => static::ORIGIN_ZIP,
+		// 	'dest' => $address->zip,
+		// 	'rate' => "RDP",
+		// 	'container' => "CP",
+		// 	'rescom' => "RES"
+		// )));
+		// $cost =  $result ?: 7.95;
 		$cost = 7.95;
 		$cartCheck = $carts->data();
 
@@ -167,6 +229,9 @@ class Cart extends \lithium\data\Model {
 		return $cost;
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function overSizeShipping($cart){
 			$items= $cart->data();
 			$cost=0;
@@ -180,6 +245,9 @@ class Cart extends \lithium\data\Model {
 			return $cost;
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function checkCartItem($itemId, $size) {
 		return static::find('first', array(
 			'conditions' => array(
@@ -190,6 +258,9 @@ class Cart extends \lithium\data\Model {
 		)));
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function reserved($item_id, $size) {
 		$total = 0;
 		$reserved =  static::find('all', array(
@@ -208,6 +279,9 @@ class Cart extends \lithium\data\Model {
 		return $total;
 	}
 
+	/**
+	 * @todo Need documentation
+	 */
 	public static function increaseExpires() {
 		$user = Session::read('userLogin');
 		$conditions = array(
@@ -227,7 +301,9 @@ class Cart extends \lithium\data\Model {
 		return true;
 	}
 
-
+	/**
+	 * @todo Need documentation
+	 */
 	public static function check($quantity = null, $cart_id = null){
 		$cart = static::find('first', array(
 			'conditions' => array(
@@ -240,11 +316,11 @@ class Cart extends \lithium\data\Model {
 		)));
 		if ($item->details->{$cart->size} == 0) {
 			$check["status"] = false;
-			$check["errors"] = "Sorry we are sold out of this item.";
+			$check["errors"] = "Sorry we are sold out of the <b>$item->description</b>.";
 		}
 		if ($quantity > $item->details->{$cart->size}) {
 			$check["status"] = false;
-			$check["errors"] =  "Sorry you have requested more of this item than what is available.";
+			$check["errors"] =  "Sorry you have requested more of the <b>$item->description</b> than what is available.";
 		}
 		if (empty($check["errors"])){
 			$check["status"] = true;
