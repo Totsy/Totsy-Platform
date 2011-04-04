2aa9398d8d8fcd1f72d8436c467ada2a2bc753f4
Lawrenberg <lhanson@totsy.com>
2011-04-04 18:20:58 -0400

    adding li3 payments

---
+++ app/libraries/li3_payments/extensions/adapter/payments/authorize_net/templates/get_customer_profiles.xml.php
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?>
+<getCustomerProfileIdsRequest xmlns="<?=$gateways['schema']['profile']; ?>">
+	<?=$this->view()->render('template', compact('config'), array('template' => 'authentication')); ?>
+</getCustomerProfileIdsRequest>
---
+++ app/libraries/li3_payments/extensions/adapter/payments/authorize_net/templates/order.xml.php
@@ -0,0 +1,17 @@
+<?php /*
+<tax>
+	<amount></amount>
+	<name></name>
+	<description></description>
+</tax>
+<shipping>
+	<amount></amount>
+	<name></name>
+	<description></description>
+</shipping>
+<duty>
+	<amount></amount>
+	<name></name>
+	<description></description>
+</duty> */
+?>
