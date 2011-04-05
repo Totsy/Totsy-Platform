acb75f4fb0ba15ee5bde194e2660d869169cf789
fitzagard <fhagard@lightcube.us>
2011-04-05 14:59:34 -0400

    Fixing bug.

--- admin/controllers/ReportsController.php
+++ admin/controllers/ReportsController.php
@@ -13,8 +13,8 @@ use Mongo;
 use MongoCode;
 use MongoDate;
 use MongoRegex;
-use li3_flash_message\extensio32ns\storage\FlashMessage;
-use \lithium\data\Model;
+use li3_flash_message\extensions\storage\FlashMessage;
+use lithium\data\Model;
 
 /**
  * The Reports Controller is the core for all reporting functionality.
