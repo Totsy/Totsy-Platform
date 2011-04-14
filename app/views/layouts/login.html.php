d37e7e4223443190c029982455305e082f13a856
Lawrenberg <lhanson@totsy.com>
2011-04-14 12:56:55 -0400

    Updating metadata again

--- app/views/layouts/login.html.php
+++ app/views/layouts/login.html.php
@@ -6,6 +6,7 @@
 <head>
 	<?php echo $this->html->charset();?>
 	<title>Totsy, the private sale site for Moms</title>
+	<meta property="fb:app_id" content="181445585225391"/>
 	<meta property="og:site_name" content="Totsy"/>
     <meta property="og:description"
           content="Totsy has this super cool find available now and so much more for kids and moms! Score the best brands for your family at up to 90% off. Tons of new sales open every day. Membership is FREE, fast and easy. Start saving now!"/>
