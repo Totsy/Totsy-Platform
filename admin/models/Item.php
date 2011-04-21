53f278af867c4e29a7637086fe1b7bc8cc46d589
troyer <troyer@totsy.com>
2011-04-21 10:01:36 -0400

    - modifying md5 hash position for sku

--- admin/models/Item.php
+++ admin/models/Item.php
@@ -105,11 +105,13 @@ class Item extends \lithium\data\Model {
 			'color' => $color
 		);
 		foreach ($params as $key => $param) {
-			if ($key != 'vendor') {
-				$sku[] = strtoupper(substr(md5($param), 0, 3));
-			} else {
+			if ($key == 'vendor') {
 				$param = preg_replace('/[^(\x20-\x7F)]*/','', $param);
 				$sku[] = strtoupper(substr($param, 0, 3));
+			} else if ($key == 'style') {
+				$sku[] = strtoupper(substr(md5($param), 4, 6));
+			} else {
+				$sku[] = strtoupper(substr(md5($param), 0, 3));
 			}
 		}
 		return preg_replace('/\s*/m', '', implode('-', $sku));
