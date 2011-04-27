8ec7990612183ce64027e00456a065d3fa1bc4ae
Lawrenberg <lhanson@totsy.com>
2011-04-27 15:21:14 -0400

    Changing upsell language

--- app/views/cart/upsell.html.php
+++ app/views/cart/upsell.html.php
@@ -1,6 +1,6 @@
 <img style="float:left" src="/img/Disneycover.jpg" width="150" style="float: left; border: 18px solid rgb(221, 221, 221);"/>
 <div class="disney">
-   <p style="font-size: 18px; float: left; padding: 0px 0px 0px 10px; width: 300px;"> Spend an additional <strong style="color:#5CBC5C"> $<?=number_format($total_left,2);?> </strong> and receive a years subscription to Disney Family Fun.</p>
+   <p style="font-size: 18px; float: left; padding: 0px 0px 0px 10px; width: 300px;"> Spend an additional <strong style="color:#5CBC5C"> $<?=number_format($total_left,2);?> </strong> we'll <strong>include<strong> a bonus one-year subscription to Disney FamilyFun!</p>
 </div>
 <div class="clear"></div><br/>
 <?=$this->html->link('Continue Shopping', $url, array('class' => 'button fr', 'style' => 'margin-right:10px;')); ?>
