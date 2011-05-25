042906c251efcd32baa1a3658ac57958472a126d
Jrek <slv.kosh@gmail.com>
2011-05-25 20:53:53 +0000

    whole bock changed

--- admin/extensions/helper/Shipment.php
+++ admin/extensions/helper/Shipment.php
@@ -18,27 +18,26 @@ namespace admin\extensions\helper;
 class Shipment extends \lithium\template\Helper {
 
 	public function link($number, array $options = array()) {
-
 		$type = $options['type'];
-		$link = null;
+		$upsBase = 'http://wwwapps.ups.com/WebTracking/processInputRequest?';
+		$upsDetails = 'sort_by=3D=status&tracknums_displayed=3D1&TypeOfInquiryNumber=3DT&loc=3D=en_US&InquiryNumber1=';
+		$upsDetails2 = '&track.x=3D0&track.y==3D0';
+		$html = "";
 		switch ($type) {
 			case 'UPS':
 			case 'NEXTDAY':
 			case 'UPSGROUND':
 			case 'SP':
-			case 'UPS':
-			case 'ups':
-				
 				if (strlen($number) == 22) {
 					$html = "<a href='http://www.ups-mi.net/packageID/PackageID.aspx?PID=$number' target='_blank' title='Tracking Info'>$number</a>";
 				} else {
 					$html = "<a href=".$upsBase.$upsDetails.$number.$upsDetails2." target='_blank' title='Tracking Info'>$number</a>";
 				}
-				
 				break;
 			default:
 				break;
 		}
-		return $link;
+		return $html;
+		
 	}
 }
