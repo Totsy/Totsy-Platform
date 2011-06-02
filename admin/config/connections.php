cab2fa5c2da8b3251f17d2b887854b70e001bc60
troyer <troyer@totsy.com>
2011-06-02 11:22:57 -0400

    - ' db -> totsy'

--- admin/config/connections.php
+++ admin/config/connections.php
@@ -22,7 +22,7 @@ use \lithium\data\Connections;
 			'host' => array('test')),
 		'development' => array(
 			'type' =>  'MongoDb',
-			'database' => array('totsy','totsy_dev'),
+			'database' => 'totsy',
 			'host' => 'localhost'),
 		'local' => array(
 			'type' =>  'MongoDb',
