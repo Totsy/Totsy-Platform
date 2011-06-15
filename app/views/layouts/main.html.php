e48104a6d1ea2a99baf9cd32b5b32db5f492c6d0
Eric The Robot <erictherobot@gmail.com>
2011-06-14 23:59:56 -0400

    adding UPS logo

--- app/views/layouts/main.html.php
+++ app/views/layouts/main.html.php
@@ -109,14 +109,16 @@
 		</ul>
 		<span id="copyright" style="padding-top:4px;" class="fl">&copy;2011 Totsy.com. All Rights Reserved.</span>
 	</div>
-	<div class="container_16 clear">
+	<div class="container_16 clear" style="margin-top:50px;">
+	<div class="prefix_7">
 	<!-- begin thawte seal -->
     <div id="thawteseal" title="Click to Verify - This site chose Thawte SSL for secure e-commerce and confidential communications.">
     <div style="float: left!important; width:100px; display:block;"><script type="text/javascript" src="https://seal.thawte.com/getthawteseal?host_name=www.totsy.com&amp;size=L&amp;lang=en"></script></div>
-
+    <div style="float: left!important; width:60px; display:block;"><a href="http://www.ups.com/" target="_blank" title="Shipping provided by UPS"><img src="/img/ups_logo.png" /></a></div>
     <div class="AuthorizeNetSeal" style="float: left!important; width:100px; display:block;"> <script type="text/javascript" language="javascript">var ANS_customer_id="98c2dcdf-499f-415d-9743-ca19c7d4381d";</script> <script type="text/javascript" language="javascript" src="//verify.authorize.net/anetseal/seal.js" ></script></div>
     </div>
     <!-- end thawte seal -->
+    </div>
     </div>
 	<script type="text/javascript">
 	$.base = '<?=rtrim(Router::match("/", $this->_request)); ?>';
