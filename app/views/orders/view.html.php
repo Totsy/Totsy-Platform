1395a3b72aa601f3cac3919d34807bb3efba4d71
erictherobot <erictherobot@gmail.com>
2011-06-17 00:41:28 -0400

    removing inline style for width in credit card form

--- app/views/orders/view.html.php
+++ app/views/orders/view.html.php
@@ -342,7 +342,7 @@
 												<?php endif ?>
 												Sales Tax: <span class="fr">$<?=number_format($order->tax,2); ?></span>
 												<br>
-												Shipping:
+												Shipping: 
 												<?php if ( array_key_exists('overSizeHandling', $order->data()) && $order->overSizeHandling !=0): ?>
                                                     <br>
                                                     Oversize Shipping: <span class="fr">$<?=number_format($order->overSizeHandling,2); ?></span>
