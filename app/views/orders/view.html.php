6438bffc657aa449f4f642f278639ee90e2ea48d
erictherobot <erictherobot@gmail.com>
2011-06-17 00:33:11 -0400

    redoing the layout so it makes better sense in the order confirmation view

--- app/views/orders/view.html.php
+++ app/views/orders/view.html.php
@@ -7,28 +7,37 @@
 
 ?>
 	<div class="grid_16">
-	<h2 class="page-title gray">Order Items</h2>
+	<h2 class="page-title gray">My Orders</h2>
 	<hr />
 </div>
-	<div class="grid_4">
-		<div class="roundy grey_inside">
+
+<div class="grid_4">
+	<div class="roundy grey_inside">
+		<h3 class="gray">My Account</h3>
+		<hr />
 		<ul class="menu main-nav">
-			<li class="firstitem17"><a href="/account" title="Account Dashboard"><span>Account Dashboard</span></a></li>
-			<li class="item18"><a href="/account/info" title="Account Information"><span>Account Information</span></a></li>
-			<li class="item18"><a href="/account/password" title="Change Password"><span>Change Password</span></a></li>
-			<li class="item19"><a href="/addresses" title="Address Book"><span>Address Book</span></a></li>
-			<li class="item20 active"><a href="/orders" title="My Orders"><span>My Orders</span></a></li>
-			<li class="item20"><a href="/Credits/view" title="My Credits"><span>My Credits</span></a></li>
-			<li class="lastitem23"><a href="/Users/invite" title="My Invitations"><span>My Invitations</span></a></li>
-			<br />
-			<h3 style="color:#999;">Need Help?</h3>
-			<hr />
-			<li class="first item18"><a href="/tickets/add" title="Contact Us"><span>Help Desk</span></a></li>
-			<li class="first item19"><a href="/pages/faq" title="Frequently Asked Questions"><span>FAQ's</span></a></li>
+		<li><a href="/account" title="Account Dashboard">Account Dashboard</a></li>
+		<li><a href="/account/info" title="Account Information">Account Information</a></li>
+		<li><a href="/account/password" title="Change Password">Change Password</a></li>
+		<li class="active"><a href="/addresses" title="Address Book">Address Book</a></li>
+		<li><a href="/orders" title="My Orders">My Orders</a></li>
+		<li><a href="/Credits/view" title="My Credits">My Credits</a></li>
+		<li><a href="/Users/invite" title="My Invitations">My Invitations</a></li>
 		</ul>
-		</div>
-		
-		<div class="roundy grey_inside">
+	</div>
+	<div class="clear"></div>
+	<div class="roundy grey_inside">
+		<h3 class="gray">Need Help?</h3>
+		<hr />
+		<ul class="menu main-nav">
+		    <li><a href="/tickets/add" title="Contact Us">Help Desk</a></li>
+			<li><a href="/pages/faq" title="Frequently Asked Questions">FAQ's</a></li>
+			<li><a href="/pages/privacy" title="Privacy Policy">Privacy Policy</a></li>
+			<li><a href="/pages/terms" title="Terms Of Use">Terms Of Use</a></li>
+		</ul>
+	</div>
+	<div class="clear"></div>
+	<div class="roundy grey_inside">
 		<?=$this->html->image('being_green/carbonzero.gif', array('style' => 'margin-right: 10px; margin-bottom:20px; float:left;'
 											)); ?>
 											<p>A tree was planted with your first order. It is watered with every additional order so it can grow big and strong to help our earth!
@@ -38,13 +47,16 @@
 												</p>
 
 		</div>
-	</div>
+</div>
+
 	
 	
 
 
 <?php if ($order): ?>
 <div class="grid_11 omega roundy grey_inside b_side">
+<h2 class="page-title gray">My Orders</h2>
+	<hr />
 		<table class="cart-table" cellspacing="0" cellpadding="0" border="0" width="695">
 				<tr>
 					<td colspan="4">
