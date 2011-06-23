0154db1d32242de0681930a9d7c6ec5b5daba410
Jrek <slv.kosh@gmail.com>
2011-06-23 16:28:13 -0400

    new mongo adapter

--- admin/config/connections.php
+++ admin/config/connections.php
@@ -15,19 +15,35 @@ use \lithium\data\Connections;
 		'production' => array(
 			'type' =>  'MongoDb',
 			'database' => 'totsy',
-			'host' => '172.20.15.38'),
+			'host' => '172.20.15.38',
+			'setSlaveOkay' => true,
+			'persistent' => 'foo',
+			'replicaSet' => true,
+			'adapter' => 'admin\extensions\adapter\mongo\Db'),
 		'test' => array(
 			'type' =>  'MongoDb',
 			'database' => 'totsy_test',
-			'host' => '172.20.15.42'),
+			'host' => '172.20.15.42',
+			'setSlaveOkay' => true,
+			'persistent' => 'foo',
+			'replicaSet' => true,
+			'adapter' => 'admin\extensions\adapter\mongo\Db'),
 		'development' => array(
 			'type' =>  'MongoDb',
-			'database' => 'totsy_dev',
-			'host' => 'localhost'),
+			'database' => 'totsy',
+			'host' => 'localhost',
+			'setSlaveOkay' => true,
+			'persistent' => 'foo',
+			'replicaSet' => true,
+			'adapter' => 'admin\extensions\adapter\mongo\Db'),
 		'local' => array(
 			'type' =>  'MongoDb',
 			'database' => 'totsy',
-			'host' => 'localhost')
+			'host' => 'localhost',
+			'setSlaveOkay' => true,
+			'persistent' => 'foo',
+			'replicaSet' => true,
+			'adapter' => 'admin\extensions\adapter\mongo\Db')
 	));
 
 ?>
