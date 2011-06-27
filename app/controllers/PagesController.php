d612a55d9224dcb61003c85ca7ddc88c5d79073f
eubiera <eubiera@totsy.com>
2011-06-27 00:11:48 -0400

    living social landing page for marketing

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -23,10 +23,10 @@ class PagesController extends BaseController {
 		}
 		if ($path[0] == 'blog') {
 			$this->redirect('http://totsyblog.blogspot.com', array('target' => '_blank'));
-		}elseif ($path[0] == 'btrendie' ){
+		}elseif ($path[0] == 'btrendie' || $path[0] == "living_social"){
 			$this->_render['layout'] = 'blank';
 		}
-		$allowed = array('terms', 'faq', 'contact', 'privacy', 'aboutus', 'btrendie', 'moms', 'testimonials', 'being_green', 'press','affiliates');
+		$allowed = array('terms', 'faq', 'contact', 'privacy', 'aboutus', 'btrendie', 'moms', 'testimonials', 'being_green', 'press','affiliates','living_social');
 		$userCheck = Session::read('userLogin');
 		if (empty($userCheck) && !in_array($path[0], $allowed)) {
 			$this->redirect('/');
