1c1c4534962e66bf91b44695d3ca669b55295a87
eubiera <eubiera@totsy.com>
2011-06-27 08:26:03 -0400

    tweaks to the shop now link. now working.

--- app/views/pages/living_social.html.php
+++ app/views/pages/living_social.html.php
@@ -35,7 +35,10 @@ Save up to 90% Off on top brands for babies, kids & moms!
 
 <div style="position:absolute; top:100px; left:700px; z-index: -1">
 <img src="/img/freeshipping.png">
-<a style="position:absolute; left:35px; top: 10px; color: #FFF; z-index : 1; font-size: 20px; font-family:arial; text-decoration:none" href="http://www.totsy.com/register">Shop Now!</a> 
+</div>
+
+<div>
+<a href="http://www.totsy.com/register" style="position:absolute; left:735px; top: 110px; color: #FFF; z-index : 1; font-size: 20px; font-family:arial; text-decoration:none">Shop Now!</a>
 </div>
 
 <div style="top:250px; position:absolute; margin-left: 656px; color: #FFF; font-size: 18px; font-family:arial; width: 280px">
@@ -53,7 +56,10 @@ Save up to 90% Off on top brands for babies, kids & moms!
 
 </div>
 
-<div style="top:600px; left: 750px; position:absolute; z-index:-1; padding-bottom:150px">
+
+<div style="top:620px; left: 700px; position:absolute; z-index:-1; color: #FFF; font-size: 25px; font-family:arial; line-height:35px">Buy now on</div>
+<div style="top:600px; left: 830px; position:absolute; z-index:-1; padding-bottom:150px">
+
 <a href="http://livingsocial.com"><img src="/img/living_social_logo.png" border=0></a>
 </div>
 
