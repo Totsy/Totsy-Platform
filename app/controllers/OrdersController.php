9798dc71428d7e4a0371ef358821bb785252718a
Jrek <slv.kosh@gmail.com>
2011-06-29 17:54:58 -0400

    avatax

--- app/controllers/OrdersController.php
+++ app/controllers/OrdersController.php
@@ -20,6 +20,7 @@ use lithium\util\Validator;
 use MongoDate;
 use MongoId;
 use li3_silverpop\extensions\Silverpop;
+use app\extensions\AvaTax;
 
 /**
  * The Orders Controller
@@ -248,12 +249,15 @@ class OrdersController extends BaseController {
 				)));
 			}
 		}
-
+		$tax = 0;
 		if ($shippingAddr) {
-			$tax = array_sum($cart->tax($shippingAddr));
+			//$tax = array_sum($cart->tax($shippingAddr));
 			$shippingCost = Cart::shipping($cart, $shippingAddr);
 			$overShippingCost = Cart::overSizeShipping($cart);
-			$tax = $tax ? $tax + (($overShippingCost + $shippingCost) * Cart::TAX_RATE) : 0;
+			//$tax = $tax ? $tax + (($overShippingCost + $shippingCost) * Cart::TAX_RATE) : 0;
+			$tax=  AvaTax::getTax( compact('cartByEvent', 'billingAddr', 'shippingAddr', 'shippingCost', 'overShippingCost') );
+		} else {
+			$tax=  AvaTax::getTax( compact('cartByEvent', 'billingAddr', 'shippingAddr') );	
 		}
 		/**
 		*	Handling services the user may be eligible for
@@ -417,6 +421,10 @@ class OrdersController extends BaseController {
 					$order->promo_code = $orderPromo->code;
 				}
 			}
+			AvaTax::postTax( compact('order','cartByEvent', 'billingAddr', 'shippingAddr', 'shippingCost', 'overShippingCost') );
+			
+			$order->tax = $tax;
+			$order->avatax = true;
 			$order->ship_date = new MongoDate(Cart::shipDate($order));
 			$order->save();
 			Cart::remove(array('session' => Session::key('default')));
