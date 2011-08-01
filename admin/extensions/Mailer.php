b56455e7328edd6fca90c160f1d7557ba540fe83
Deepen Shah <deepen@gmail.com>
2011-08-01 11:20:33 -0400

    Reverting Mailer and Bootstrap

--- admin/extensions/Mailer.php
+++ admin/extensions/Mailer.php
@@ -9,14 +9,10 @@ use lithium\action\Request;
 
 class Mailer {
 	public static function send($template, $email, $vars = array(), $options = array(), $schedule_time = null) {
-		$email = "dshah@totsy.com";
-
 		Sailthru::send($template, $email, $vars, $options, $schedule_time);
 	}
 
 	public static function optOut($email, $vars = array(), $list = array(), $templates = array() , $optout="none") {
-		$email = "dshah@totsy.com";
-	
 		Sailthru::setEmail($email, $vars, $list, $templates);
 	}
 }
