e5c4b54560b110897734c14abb39a9676da19b9b
Tom Maiaroto <tom.maiaroto@gmail.com>
2011-08-01 10:36:09 -0700

    routing fixes

--- app/config/routes.php
+++ app/config/routes.php
@@ -71,44 +71,4 @@ Router::connect('/checkout/shipping', 'Orders::shipping');
 Router::connect('/checkout/payment', 'Orders::payment');
 Router::connect('/checkout/review', 'Orders::review');
 
-Router::connect('/', 'Events::index');
-Router::connect('/sales/{:args}', 'Events::index');
-Router::connect('/{:action:login|logout}', array('controller' => 'users'));
-Router::connect('/addresses', 'Addresses::view');
-Router::connect('/addresses/edit{:args}', 'Addresses::edit');
-Router::connect('/account/info', 'Users::info');
-Router::connect('/account/credits', 'Credits::view');
-Router::connect('/account/invites', 'Users::invite');
-Router::connect('/account/password', 'Users::password');
-Router::connect('/upgrade', 'Users::upgrade');
-Router::connect('/events/view/{:item:[a-z0-9\-]+}', 'Events::view');
-Router::connect('/welcome', 'Users::affiliate');
-Router::connect('/sale/{:event:[a-z0-9\-]+}', 'Events::view');
-Router::connect('/sale/{:event:[a-z0-9\-]+}/{:item:[a-z0-9\-]+}', 'Items::view');
-/**
-* Taking this route out, as the menu helper is not ready
-* for custom routes.
-*/
-//Router::connect('/help', 'Tickets::add');
-
-/**
- * Wire up the "search" for the 404 page, that attempts to figure out what you wanted.
- */
-Router::connect('/search/{:search}', 'Search::view');
-
-/**
- * Connect the testing routes.
- */
-if (!Environment::is('production')) {
-	Router::connect('/test/{:args}', array('controller' => '\lithium\test\Controller'));
-	Router::connect('/test', array('controller' => '\lithium\test\Controller'));
-}
-
-/**
- * Finally, connect the default routes.
- */
-Router::connect('/{:controller}/{:action}/{:id:[0-9a-f]{24}}.{:type}', array('id' => null));
-Router::connect('/{:controller}/{:action}/{:id:[0-9a-f]{24}}');
-Router::connect('/{:controller}/{:action}/{:args}');
-
 ?>
