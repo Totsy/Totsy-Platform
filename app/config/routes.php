f12373057d984717cb596d79ccbce95ee3a453e0
Tom Maiaroto <tom.maiaroto@gmail.com>
2011-08-02 07:56:59 -0700

    make it work fixes

--- app/config/routes.php
+++ app/config/routes.php
@@ -62,6 +62,7 @@ Router::connect('/unsubcentral/del', array('controller' => 'unsubcentral', 'acti
 Router::connect('/login', 'Users::login');
 Router::connect('/register', 'Users::register');
 Router::connect('/register/facebook', 'Users::fbregister');
+Router::connect('/register/{:locale}', 'Users::register');
 Router::connect('/momoftheweek', 'MomOfTheWeeks::index');
 Router::connect('/momoftheweek/fbml', 'MomOfTheWeeks::fbml');
 Router::connect('/surveys', 'Surveys::index');
