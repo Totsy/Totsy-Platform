fd94a97480e3f39191df28705b51c25ebb4fea0b
David Persson <davidpersson@gmx.de>
2011-08-03 19:09:30 +0200

    Initial implementation for structured GridFS based DAV VFS.

--- admin/controllers/FilesController.php
+++ admin/controllers/FilesController.php
@@ -7,10 +7,14 @@ use lithium\core\Environment;
 use admin\models\File;
 use admin\extensions\sabre\dav\auth\backend\Lithium as Sabre_DAV_Auth_Backend_Lithium;
 use Sabre_DAV_Server;
-use Sabre_DAV_FS_Directory;
+// use Sabre_DAV_FS_Directory;
+// use admin\extensions\sabre\dav\TotsyTree;
+use admin\extensions\sabre\dav\StructuredDirectory;
+use admin\extensions\sabre\dav\FlatDirectory;
 use Sabre_DAV_Locks_Backend_File;
 use Sabre_DAV_Locks_Plugin;
 use Sabre_DAV_TemporaryFileFilterPlugin;
+use Sabre_DAV_Auth_Plugin;
 
 /**
  * This controller works with SWFUpload to provide flash/javascript
@@ -53,12 +57,18 @@ class FilesController extends \lithium\action\Controller {
 
 		// @todo This is temporary and will be replaced by an implementation
 		//       storing files directly in GridFS.
-		$root = new Sabre_DAV_FS_Directory($resources . '/dav/share');
-
+		// $root = new Sabre_DAV_FS_Directory($resources . '/dav/share');
+		$root = array(
+			new FlatDirectory('pending'),
+			new FlatDirectory('failed'),
+			new StructuredDirectory('events'),
+			new StructuredDirectory('items')
+			// 'events' => new StructuredDirectory(array('model' => '\admin\models\Event')),
+		);
 		$server = new Sabre_DAV_Server($root);
 
 		$server->debugExceptions = !Environment::is('production');
-		$server->setBaseUri('/dav');
+		$server->setBaseUri('/files/dav');
 
 		$backend = new Sabre_DAV_Locks_Backend_File($resources . '/dav/locks.dat');
 		$plugin = new Sabre_DAV_Locks_Plugin($backend);
