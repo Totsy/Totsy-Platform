60d90e91441e1134fa36a94593906b9f735f8a12
David Persson <davidpersson@gmx.de>
2011-08-03 12:17:09 +0200

    Moving `Dav::handle()` to Files::dav(). Updating routes.

--- admin/controllers/FilesController.php
+++ admin/controllers/FilesController.php
@@ -2,7 +2,15 @@
 
 namespace admin\controllers;
 
+use lithium\core\Libraries;
+use lithium\core\Environment;
 use admin\models\File;
+use admin\extensions\dav\Auth;
+use Sabre_DAV_Server;
+use Sabre_DAV_FS_Directory;
+use Sabre_DAV_Locks_Backend_File;
+use Sabre_DAV_Locks_Plugin;
+use Sabre_DAV_TemporaryFileFilterPlugin;
 
 /**
  * This controller works with SWFUpload to provide flash/javascript
@@ -38,6 +46,33 @@ class FilesController extends \lithium\action\Controller {
 			$tag = $this->tag;
 		}
 		return compact('id', 'fileName', 'tag');
+	}
+
+	public function dav() {
+		$resources = Libraries::get('admin', 'resources');
+
+		// @todo This is temporary and will be replaced by an implementation
+		//       storing files directly in GridFS.
+		$root = new Sabre_DAV_FS_Directory($resources . '/dav/share');
+
+		$server = new Sabre_DAV_Server($root);
+
+		$server->debugExceptions = !Environment::is('production');
+		$server->setBaseUri('/dav');
+
+		$backend = new Sabre_DAV_Locks_Backend_File($resources . '/dav/locks.dat');
+		$plugin = new Sabre_DAV_Locks_Plugin($backend);
+		$server->addPlugin($plugin);
+
+		$plugin = new Sabre_DAV_TemporaryFileFilterPlugin($resources . '/dav/temporary');
+		$server->addPlugin($plugin);
+
+		$backend = new Auth();
+		$plugin = new Sabre_DAV_Auth_Plugin($backend, 'Totsy DAV'); /* 2nd arg is the realm. */
+		$server->addPlugin($plugin);
+
+		$server->exec();
+		exit;
 	}
 
 	/**
