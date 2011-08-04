b9d2cbe2956428ba7f33792c5b30f99e0fda7a7e
widrototsy <jwidro@totsy.com>
2011-08-04 17:05:16 -0300

    removed `

--- admin/extensions/command/OrderExport.php
+++ admin/extensions/command/OrderExport.php
@@ -34,27 +34,6 @@ use admin\extensions\Mailer;
  * @see admin/extensions/command/Exchanger
  * @see admin/controllers/QueueController
  */
-
-
-//sets maxlifetime to 5 hours
-ini_set("session.gc_maxlifetime", "18000");
-
-//check the maxlifetime
-//echo ini_get("session.gc_maxlifetime");
-
-//specifies session path to avoid default maxlifetime value of 24 mins to override
-session_save_path('/www/admin/resources/totsy/tmp');
-
-//to check the session path 
-//echo session_save_path();
-
-ini_set('session.gc_probability', 1);
-
-//check the gc_probability
-//echo ini_get("session.gc_probability");
-
-
-
 class OrderExport extends Base {
 
 	/**
@@ -159,7 +138,7 @@ class OrderExport extends Base {
 	/**
 	 * Main method for exporting Order and PO files.
 	 *
-	 * The `run` method will query the pending event transactions
+	 * The run method will query the pending event transactions
 	 * that have not yet been processed. This queuing system will be managed
 	 * from the admin dashboard.
 	 *
@@ -422,7 +401,7 @@ class OrderExport extends Base {
 							}
 							if ($this->test != 'true') {
 								$processedOrder = ProcessedOrder::connection()->connection->{'orders.processed'};
-						`		$processedOrder->save($orderFile[$inc] + $this->batchId);
+								$processedOrder->save($orderFile[$inc] + $this->batchId);
 							}
 							$this->log("Adding order $order[_id] to $handle");
 							fputcsv($fp, $orderFile[$inc], chr(9));
