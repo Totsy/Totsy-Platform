2f4cd13318c8bf605b413b349978a6ff4f1ef758
David Persson <davidpersson@gmx.de>
2011-08-04 14:27:46 +0200

    Replacing StructuredDirectory with Model/Year/Month/ItemDirectory.

---
+++ admin/extensions/sabre/dav/YearDirectory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace admin\extensions\sabre\dav;
+
+use admin\extensions\sabre\dav\MonthDirectory;
+
+class YearDirectory extends \admin\extensions\sabre\dav\Directory {
+
+	public function getChild($name) {
+		return new MonthDirectory(array('value' => $name, 'parent' => $this));
+	}
+
+	public function getChildren() {
+		$children = array();
+
+		foreach ($this->_range() as $month) {
+			$children[] = new MonthDirectory(array('value' => $month, 'parent' => $this));
+		}
+		return $children;
+	}
+
+	public function childExists($name) {
+		return in_array($name, $this->_range());
+	}
+
+	protected function _range() {
+		return range(1, 12);
+	}
+}
+
+?>
