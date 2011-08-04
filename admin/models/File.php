cf4de950f9727fa4bec1ff869f5ede9087dba2e1
David Persson <davidpersson@gmx.de>
2011-08-04 17:45:40 +0200

    Adding dupe, used and mimeType methods to File model.

--- admin/models/File.php
+++ admin/models/File.php
@@ -4,6 +4,8 @@ namespace admin\models;
 
 use lithium\data\Connections;
 
+use admin\models\Event;
+use admin\models\Item;
 
 class File extends \lithium\data\Model {
 
@@ -12,7 +14,42 @@ class File extends \lithium\data\Model {
 	public static function getGridFS() {
 		return static::_connection()->connection->getGridFS();
 	}
-}
 
+	public static function dupe($data) {
+		rewind($data);
+
+		$context = hash_init('md5');
+		hash_update_stream($context, $data);
+		$hash = hash_final($context);
+
+		return File::first(array(
+			'conditions' => array('md5' => $hash)
+		));
+	}
+
+	public static function used($id) {
+		$count  = Event::count(array(
+			'conditions' => array(
+				'images' => $id
+			)
+		));
+		$count += Item::count(array(
+			'conditions' => array(
+				'images' => $id
+			)
+		));
+		return $count;
+	}
+
+	public static mimeType($data) {
+		$context = finfo_open(FILEINFO_NONE);
+
+		rewind($data);
+		$result = finfo_buffer($context, $data);
+
+		finfo_close($context);
+		return $result;
+	}
+}
 
 ?>
