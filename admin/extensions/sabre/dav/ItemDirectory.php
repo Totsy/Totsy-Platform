43062bda800d5ffc7bb27742adf0fd8546f8acf2
David Persson <davidpersson@gmx.de>
2011-08-04 17:43:47 +0200

    Fully implementing ItemDirectory.

--- admin/extensions/sabre/dav/ItemDirectory.php
+++ admin/extensions/sabre/dav/ItemDirectory.php
@@ -8,27 +8,42 @@ use admin\models\File;
 class ItemDirectory extends \admin\extensions\sabre\dav\Directory {
 
 	public function getChild($name) {
-		// return new ItemFile(array('value' => $name, 'parent' => $this));
+		$this->_model();
+		$data = $model::find('first', array(
+			'conditions' => $this->_conditions()
+		));
+		return new ItemFile(array('value' => $data->images[$name], 'parent' => $this));
 	}
 
 	public function getChildren() {
-		/*
-		$data = ::find('all', array(
-			'conditions' => array(
-				'url' => $this->getValue()
-			)
+		$this->_model();
+		$data = $model::find('first', array(
+			'conditions' => $this->_conditions()
 		));
 
 		$children = array();
-		foreach ($data as $item) {
-			$children[] = new ItemDirectory(array('value' => $item->url, 'parent' => $this));
+		foreach ($data->images as $name => $id) {
+			$children[] = new ItemFile(array('value' => $name, 'parent' => $this));
 		}
 		return $children;
-		*/
 	}
 
 	public function childExists($name) {
-		return false;
+		$this->_model();
+		$data = $model::find('first', array(
+			'conditions' => $this->_conditions()
+		));
+		return isset($data->images[$name]);
+	}
+
+	protected function _conditions() {
+		return array(
+			'url' => $this->getValue()
+		);
+	}
+
+	protected function _model() {
+		return $this->getParent()->getParent()->getParent()->getValue();
 	}
 }
 
