410e9c7f8291f24067f39ba30c593da75d4d3bb1
David Persson <davidpersson@gmx.de>
2011-08-08 15:24:08 +0200

    Moving auth bootstrap into its own file.

--- admin/config/bootstrap.php
+++ admin/config/bootstrap.php
@@ -57,7 +57,7 @@ require __DIR__ . '/bootstrap/auth.php';
 
 /**
  * This file contains configuration for session (and/or cookie) storage, and
- * user or web service * authentication.
+ * user or web service * authentication..
  */
 require __DIR__ . '/bootstrap/session.php';
 
