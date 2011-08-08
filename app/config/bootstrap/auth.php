113e1e051227d31f7b7d1263a7b8031088906b8a
David Persson <davidpersson@gmx.de>
2011-08-08 15:32:49 +0200

    Refactoring auth into filter and bootstrap file.
    Conflicts:
    
    	app/config/bootstrap.php
    	app/config/routes.php

---
+++ app/config/bootstrap/auth.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Configure Authentication and Access Control. Request are checked
+ * against the session based form adapter.
+ */
+use lithium\security\Auth;
+use lithium\action\Dispatcher;
+use lithium\action\Response;
+
+Auth::config(array(
+	'userLogin' => array(
+		'model' => 'User',
+		'adapter' => 'Form',
+		'fields' => array('email', 'password')
+	)
+));
+
+Dispatcher::applyFilter('_call', function($self, $params, $chain) {
+	$skip = array('login', 'logout', 'register');
+
+	$granted = in_array($params['request']->url, $skip);
+	$granted = $granted || Auth::check('userLogin', $params['request']);
+
+	if (!$granted) {
+		/* Redirect all non-authenticated users to the register page. */
+		return new Response(array('location' => 'Users::register'));
+	}
+	return $chain->next($self, $params, $chain);
+});
+
+?>
