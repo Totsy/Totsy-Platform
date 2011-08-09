02c1abfec9cb272e4382b036bbdc3687c80fa8a3
David Persson <davidpersson@gmx.de>
2011-08-09 14:33:16 +0200

    Adding pending action and view.

---
+++ admin/views/files/pending.html.php
@@ -0,0 +1,24 @@
+<?php if ($files): ?>
+	<ul>
+	<?php foreach($files as $file): ?>
+		<li>
+			<?=$file->name ?: $file->_id; ?>
+			<?php
+				$meta = array();
+
+				if ($file->created_date) {
+					$meta[] = date('m/d/y', $file->created_date->sec);
+				}
+				$meta[] = sprintf('%.2f MB', $file->file->getSize() / MEGABYTE);
+
+				if ($file->mime_type) {
+					$meta[] = $file->mime_type;
+				}
+			?>
+			(<?=implode(', ', $meta) ?>)
+		</li>
+	<?php endforeach; ?>
+	</ul>
+<?php else: ?>
+	<div class="none-available">No pending files available.</div>
+<?php endif; ?>
