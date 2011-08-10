9c53658c2e6f8c20a53766651122cb931cf1a680
David Persson <davidpersson@gmx.de>
2011-08-10 19:00:06 +0200

    Wiring up _items sub directory.

--- admin/extensions/sabre/dav/EventDirectory.php
+++ admin/extensions/sabre/dav/EventDirectory.php
@@ -3,12 +3,16 @@
 namespace admin\extensions\sabre\dav;
 
 use admin\extensions\sabre\dav\EventImageDirectory;
+use admin\extensions\sabre\dav\ItemsDirectory;
 use admin\models\EventImage;
 use Sabre_DAV_Exception_FileNotFound;
 
 class EventDirectory extends \admin\extensions\sabre\dav\GenericDirectory {
 
 	public function getChild($name) {
+		if ($name == '_items') {
+			return new ItemsDirectory(array('parent' => $this));
+		}
 		if (!isset(EventImage::$types[$name])) {
 			throw new Sabre_DAV_Exception_FileNotFound("File `{$name}` not found,");
 		}
@@ -20,11 +24,13 @@ class EventDirectory extends \admin\extensions\sabre\dav\GenericDirectory {
 		foreach (EventImage::$types as $name => $type) {
 			$children[] = new EventImageDirectory(array('value' => $name, 'parent' => $this));
 		}
+		$children[] = new ItemsDirectory(array('parent' => $this));
+
 		return $children;
 	}
 
 	public function childExists($name) {
-		return isset(EventImage::$types[$name]);
+		return isset(EventImage::$types[$name]) || $name == '_items';
 	}
 }
 
