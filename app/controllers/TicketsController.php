c99fa3faa1219401048c93307e154ba0c5a33b74
Jrek <slv.kosh@gmail.com>
2011-08-10 12:05:41 -0400

    sailthru behalf email and replay-to added for tickets

--- app/controllers/TicketsController.php
+++ app/controllers/TicketsController.php
@@ -32,9 +32,23 @@ class TicketsController extends BaseController {
 		$agent = array('user_agent' => $this->request->env('HTTP_USER_AGENT'));
 		$args = array('issue' => $this->request->data) + array('user' => $user) + $agent;
 		if (($this->request->data) && $ticket->save($args)) {
-			$email = $list[$args['issue']['issue_type']]; 
-			Mailer::send('Tickets', $email, $args);
-			//$this->redirect('tickets/sent');
+			$email = $list[$args['issue']['issue_type']];
+			$options = array();
+			
+			if (is_object($user)){
+				if (isset($user->email) && !empty($user->email)){ 
+					$options['replyto'] = $options['behalf_email'] = $user->email;
+				} else if (isset($user->confirmemail) && !empty($user->confirmemail)){
+					$options['replyto'] = $options['behalf_email'] = $user->confirmemail;
+				}
+			} else if (is_array($user)){
+				if (array_key_exists('email',$user) && !empty($user['email'])){
+					$options['replyto'] = $options['behalf_email'] = $user['email'];
+				} else if (array_key_exists('confirmemail',$user) && !empty($user['confirmemail'])){
+					$options['replyto'] = $options['behalf_email'] = $user['confirmemail'];
+				} 
+			}
+			Mailer::send('Tickets', $email, $args, $options);
 			$this->_render['template'] = 'sent';
 		}
 
