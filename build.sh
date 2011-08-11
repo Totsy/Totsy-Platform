12f1616e1f369e189f478d983efe8228b6a26345
David Persson <davidpersson@gmx.de>
2011-08-11 12:46:17 +0200

    Adding admin test runs to run-tests.

--- build.sh
+++ build.sh
@@ -118,6 +118,14 @@ case $COMMAND in
 		echo "Running (admin) Lithium unit tests..."
 		cd $PROJECT_DIR/admin
 		libraries/lithium/console/li3 --env=test test libraries/lithium/tests/cases
+		echo
+
+		echo "Running admin tests..."
+		cd $PROJECT_DIR/admin
+		libraries/lithium/console/li3 test tests/cases/models
+		# libraries/lithium/console/li3 test tests/cases/controllers
+		# libraries/lithium/console/li3 test tests/cases/extensions
+		echo
 
 		echo "Running SabreDAV tests..."
 		cd $PROJECT_DIR/admin/libraries/_source/sabredav/tests
@@ -126,6 +134,7 @@ case $COMMAND in
 		echo "Running Imagine tests..."
 		cd $PROJECT_DIR/admin/libraries/_source/Imagine/tests
 		phpunit
+		echo
 		;;
 
 	optimize-repo)
