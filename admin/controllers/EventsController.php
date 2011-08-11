e63946f1a404c177be2b3dd347f7954cb227e687
David Persson <davidpersson@gmx.de>
2011-08-11 12:50:49 +0200

    Guard against cases where an event is created without uploading images at the same time.

--- admin/controllers/EventsController.php
+++ admin/controllers/EventsController.php
@@ -635,6 +635,8 @@ class EventsController extends BaseController {
 	 * @return array
 	 */
 	protected function parseImages($imageRecord = null) {
+		$images = array();
+
 		foreach ($this->request->data as $key => $value) {
 			if (substr($key, -6) == '_image' ) {
 				$images["$key"] = $value;
