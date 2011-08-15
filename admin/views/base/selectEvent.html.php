36b8df72a9dd5272c14d585d92536367a2fea84f
troyer <troyer@totsy.com>
2011-08-15 17:27:39 -0400

    remove condition

--- admin/views/base/selectEvent.html.php
+++ admin/views/base/selectEvent.html.php
@@ -6,14 +6,10 @@
 	<h2 id="page-heading">Select Event for <?=$type?> Administration</h2>
 </div>
 <div class='clear'></div>
-<?php if(!empty($events)) :?>
 <div class="grid_16">
 	<?=$this->events->build($events, array('type' => $type))?>
 </div>
-<?php endif ?>
 <div class='clear'></div>
-
-
 <script type="text/javascript" charset="utf-8">
 	$(document).ready(function() {
 		$('#itemTable').dataTable();
