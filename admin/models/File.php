7dcbb9474a5294a21707e12742852736278227be
David Persson <davidpersson@gmx.de>
2011-08-16 19:15:00 +0200

    Move upgrade block into if condition.

--- admin/models/File.php
+++ admin/models/File.php
@@ -32,16 +32,6 @@ class File extends \lithium\data\Model {
 		/* Normalize $data */
 		$close = false;
 
-		/* Silently upgrade non seekable streams. */
-		$meta = stream_get_meta_data($stream);
-		if (!$meta['seekable']) {
-			$close = true;
-
-			$upgrade = fopen('php://temp', 'w+b');
-			stream_copy_to_stream($stream, $upgrade);
-
-			$stream = $upgrade;
-		}
 
 		if (is_string($data)) {
 			/* Only handles we open inside here are also closed here. */
@@ -55,6 +45,17 @@ class File extends \lithium\data\Model {
 			}
 		} else {
 			$handle = $data;
+
+			/* Silently upgrade non seekable streams. */
+			$meta = stream_get_meta_data($handle);
+			if (!$meta['seekable']) {
+				$close = true;
+
+				$upgrade = fopen('php://temp', 'w+b');
+				stream_copy_to_stream($handle, $upgrade);
+
+				$handle = $upgrade;
+			}
 		}
 
 		/* Dupe detection */
