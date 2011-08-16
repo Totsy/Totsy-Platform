51be06186e8bc8ec317931311021f704449938a7
David Persson <davidpersson@gmx.de>
2011-08-16 12:23:37 +0200

    Adding tests for resizeAndSave().

--- admin/tests/integration/models/ImageTest.php
+++ admin/tests/integration/models/ImageTest.php
@@ -3,7 +3,7 @@
 namespace admin\tests\integration\models;
 
 use admin\models\Image;
-use li3_fixtures\test\Fixture;
+use lithium\core\Libraries;
 
 class ImageTest extends \lithium\test\Integration {
 
@@ -18,6 +18,85 @@ class ImageTest extends \lithium\test\Integration {
 		$this->assertEqual($expected, $result);
 
 		$file->delete();
+	}
+
+	public function testResizeAndSaveWithBytes() {
+		$backup = Image::$types;
+
+		$file = LITHIUM_APP_PATH . '/tests/data/image_jpg.jpg';
+		$bytes = file_get_contents($file);
+
+		Image::$types = array(
+			'logo' => array(
+				'dimensions' => array(3, 5)
+			)
+		);
+
+		$file = Image::resizeAndSave('logo', $bytes, null, array('dedupe' => false));
+
+		$result = $file->dimensions->data();
+		$expected = array('width' => 3, 'height' => 5);
+		$this->assertEqual($expected, $result);
+
+		$file->delete();
+		Image::$types = $backup;
+	}
+
+	public function testResizeAndSaveWithFileupload() {
+		$backup = Image::$types;
+
+		$file = LITHIUM_APP_PATH . '/tests/data/image_jpg.jpg';
+		$path = Libraries::get('admin', 'resources');
+		$uploaded = "{$path}/tmp/tests/uploaded.jpg";
+		copy($file, $uploaded);
+
+		Image::$types = array(
+			'logo' => array(
+				'dimensions' => array(3, 5)
+			)
+		);
+
+		$data = array(
+			'name' => 'image_jpg.jpg',
+			'tmp_name' => $uploaded,
+			'mime_type' => 'image/jpeg'
+		);
+
+		$file = Image::resizeAndSave('logo', $data, null, array('dedupe' => false));
+
+		$result = $file->dimensions->data();
+		$expected = array('width' => 3, 'height' => 5);
+		$this->assertEqual($expected, $result);
+
+		$result = $file->name;
+		$expected = 'image_jpg.jpg';
+		$this->assertEqual($expected, $result);
+
+		$file->delete();
+		unlink($uploaded);
+		Image::$types = $backup;
+	}
+
+	public function testResizeAndSaveUpscaling() {
+		$backup = Image::$types;
+
+		$file = LITHIUM_APP_PATH . '/tests/data/image_jpg.jpg';
+		$bytes = file_get_contents($file);
+
+		Image::$types = array(
+			'logo' => array(
+				'dimensions' => array(500, 300)
+			)
+		);
+
+		$file = Image::resizeAndSave('logo', $bytes, null, array('dedupe' => false));
+
+		$result = $file->dimensions->data();
+		$expected = array('width' => 500, 'height' => 300);
+		$this->assertEqual($expected, $result);
+
+		$file->delete();
+		Image::$types = $backup;
 	}
 
 	public function testDimensionsBc() {
