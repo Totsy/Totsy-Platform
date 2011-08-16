bd313a85d3911dca028bf2c7d4a918feab958f54
David Persson <davidpersson@gmx.de>
2011-08-16 15:18:12 +0200

    Adding initial WebDAV functional test.

---
+++ admin/tests/mocks/extensions/sabre/dav/ResponseMock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace admin\tests\mocks\extensions\sabre\dav;
+
+class ResponseMock extends \Sabre_HTTP_Response {
+
+    public $headers = array();
+    public $status = '';
+    public $body = '';
+
+    public function setHeader($name, $value, $overwrite = true) {
+        $this->headers[$name] = $value;
+    }
+
+    public function sendStatus($code) {
+        $this->status = $this->getStatusMessage($code);
+    }
+
+    public function sendBody($body) {
+        $this->body = $body;
+    }
+}
+
+?>
