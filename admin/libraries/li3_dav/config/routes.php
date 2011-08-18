253ee9ebcb7433bf8cc4671672389c8b367d2951
David Persson <davidpersson@gmx.de>
2011-08-18 17:43:01 +0200

    Replacing digest auth with token auth.

--- admin/libraries/li3_dav/config/routes.php
+++ admin/libraries/li3_dav/config/routes.php
@@ -5,7 +5,10 @@ use lithium\net\http\Router;
 Router::connect('/files/dav', array(
 	'library' => 'li3_dav', 'controller' => 'files', 'action' => 'dav'
 ));
-Router::connect('/files/dav/{:file:.*}', array(
+Router::connect('/files/dav/{:token:[a-zA-Z0-9]+}', array(
+	'library' => 'li3_dav', 'controller' => 'files', 'action' => 'dav'
+));
+Router::connect('/files/dav/{:token:[a-zA-Z0-9]+}/{:file:.*}', array(
 	'library' => 'li3_dav', 'controller' => 'files', 'action' => 'dav'
 ));
 
