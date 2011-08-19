0cae168987be85a08a19f8942a5891def5716200
David Persson <davidpersson@gmx.de>
2011-08-19 14:54:01 +0200

    Deprecating method.

--- admin/controllers/EventsController.php
+++ admin/controllers/EventsController.php
@@ -628,25 +628,6 @@ class EventsController extends BaseController {
 		return $items;
 	}
 
-	/**
-	 * Parse the images from the request using the key
-	 * @param object
-	 * @return array
-	 */
-	protected function parseImages($imageRecord = null) {
-		$images = array();
-
-		foreach ($this->request->data as $key => $value) {
-			if (substr($key, -6) == '_image' ) {
-				$images["$key"] = $value;
-			}
-		}
-		if (empty($images) && !empty($imageRecord)) {
-			$images = $imageRecord->data();
-		}
-		return $images;
-	}
-
 	public function preview($_id = null) {
 
 		$shareurl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
@@ -723,6 +704,26 @@ class EventsController extends BaseController {
 		} else {
 			return $str;
 		}
+    }
+	/**
+	 * Parse the images from the request using the key
+	 *
+	 * @deprecated
+	 * @param object
+	 * @return array
+	 */
+	protected function parseImages($imageRecord = null) {
+		$images = array();
+
+		foreach ($this->request->data as $key => $value) {
+			if (substr($key, -6) == '_image' ) {
+				$images["$key"] = $value;
+			}
+		}
+		if (empty($images) && !empty($imageRecord)) {
+			$images = $imageRecord->data();
+		}
+		return $images;
 	}
 }
 
