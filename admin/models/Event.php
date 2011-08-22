5aced401361d2bf830ab327bd6fa2c9b25093859
Jonathan Widro <jwidro@totsy.com>
2011-08-22 12:59:54 -0400

    added check for escaped bad chars

--- admin/models/Event.php
+++ admin/models/Event.php
@@ -117,7 +117,7 @@ class Event extends \lithium\data\Model {
 		$check_vendor_style = array();
 		
 		//arrays of header names to check stuff
-		$check_required = array("vendor", "vendor_style", "category", "sub-category", "description", "color", "quantity");
+		$check_required = array("vendor", "vendor_style", "category", "sub-category", "description", "quantity");
 		$check_badchars = array("vendor", "vendor_style", "age", "category", "sub-category", "description", "color", "no size");
 		$check_decimals = array("msrp", "sale_retail", "percentage_off", "orig_wholesale", "sale_wholesale", "imu");
 		$check_departments = array("Girls", "Boys", "Momsdads");
@@ -158,12 +158,16 @@ class Event extends \lithium\data\Model {
 					
 							if (!empty($val)) {
 								if(strpos($val, "&")){
-									$errors[] = "$heading[$col] has an illegal character in row #$row";
-									$output .= Event::makecell($val, true);
+									if(!strpos($val, "\&")){
+										$errors[] = "$heading[$col] has an illegal character in row #$row";
+										$output .= Event::makecell($val, true);
+									}
 								}
 								if(strpos($val, "!")){
-									$errors[] = "$heading[$col] has an illegal character in row #$row";
-									$output .= Event::makecell($val, true);
+									if(!strpos($val, "\!")){
+										$errors[] = "$heading[$col] has an illegal character in row #$row";
+										$output .= Event::makecell($val, true);
+									}
 								}
 							}
 						}
