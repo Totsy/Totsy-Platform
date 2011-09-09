414ba63ab61cece305391bc805edfafb859eebe2
Jonathan Widro <jwidro@totsy.com>
2011-09-09 16:29:44 -0400

    msft weird characters replace func to base

--- admin/models/Base.php
+++ admin/models/Base.php
@@ -44,21 +44,6 @@ class Base extends \lithium\data\Model {
 		return $user['_id'];
 	}
 
-	public static function convert_smart_quotes($string){ 
-	    $search = array(chr(145), 
-	                    chr(146), 
-	                    chr(147), 
-	                    chr(148), 
-	                    chr(151)); 
-	 
-	    $replace = array("'", 
-	                     "'", 
-	                     '"', 
-	                     '"', 
-	                     '-'); 
-	 
-	    return str_replace($search, $replace, $string); 
-	} 
 }
 
 ?>
