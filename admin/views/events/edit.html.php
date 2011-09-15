1842fbfa9dadf507d1897b44a89b34bd434af697
David Persson <davidpersson@gmx.de>
2011-09-15 21:05:48 +0200

    Fixing unclosed strong tag.

--- admin/views/events/edit.html.php
+++ admin/views/events/edit.html.php
@@ -424,11 +424,11 @@ $(function() {
 
 			<br><br>
 			<h2 id="">Delete Items</h2>
-				<p>Click the button below to delete all items from this event. <strong>WARNING - This action cannot be undone. All items associated with this event will be deleted!!!!!!<strong></p>
-				<?php echo $this->form->create(null, array('url' => 'Items::removeItems', 'name' => 'item-delete')); ?>
-					<?php echo $this->form->hidden('event', array('value' => $event->_id)); ?>
-					<?php echo $this->form->submit('Delete All Items'); ?>
-				<?php echo $this->form->end(); ?>
+				<p>Click the button below to delete all items from this event. <strong>WARNING - This action cannot be undone. All items associated with this event will be deleted!!!!!!</strong></p>
+				<?=$this->form->create(null, array('url' => 'Items::removeItems', 'name' => 'item-delete')); ?>
+					<?=$this->form->hidden('event', array('value' => $event->_id)); ?>
+					<?=$this->form->submit('Delete All Items'); ?>
+				<?=$this->form->end(); ?>
 		</div>
 		<div id="event_history">
 				<?php
