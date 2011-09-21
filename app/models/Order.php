1e72db93551548ba1ad09ae25c56c090d3778210
David Persson <davidpersson@gmx.de>
2011-09-21 21:41:03 +0200

    Check if service has value first.

--- app/models/Order.php
+++ app/models/Order.php
@@ -222,7 +222,7 @@ class Order extends Base {
 				'shipDate' => Cart::shipDate($order)
 			);
 			#In Case Of First Order, Send an Email About 10$ Off Discount
-			if (array_key_exists('freeshipping', $service) && $service['freeshipping'] === 'eligible') {
+			if ($service && array_key_exists('freeshipping', $service) && $service['freeshipping'] === 'eligible') {
 				Mailer::send('Welcome_10_Off', $user->email, $data);
 			}
 			Mailer::send('Order_Confirmation', $user->email, $data);
