7fae73b2b2ee210c677a9a24d2076e4fbfafb794
David Persson <davidpersson@gmx.de>
2011-09-22 12:33:08 +0200

    Mocking out payments.
    Conflicts:
    
    	app/models/Order.php

--- app/tests/mocks/models/OrderMock.php
+++ app/tests/mocks/models/OrderMock.php
@@ -5,7 +5,8 @@ namespace app\tests\mocks\models;
 class OrderMock extends \app\models\Order {
 
 	protected static $_classes = array(
-		'tax' => 'app\tests\mocks\extensions\AvaTaxMock'
+		'tax' => 'app\tests\mocks\extensions\AvaTaxMock',
+		'payments' => 'app\tests\mocks\extensions\PaymentsMock'
 	);
 }
 
