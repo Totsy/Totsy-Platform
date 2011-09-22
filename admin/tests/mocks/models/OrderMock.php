3a27c65f22f513a537e8ed9cf4a294b33f4e295f
David Persson <davidpersson@gmx.de>
2011-09-22 14:16:28 +0200

    Mocking payments for admin tests.

--- admin/tests/mocks/models/OrderMock.php
+++ admin/tests/mocks/models/OrderMock.php
@@ -5,7 +5,8 @@ namespace admin\tests\mocks\models;
 class OrderMock extends \admin\models\Order {
 
 	protected static $_classes = array(
-		'tax' => 'admin\tests\mocks\extensions\AvaTaxMock'
+		'tax' => 'admin\tests\mocks\extensions\AvaTaxMock',
+		'payments' => 'admin\tests\mocks\extensions\PaymentsMock'
 	);
 }
 
