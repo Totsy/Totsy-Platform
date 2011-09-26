f2448ac3087300c79af3037afd2ddf6814feb95a
David Persson <davidpersson@gmx.de>
2011-09-26 16:13:37 +0200

    Mock credit card encrypt/decrypt.

--- app/tests/mocks/models/OrderMock.php
+++ app/tests/mocks/models/OrderMock.php
@@ -2,12 +2,30 @@
 
 namespace app\tests\mocks\models;
 
+use lithium\storage\Session;
+
 class OrderMock extends \app\models\Order {
 
 	protected static $_classes = array(
 		'tax' => 'app\tests\mocks\extensions\AvaTaxMock',
 		'payments' => 'app\tests\mocks\extensions\PaymentsMock'
 	);
+
+	public static function creditCardDecrypt($user_id) {
+		$cc_infos = Session::read('cc_infos');
+
+		foreach ($cc_infos as $key => &$value) {
+			$value = str_replace('encrypted:', '', $value);
+		}
+		return $cc_infos;
+	}
+
+	public static function creditCardEncrypt($cc_infos, $user_id, $save_iv_in_session = false) {
+		foreach ($cc_infos as $key => &$value) {
+			$value = 'encrypted:' . $value;
+		}
+		return $cc_infos;
+	}
 }
 
 ?>
