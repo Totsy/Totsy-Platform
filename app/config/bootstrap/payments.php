96e34969a2575c573648e3d997661698eee09c3c
David Persson <davidpersson@gmx.de>
2011-10-02 20:35:38 +0200

    Provide testing credentials.

--- app/config/bootstrap/payments.php
+++ app/config/bootstrap/payments.php
@@ -9,11 +9,10 @@ use li3_payments\payments\Processor;
  * `https://ebctest.cybersource.com` and go to `Account Managmenet` ->
  * `Transaction Security Keys` -> `Security Keys for the SOAP Toolkit`.
  */
-
-$key  = '<transaction key>';
+$key = 'uYBI7tVxPce91/BmZkOHjnSGF1gfTPHC0Ba+vYRyvcFUY3KyeLaQlVBjDU3XqWvraaJLZFy7kFRfPrd/Yz6UYEHQI5ZPFZUfzEUN9ltgGSU+NonwiZw/w1cXRRJNiOI2BgkPZbrZl+Z4YWuqiwXIF/Kfr7rvhd17WW4rwvUsOfgOEdeyvOjTIIv0izWZc3ZtdIYXWB9M8cLQFr69hHK9wVRjcrJ4tpCVUGMNTdepa+tpoktkXLuQVF8+t39jPpRgQdAjlk8VlR/MRQ32W2AZJT42ifCJnD/DVxdFEk2I4jYGCQ9lutmX5nhha6qLBcgX8p+vuu+F3XtZbivC9Sw5+A==';
 $test = array(
 	'adapter' => 'CyberSource',
-	'merchantID' => '<merchant id>',
+	'merchantID' => 'hlince',
 	'transactionKey' => $key,
 	'endpoint' => 'test'
 );
