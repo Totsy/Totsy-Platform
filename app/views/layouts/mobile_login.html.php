e9b4661247b02c75d845517e4721b4a752cd7408
Eric The Robot <erictherobot@gmail.com>
2011-10-06 19:16:30 -0400

    checking in mobile app progress

--- app/views/layouts/mobile_login.html.php
+++ app/views/layouts/mobile_login.html.php
@@ -1,3 +1,6 @@
+<style>
+	body { background:#f5f5f5;}
+</style>
 <?php use lithium\net\http\Router; ?>
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml"
@@ -6,17 +9,62 @@
 <head>
 	<?php echo $this->html->charset();?>
 	<title>Totsy, the private sale site for Moms</title>
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+	<meta name = "format-detection" content = "telephone=no">
+	<link media="only screen and (max-device-width: 480px) and (min-device-width: 320px)" href="css/mobile.css" type="text/css" rel="stylesheet" />
+	<link media="handheld, only screen and (max-device-width: 319px)" href="css/mobile_simple.css" type="text/css" rel="stylesheet" />
+	<?=$this->html->style(array('base', 'mobile.css', 'mobile_reset.css'), array('media' => 'screen')); ?>
 	<meta property="fb:app_id" content="181445585225391"/>
 	<meta property="og:site_name" content="Totsy"/>
-    <meta name="description"
-          content="Totsy has this super cool find available now and so much more for kids and moms! Score the best brands for your family at up to 90% off. Tons of new sales open every day. Membership is FREE, fast and easy. Start saving now!"/>
+	<meta name="description" content="Totsy has this super cool find available now and so much more for kids and moms! Score the best brands for your family at up to 90% off. Tons of new sales open every day. Membership is FREE, fast and easy. Start saving now!"/>
 
 	<?php echo $this->scripts(); ?>
 	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+	
+	<script type="application/x-javascript">
+		if (navigator.userAgent.indexOf('iPhone') != -1) {
+		addEventListener("load", function() {
+		setTimeout(hideURLbar, 0);
+		}, false);
+		}
+		
+		function hideURLbar() {
+		window.scrollTo(0, 1);
+		}
+	</script>
 </head>
 <body>
+	<?php echo $this->html->link($this->html->image('logo.png', array('width'=>'100', 'style' => 'text-align:center; margin:20px 0px 3px 110px;')), '/sales', array('escape'=> false)); ?>
 	<?php echo $this->content(); ?>
-    <!-- Affiliate Pixel -->
-    <?php echo $pixel; ?>		
+	<!-- Affiliate Pixel -->
+	<?php echo $pixel; ?>
+	<script>
+	    $(document).ready(function(){
+    $("label.inlined + input.input-text").each(function (type) {
+     
+    Event.observe(window, 'load', function () {
+    setTimeout(function(){
+    if (!input.value.empty()) {
+    input.previous().addClassName('has-text');
+    }
+    }, 200);
+    });
+     
+    $(this).focus(function () {
+    $(this).prev("label.inlined").addClass("focus");
+    });
+     
+    $(this).keypress(function () {
+    $(this).prev("label.inlined").addClass("has-text").removeClass("focus");
+    });
+     
+    $(this).blur(function () {
+    if($(this).val() == "") {
+    $(this).prev("label.inlined").removeClass("has-text").removeClass("focus");
+    }
+    });
+    });
+    });
+</script>
 </body>
 </html>
--- app/views/users/mobile_login.html.php
+++ app/views/users/mobile_login.html.php
@@ -1,12 +1,51 @@
+<style>
+	.mobile { min-width:320px; text-align: center!important; margin:0px auto; padding:0px; }
+	#mobileFooter { float: left; }
+	#mobileFooter  ul, li { margin:0px; padding:0px; }
+	#mobileFooter  li { float: left; }
+	
+	.error {background:#ff0000; color:#fff; font-size: 12px; }
+	
+	.legal { font-size:11px; text-align: center;}
+	
+	h1, h2, h3, h4, h5, h6 { margin:0px; padding:0px; } 
+	h2 { font-size:18px; color:#000; margin-bottom: 10px;  }
+	
+	#mobileFooter { bottom:0; position: absolute; text-align: center!important;}
+	#mobileFooter li { padding:10px;}
+	
+	.mobile-input {margin-left:2px; padding:5px; border:1px solid #ddd; font-size:14px; margin-bottom:10px;}
+	.mobile-password {margin-left:0px; padding:5px; border:1px solid #ddd; font-size:14px;}
+</style>
 <?php if ($message){ echo $message; } ?>
-<h2>Member Sign In</h2>
-<?=$this->form->create(null,array('id'=>'loginForm'));?>
-<?=$this->form->field('email', array('class'=>"validate['required']  inputbox", 'id'=>"email"));?>
-<?=$this->form->field('password', array('class'=>"validate['required'] inputbox",
-//'name' => 'password',
-'id' => 'password', 'type' => 'password'));?>
-<?=$this->form->checkbox('remember_me', array('class' => 'fl checkbox')); ?> Remember Me <br/>
-<?=$this->form->submit('Sign In', array('class'=>"button fr"));?> 
-<?=$this->form->end();?>
-<div style="clear:both;"></div>
-<p style='margin-top: 10px'> <?=$this->html->link('Forgot your password?','/reset', array('class'=>"md", 'title'=>"Forgot your password?"))?> </p>
+<div class="mobile">
+	<h2>Sign In</h2>
+	<?=$this->form->create(null,array('id'=>'loginForm'));?>
+	<input type="input" class="validate['required'] input-text mobile-input" id="email" />
+	<br />
+	<input type="password" class="validate['required'] input-text mobile-password" id="password" />
+	<br />
+	<?=$this->form->submit('Login', array('class' => "button", "style" => "text-align:center; margin:10px 0px 10px 187px;"));?> 
+	<?=$this->form->end();?>
+	<div style="clear:both;"></div>
+	<div id="fb-root"></div>
+			<script src="http://connect.facebook.net/en_US/all.js"></script>
+			<script>
+				FB.init({ 
+				appId:'130085027045086', cookie:true, 
+				status:true, xfbml:true, oauth: true
+				});
+			</script>
+		<!-- <fb:login-button>Login with Facebook</fb:login-button> -->
+		<a href="http://www.facebook.com/dialog/oauth?client_id=130085027045086&redirect_uri=http://eric.totsy.com&display=touch"><img src="http://www.totsy.com/img/fb_login_btn.png" /></a>
+		
+		<p style='margin-top: 10px'> <?=$this->html->link('Forgot your password?','/reset', array('class'=>"md", 'title'=>"Forgot your password?", 'style' => 'margin-bottom:5px;'))?> </p>
+	
+	<div id="mobileFooter">
+		<ul>
+			<li><a href="/terms">Terms</a></li>
+			<li style="float:right;"><a href="/privacy_policy">Privacy Policy</a></li>
+		</ul>
+	</div>
+	<div class="legal">&copy; 2011 Totsy, Inc. All Rights Reserved.</div>
+	</div>
