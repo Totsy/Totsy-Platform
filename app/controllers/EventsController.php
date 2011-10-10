0c600ebb679afec6f056297a8f7fa43eeaf533de
Jonathan Widro <jwidro@totsy.com>
2011-10-10 14:31:53 -0400

    checks for viewlive

--- app/controllers/EventsController.php
+++ app/controllers/EventsController.php
@@ -27,14 +27,11 @@ class EventsController extends BaseController {
 			$pendingEvents = Event::pending(null,array(),$departments);
 		}
 
-		$itemCounts = array();
-		/*
-		// DON'T COUNT ITEMS !!!!
-		// IMPORTANT
-		// Slav
+		
 		$itemCounts = $this->inventoryCheck(Event::open(array(
 			'fields' => array('items')
 		)));
+		
 
 		//Sort events open/sold out
 		foreach ($openEvents as $key => $event) {
@@ -45,8 +42,7 @@ class EventsController extends BaseController {
 				}
 			}
 		}
-		*/
-		if (isset($events_closed) && !empty($events_closed)) {
+		if (!empty($events_closed)) {
 			if (!empty($openEvents)) {
 				foreach ($events_closed as $event) {
 					$openEvents[] = $event;
@@ -80,6 +76,13 @@ class EventsController extends BaseController {
 				'enabled' => true,
 				'url' => $url
 		)));
+		if (!$event) {
+			$event = Event::first(array(
+				'conditions' => array(
+				'viewlive' => true,
+				'url' => $url
+			)));
+		}
 		if (!$event) {
 			$this->_render['template'] = 'noevent';
 			return array('event' => null, 'items' => array(), 'shareurl');
@@ -164,8 +167,6 @@ class EventsController extends BaseController {
 
 	public function inventoryCheck($events) {
 		$events = $events->data();
-		$itemCounts = array();
-
 		foreach ($events as $eventItems) {
 			$count = 0;
 			$id = $eventItems['_id'] ;
