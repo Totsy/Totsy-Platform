4ec715157a377f5285dc3965c904626df96e9491
Jrek <slv.kosh@gmail.com>
2011-10-11 17:04:37 -0400

    shortDescLimit update

--- admin/controllers/EventsController.php
+++ admin/controllers/EventsController.php
@@ -21,7 +21,7 @@ class EventsController extends BaseController {
 	/**
 	 * Limit characters for event\deal short description
 	 */
-	private $sortDescLimit = 45;
+	private $shortDescLimit = 45;
 	
 	/**
 	 * List of event keys that should be in the view
