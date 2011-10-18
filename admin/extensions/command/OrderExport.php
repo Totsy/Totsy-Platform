d3ba26623ee3fac5255a887d8cbb6aec6ebe9a7c
Jonathan Widro <jwidro@totsy.com>
2011-10-18 10:49:01 -0400

    temporary change to send email

--- admin/extensions/command/OrderExport.php
+++ admin/extensions/command/OrderExport.php
@@ -210,7 +210,7 @@ class OrderExport extends Base {
 						$queue->save();
 						$this->summary['from_email'] = 'no-reply@totsy.com';
 						//$this->summary['to_email'] = 'logistics@totsy.com';
-						$this->summary['to_email'] = 'jwidro@totsy.com';
+						$this->summary['to_email'] = 'jwidro@totsy.com,mmiller@totsy.com';
 						if ($this->test != 'true') {
                            Mailer::send('Order_Export', $this->summary['to_email'], $this->summary);
                         }
