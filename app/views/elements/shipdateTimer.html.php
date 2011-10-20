1a4f9f143404035617c8eb1a234b6f6020474f72
Eric The Robot <erictherobot@gmail.com>
2011-10-20 10:59:32 -0400

    changing estimated delivery date wording and calculation

--- app/views/elements/shipdateTimer.html.php
+++ app/views/elements/shipdateTimer.html.php
@@ -3,6 +3,6 @@ Item Reserved For:<br />
     <span id="itemCounter" style="color:#009900; font-weight:bold;"></span>
 </span>
 <span style="float:left">
- Estimated Shipping Date: <br />
+ Estimated Delivery Date: <br />
  	<span style="font-weight:bold; color:#009900;"><?=date('m-d-Y', $shipDate)?></span>
 </span>	
