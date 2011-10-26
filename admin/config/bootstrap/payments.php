de5b398ade7a8814164ccd7b066313e0ce82c1b6
Howard Lince III <hlince@live.com>
2011-10-26 11:11:13 -0500

    adding a newly generated cybersource key for live.

--- admin/config/bootstrap/payments.php
+++ admin/config/bootstrap/payments.php
@@ -1,6 +1,30 @@
 <?php
 
 use li3_payments\payments\Processor;
+use lithium\core\Environment;
+
+$adapterFilters = array(
+	'cybersource' => function($function, $params, $name) {
+		$options = $params['options'];
+
+		switch ($function) {
+			case 'authorize' && $params['pmt']->type == 'amex':
+				return true;
+				break;
+		}
+		if (isset($options['processor']) && $options['processor'] == 'CyberSource') {
+			return true;
+		}
+	},
+	'authorizenet' => function($function, $params, $name) {
+		$options = $params['options'];
+
+		$processor = isset($options['processor']) ? $options['processor'] : false;
+		if (!$processor || $processor == 'AuthorizeNet') {
+			return true;
+		}
+	}
+);
 
 /**
  * Payment configuration.
@@ -14,15 +38,23 @@ $test = array(
 	'adapter' => 'CyberSource',
 	'merchantID' => 'hlince',
 	'transactionKey' => $key,
-	'endpoint' => 'test'
+	'endpoint' => 'test',
+	'filters' => array(
+		'alwaysProcessAdapter' => true,
+		'adapter' => $adapterFilters['cybersource']
+	)
 );
 
-$key  = '<transaction key>';
+$key = 'BexYoSnNAjU/1+osPIPukh0uYy4qf8tc7+f2Xb107q4Y1tI6tCHSdzdtDxgyAKzpb9IrD6vwxca6OMadqpcC2WuFUN2gWIsXnyEpAkEAjpNShVS1Ex0GkEi5/+7C0pMKKVgL5celaTLwYLH/Bnb8dXwNp+/aOogskyIApmZ2j0JbXJuLr5+r/ZEuTWKrChIDHS5jLip/y1zv5/ZdvXTurhjW0jq0IdJ3N20PGDIArOlv0isPq/DFxro4xp2qlwLZa4VQ3aBYixefISkCQQCOk1KFVLUTHQaQSLn/7sLSkwopWAvlx6VpMvBgsf8Gdvx1fA2n79o6iCyTIgCmZnaPQg==';
 $live = array(
 	'adapter' => 'CyberSource',
-	'merchantID' => '<merchant id>',
+	'merchantID' => 'totsy',
 	'transactionKey' => $key,
-	'endpoint' => 'live'
+	'endpoint' => 'live',
+	'filters' => array(
+		'alwaysProcessAdapter' => true,
+		'adapter' => $adapterFilters['cybersource']
+	)
 );
 
 Processor::config(array(
@@ -31,6 +63,30 @@ Processor::config(array(
 		'test' => $test,
 		'development' => $test,
 		'local' => $test
+	),
+	'authorizenet' => array(
+		'adapter' => 'AuthorizeNet',
+		'login' => '8M2rfU63AKzX',
+		'key' => '2J6978WzN6WV6jb7',
+		'debug' => false,
+		'gateway' => 'live',
+		'connection' => array('classes' => array('socket' => 'lithium\net\socket\Curl')),
+		'filters' => array(
+			'alwaysProcessAdapter' => true,
+			'adapter' => $adapterFilters['authorizenet']
+		)
+	),
+	'authorizenet-test' => array(
+		'adapter' => 'AuthorizeNet',
+		'login' => '7uXvS44q',
+		'key' => '5z4X93s7cq4P2tEQ',
+		'debug' => false,
+		'gateway' => 'test',
+		'connection' => array('classes' => array('socket' => 'lithium\net\socket\Curl')),
+		'filters' => array(
+			'alwaysProcessAdapter' => true,
+			'adapter' => $adapterFilters['authorizenet']
+		)
 	),
 	'local' => $test,
 	'test' => $test
