07b6861914115c3e3da2091a88463c8ea9da6264
Jrek <slv.kosh@gmail.com>
2011-11-16 11:31:32 -0500

    added image for pulse

--- app/views/api/rss/items/default.php
+++ app/views/api/rss/items/default.php
@@ -1,26 +1,37 @@
 <?php use app\models\File; ?>
 <?php echo '<?xml version="1.0"?>'."\n"; ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:tns='http://totsy.com/totsy-xml-rss-name-space' xmlns:ev='http://purl.org/rss/1.0/modules/event/'>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:tns="http://totsy.com/totsy-xml-rss-name-space" xmlns:ev='http://purl.org/rss/1.0/modules/event/' xmlns:dc="http://purl.org/dc/elements/1.1/">
 	<channel> 
+		<atom:link rel="self" href="https://slavik.totsy.com/api/items.rss?auth_token=a23a3ca72553e553be95d2982300fe84cd3058f4" type="application/rss+xml"/>
     	<title>Items</title>     
     	<link>http://totsy.com/</link> 
     	<description></description> 
 <?php if (is_array($data['items'])){ 
 		foreach($data['items'] as $item){ 
-
-			if (isset($item['primary_image'])){
+			if (isset($item['zoom_image'])){
+				$image = $item['base_url'].'image/'.$item['zoom_image'].'.jpg';
+				$image_size = File::first($item['zoom_image'])->file->getSize();
+			} else if (isset($item['primary_image'])){
 				$image = $item['base_url'].'image/'.$item['primary_image'].'.jpg';
 				$image_size = File::first($item['primary_image'])->file->getSize();
 			} else {
 				$image = $item['base_url'].'/img/no-image-large.jpeg';
 				$image_size = '35445';
-			} ?>
+			} 
+			$link = $item['base_url'].'sale/'.$item['event_url'].'/'.$item['url'];
+			?>
 		<item>
-			<title><?php echo htmlspecialchars($item['description']) ?></title>
-			<link><?php echo $item['base_url'].'sale/'.$item['event_url'].'/'.$item['url'];?></link>
+			<title><![CDATA[<?php echo $item['description']; ?>]]></title>
+			<link><?=$link?></link>
 			<enclosure url="<?=$image?>" length="<?=$image_size?>" type="image/jpeg" />
 <?php if (array_key_exists('blurb',$item)) { ?>
-			<description><?php echo htmlspecialchars($item['blurb']); ?></description>
+			<description><![CDATA[<?php 
+				//echo htmlspecialchars('<img src="'.$image.'" width="150" height="150">'.$item['blurb']);
+				?><a href="<?=$link ?>">
+					<img src="<?=$image?>" style="border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-top-style: solid; border-right-style: solid; border-bottom-style: solid; border-left-style: solid; margin-left: 4px; margin-right: 4px; margin-top: 4px; margin-bottom: 4px;>
+				   </a><?php 
+				echo $item['blurb'] ?> 
+				?>]]></description>
 <?php } else { ?>
 			<description/>
 <?php } ?>
@@ -30,12 +41,14 @@
 			<ev:startdate><?php echo date('c',$item['start_date']['sec']); ?></ev:startdate>
 			<ev:enddate><?php echo date('c',$item['end_date']['sec']); ?></ev:enddate>
 			<ev:type>sale</ev:type>
+			<dc:creator><![CDATA[ Totsy ]]></dc:creator>
 		</item>
-<?php 	}
+<?php 	
+		}
 	} 
+	
 ?>
 	</channel>
 </rss>
-
 
 
