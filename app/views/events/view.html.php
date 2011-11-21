b9d4c1c66e5b81d52920164012999d79b8fd2347
gene <gsuper@totsy.com>
2011-11-21 11:40:10 -0500

    xmas shipping

--- app/views/events/view.html.php
+++ app/views/events/view.html.php
@@ -48,6 +48,29 @@
 						
 					</div>
 <?php endif ?>
+	<?php
+	if($missChristmasCount>0){
+	?>
+				<div style="margin-top:10px;line-height:12px;font-weight:bold; color:#990000; font-size:11px;text-align:left;">
+				<img src="/img/truck_red.png">
+				One on more of the items in your cart are not guaranteed to arrive before 12/26.*
+				</div>
+	
+	
+	<?php
+	}
+	else{
+	?>
+				<div style="margin-top:10px;line-height:12px;font-weight:bold; color:#999999; font-size:11px;text-align:left;">
+				<img src="/img/truck_grey.png">
+				Item will be delivered on or before 12/23.*
+				</div>
+	
+	
+	<?php
+	}
+	?>
+
 			</div>
 		</div>
 		<br />
