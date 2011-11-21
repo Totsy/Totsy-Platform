15f06b74a9f49921bc8bc36ecf2391e6e2ca7d29
eubiera <eubiera@totsy.com>
2011-11-21 09:40:35 -0500

    controller and assets for Rewards

---
+++ app/views/rewards/index.html.php
@@ -0,0 +1,47 @@
+<div class="grid_16">
+	<br />
+	<h1 class="page-title gray">
+		<span class="_red">
+		Totsy Rewards Program
+		</span>
+	</h1>
+	<hr />
+	<div id="rewards-enrollment-banner">
+		<div id="rewards-enroll-now-button-outer">
+			<div id="rewards-enroll-now-button-inner">
+				<a class="button" href="" style="width: 213px;">Enroll Now</a>
+				<div style="height:35px !important">
+				Check my balance
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="grid_11">
+<br />
+<h1 class="page-title gray">
+	<span class="_red">
+		Earn Points
+	</span>
+</h1>
+<hr />
+<div>
+Once you enroll, you're ready to start earning points! The more things you do on Totsy, like shopping and sharing our deals -  the more points you'll earn. And, when you refer friends to sign up and shop, you can earn bonus points based on your friends' activity!
+</div>
+
+<div>
+<img src="/img/rewards_points_chart.png">
+</div>
+<h1 class="page-title gray">
+	<span class="_red">
+		Get Rewards
+	</span>
+</h1>
+<hr />
+
+</div>
+
+<div class="grid_5">
+<span style="border: medium none !important; height:397; width:270"></span>
+</div>
