564798e9c7caf5c6ef53911312df3657ed34f0ba
gene <gsuper@totsy.com>
2011-11-22 16:09:15 -0500

    xmas ship

--- app/views/orders/payment.html.php
+++ app/views/orders/payment.html.php
@@ -244,6 +244,30 @@ var paymentForm = new Object();
 	</div>
 <?php endif ?>
 <?=$this->form->end();?>
+
+
+<div class="clear"></div>
+<div style="color:#707070; font-size:12px; font-weight:bold; padding:10px;">
+				<?php
+				if($missChristmasCount>0){
+				?>
+				* Totsy ships all items complete and does not split items into separate orders. If you would like the designated items in your cart delivered on or before 12/23, we suggest you order them separately from the items not guaranteed to arrive before 12/26. Our delivery guarantee does not apply when transportation networks are affected by weather. Please contact our Customer Service department at 888-247-9444 or email <a href="mailto:support@totsy.com">support@totsy.com</a> with any questions.
+				
+				<?php
+				}
+				else{
+				?>
+				
+				* Our delivery guarantee does not apply when transportation networks are affected by weather.
+				
+				<?php
+				}
+				?>
+				
+</div>
+
+
+
 <div id="address_form" style="display:none">
 	<?=$this->form->create(null ,array('id'=>'selectForm')); ?>
 	<?=$this->form->hidden('address_id', array('class' => 'inputbox', 'id' => 'address_id')); ?>
