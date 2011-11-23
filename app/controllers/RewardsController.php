709663376af5da2067b40c5d3332b0e62760165c
eubiera <eubiera@totsy.com>
2011-11-22 19:09:15 -0500

    developed program overview HTML and CSS

---
+++ app/controllers/RewardsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers;
+
+use app\extensions\Mailer;
+use lithium\action\Request;
+use \lithium\data\Connections;
+use \lithium\util\Validator;
+
+class RewardsController extends BaseController {
+
+	//This function takes an email (unsubscribed user) from Sailthru and posts it to Unsubcentral's API under the list unsubscribed
+	public function index() {
+		//to serve either register or view page for 500 point reward program
+  	}
+
+}
+
+?>
---
+++ app/views/rewards/index.html.php
@@ -0,0 +1,165 @@
+<div class="grid_16">
+	<br />
+	<h1 class="page-title gray">
+		<span class="_red">
+		Totsy Rewards Program
+		</span>
+	</h1>
+	<hr />
+	<div id="rewards-enrollment-banner">
+		<div id="rewards-enroll-now-button-outer">
+			<div id="rewards-enroll-now-button-inner">
+				<a class="button" href="" style="width: 213px; font-size:16px !important; font-weight:normal">Enroll Now!</a>
+				<div id="rewards-check-balance">
+				Check my balance
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="grid_11">
+<br />
+<h1 class="page-title gray">
+	<span class="_red">
+		Earn Points
+	</span>
+</h1>
+<hr />
+<div>
+Once you enroll, you're ready to start earning points! The more things you do on Totsy, like shopping and sharing our deals -  the more points you'll earn. And, when you refer friends to sign up and shop, you can earn bonus points based on your friends' activity!
+</div>
+
+<div>
+<img src="/img/rewards_points_chart.png">
+</div>
+<h1 class="page-title gray">
+	<span class="_red">
+		Get Rewards
+	</span>
+</h1>
+
+<hr />
+<div style="padding-top:20px">
+<div style="float:left">
+	<img src="/img/rewards_little_girl.png">
+</div>
+
+<div style="float:right">
+	<div class="page-title gray" style="width:455px !important">
+	Redeem your points for discounts, donations, and credits toward future Totsy purchases! <a href="#">Enroll now to start earning!</a>
+	</div>
+	<br />
+		<ul id="get-rewards">
+			<li>
+				<span>500 points</span>$10 Off an order over $50
+				<hr />
+			</li>
+			<li>
+				<span>1,000 points</span><a href="">Plant a Tree</a> or Free Shipping on an order over $40
+				<hr />
+			</li>
+			<li>
+				<span>1,500 points</span>$15 Off an order over $60
+				<hr />
+			</li>
+			<li>
+				<span>2,000 points</span>$20 Off an order over $100
+				<hr />
+			</li>
+			<li>
+				<span>5,000 points</span>$50 Off an order over $100
+			</li>
+		</ul>
+	<div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="grid_5">
+	<div style="padding-top:10px;">
+		<table id="rewards-top-members">
+		
+		<!-- dynamic stuff -->
+			<tr>
+				<td>
+					<div class="count">1</div>
+					<div class="thumb">
+					    <img src="/img/test_thumb.jpg" width="50" height="50">
+					</div>
+					<div class="content">
+					    <div class="name">First Name Last Name</div>
+					    <div style="clear:both"></div>
+					    <div class="address">Jersey City, NJ</div>
+					    <div style="clear:both"></div>
+					    <div class="graph-data"></div>
+					</div>
+				</td>
+			</tr>
+		<!-- -->	
+			
+			<tr>
+				<td>
+					<div class="count">2</div>
+					<div class="thumb">
+					    <img src="/img/test_thumb.jpg" width="50" height="50">
+					</div>
+					<div class="content">
+					    <div class="name">First name Last Name</div>
+					    <div style="clear:both"></div>
+					    <div class="address">Jersey City, NJ</div>
+					    <div style="clear:both"></div>
+					    <div class="graph-data"></div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="count">3</div>
+					<div class="thumb">
+					    <img src="/img/test_thumb.jpg" width="50" height="50">
+					</div>
+					<div class="content">
+					    <div class="name">First name Last Name</div>
+					    <div style="clear:both"></div>
+					    <div class="address">Jersey City, NJ</div>
+					    <div style="clear:both"></div>
+					    <div class="graph-data"></div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="count">4</div>
+					<div class="thumb">
+					    <img src="/img/test_thumb.jpg" width="50" height="50">
+					</div>
+					<div class="content">
+					    <div class="name">First name Last Name</div>
+					    <div style="clear:both"></div>
+					    <div class="address">Jersey City, NJ</div>
+					    <div style="clear:both"></div>
+					    <div class="graph-data"></div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="count">5</div>
+					<div class="thumb">
+					    <img src="/img/test_thumb.jpg" width="50" height="50">
+					</div>
+					<div class="content">
+					    <div class="name">First name Last Name</div>
+					    <div style="clear:both"></div>
+					    <div class="address">Jersey City, NJ</div>
+					    <div style="clear:both"></div>
+					    <div class="graph-data"></div>
+					</div>
+				</td>
+			</tr>
+			<!-- dynamic section ends here -->		
+		</table>
+	</div>
+</div>
