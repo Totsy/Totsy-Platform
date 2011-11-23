eb9fa175edd67f64d85d36d23d118cb59d40e08f
Eric The Robot <erictherobot@gmail.com>
2011-11-23 14:43:20 -0500

    adding asset versioning to all assets

--- app/views/events/index.html.php
+++ app/views/events/index.html.php
@@ -1,7 +1,7 @@
 <?=$this->html->script(array('jquery.nivo.slider.pack'));?>
 <script src="/js/jquery.tmpl.js" type="text/javascript"></script>
 
-<?=$this->html->script(array('cart-timer.js', 'cart-items-timer.js', 'cart-popup.js?v=001'));?>
+<?=$this->html->script(array('cart-timer.js?v=007', 'cart-items-timer.js?v=007', 'cart-popup.js?v=007'));?>
 
 <!-- template used for items on cart. jquery.tmpl.js driven -->
 <?=$this->view()->render( array('element' => 'popupCartItems') ); ?>
