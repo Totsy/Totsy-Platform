816322278871604010d4d4383a246294f9067ca2
gene <gsuper@totsy.com>
2011-11-23 18:14:52 -0500

    xmas check for order date

--- app/controllers/OrdersController.php
+++ app/controllers/OrdersController.php
@@ -89,7 +89,7 @@ class OrdersController extends BaseController {
 				'user_id' => (string) $user['_id']
 		)));
 		$new = ($order->date_created->sec > (time() - 120)) ? true : false;
-		if($order->date_created->sec<1322071200){
+		if($order->date_created->sec<1322006400){
 			$shipDate = Cart::shipDate($order, true);	
 			$shipDate = date('M d, Y', $shipDate);
 		}
