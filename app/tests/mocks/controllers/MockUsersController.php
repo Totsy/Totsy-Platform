6b945408ad9bf02049ce8c10f513b17f02356346
PoTa <pota@mosfet.hu>
2011-11-25 06:52:04 +0100

    Initial test coverage for UsersController.

--- app/tests/mocks/controllers/MockUsersController.php
+++ app/tests/mocks/controllers/MockUsersController.php
@@ -3,11 +3,26 @@
 namespace app\tests\mocks\controllers;
 
 class MockUsersController extends \app\controllers\UsersController {
-
 	public $redirect = array();
+
+	protected function _init() {
+		parent::_init();
+		$this->_classes['mailer'] = 'app\tests\mocks\extensions\MailerMock';
+		$this->resetMailer();
+	}
 
 	public function redirect($url, array $options = array()) {
 		$this->redirect = func_get_args();
+		return 'redirect';
+	}
+
+	public function mailer() {
+		return $this->_classes['mailer'];
+	}
+
+	protected function resetMailer() {
+		$mailer = $this->mailer();
+		$mailer::reset();
 	}
 }
 
