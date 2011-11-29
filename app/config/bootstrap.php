b4435515421da5e34fb9f77b8a85421957674b0e
David Persson <davidpersson@gmx.de>
2011-11-29 21:27:49 +0100

    Consolidating action bootstrap.

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -47,13 +47,6 @@ require __DIR__ . '/bootstrap/connections.php';
 require __DIR__ . '/bootstrap/errors.php';
 
 /**
- * This file defines bindings between classes which are triggered during the request cycle, and
- * allow the framework to automatically configure its environmental settings. You can add your own
- * behavior and modify the dispatch cycle to suit your needs.
- */
-require __DIR__ . '/bootstrap/action.php';
-
-/**
  * This file contains configurations for connecting to external caching resources, as well as
  * default caching rules for various systems within your application
  */
