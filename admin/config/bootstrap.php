94c53edb0a0adc4cf4739c3a101e414a7d27268f
David Persson <davidpersson@gmx.de>
2011-11-29 21:27:49 +0100

    Consolidating action bootstrap.

--- admin/config/bootstrap.php
+++ admin/config/bootstrap.php
@@ -64,13 +64,6 @@ Environment::is(function($request) {
 require __DIR__ . '/connections.php';
 
 /**
- * This file defines bindings between classes which are triggered during the request cycle, and
- * allow the framework to automatically configure its environmental settings. You can add your own
- * behavior and modify the dispatch cycle to suit your needs.
- */
-require __DIR__ . '/bootstrap/action.php';
-
-/**
  * Error-handling.
  */
 require __DIR__ . '/bootstrap/error.php';
--- libraries/totsy_common/config/bootstrap.php
+++ libraries/totsy_common/config/bootstrap.php
@@ -1,6 +1,16 @@
 <?php
 
+/**
+ * Error handling.
+ */
 require __DIR__ . 'bootstrap/error.php';
+
+/**
+ * This file defines bindings between classes which are triggered during the request cycle, and
+ * allow the framework to automatically configure its environmental settings. You can add your own
+ * behavior and modify the dispatch cycle to suit your needs.
+ */
+require __DIR__ . '/bootstrap/action.php';
 
 /**
  * This file contains configurations for handling different content types within the framework,
