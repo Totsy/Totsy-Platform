36be79fced4e7bd308a815844a60ad1044585a26
PoTa <pota@mosfet.hu>
2011-11-29 12:00:31 +0100

    Refactor UserTest and UsersControllerTest not to depend on clean state of the database (e.g. fixture data violating uniqueness constraints).

--- app/tests/cases/models/UserTest.php
+++ app/tests/cases/models/UserTest.php
@@ -9,52 +9,37 @@ use li3_fixtures\test\Fixture;
 
 class UserTest extends \lithium\test\Unit {
 	public function setUp() {
+		$this->sessionConfig = Session::Config();
+		Session::config(array(
+			'default' => array('adapter' => 'app\tests\mocks\storage\session\adapter\MemoryMock'),
+			'cookie' => array('adapter' => 'app\tests\mocks\storage\session\adapter\MemoryMock')
+		));
 		$this->users = Fixture::load('User')->map(function ($fixture) {
-			return User::create($fixture);
+			/* Ensure we're always able to insert this record. */
+			$fixture['confirmemail'] = $fixture['email'] = uniqid('user') . '@example.com';
+			$user = User::create($fixture);
+			$user->save(null, array('validate' => false));
+			return $user;
 		});
-		$this->failed_fixtures = array();
-		foreach ($this->users as $short => $user) {
-			if (!$user->save()) {
-				$this->failed_fixtures[] = $short;
-			}
-		}
 	}
 
 	public function tearDown() {
-		foreach ($this->users as $short => $user) {
+		foreach ($this->users as $user) {
 			$user->delete();
 		}
-	}
-
-	protected function _assertLoaded($fixtures = null) {
-		$failed = $fixtures ? array_intersect($this->failed_fixtures, (array) $fixtures) : $this->failed_fixtures;
-		$users = $this->users;
-		$errors = function ($short) use ($users) { return $short . " (" . join(", ", array_keys($users[$short]->errors())) . ")"; };
-		$this->assertTrue(empty($failed), "Some fixtures failed to load: " . join(", ", array_map($errors, $failed)));
+		Session::Config($this->sessionConfig);
 	}
 
 	protected function _user($short) {
 		$_id = $this->users[$short] ? $this->users[$short]->_id : null;
-		return $_id ? User::find($this->users[$short]->_id) : null;
-	}
-
-	protected function _fakeSession($short) {
-		$this->sessionSave = Session::read('userLogin');
-		Session::write('userLogin', array('_id' => $this->users[$short]->_id));
-	}
-
-	protected function _unfakeSession() {
-		Session::write('userLogin', $this->sessionSave);
+		return $_id ? User::find($this->users[$short]->_id->{'$id'}) : null;
 	}
 
 	public function testLookup() {
-		$users = array('user1', 'user2');
-		$this->_assertLoaded($users);
-
 		$result = User::lookup('invalid');
 		$this->assertNull($result);
 
-		foreach ($users as $short) {
+		foreach (array('user1', 'user2') as $short) {
 			$user = $this->users[$short];
 			foreach (array('email', '_id') as $attr) {
 				$lookup = User::lookup($user->$attr);
@@ -73,40 +58,35 @@ class UserTest extends \lithium\test\Unit {
 	}
 
 	public function testLog() {
-		$this->_assertLoaded('user1');
-		$old = $this->_user('user1');
-		$this->_fakeSession('user1');
+		$old = $this->users['user1'];
+		Session::write('userLogin', $old->data());
 
 		$result = User::log('1.2.3.4');
 		$this->assertTrue($result);
 
-		$this->_unfakeSession();
-		$new = $this->_user('user1');
+		$updated = $this->_user('user1');
 
 		$expected = $old->logincounter + 1;
-		$result = $new->logincounter;
+		$result = $updated->logincounter;
 		$this->assertEqual($expected, $result);
 
 		$expected = '1.2.3.4';
-		$result = $new->lastip;
+		$result = $updated->lastip;
 		$this->assertEqual($expected, $result);
 
 		$expected = $old->lastlogin;
-		$result = $new->lastlogin;
+		$result = $updated->lastlogin;
 		$this->assertNotEqual($expected, $result);
 	}
 
 	public function testApplyCredit() {
-		$this->_assertLoaded('user1');
-		$old = $this->_user('user1');
-
+		$old = $this->users['user1'];
 		$result = User::applyCredit($old->_id, 100);
 		$this->assertTrue($result);
 
-		$new = $this->_user('user1');
-
+		$updated = $this->_user('user1');
 		$expected = $old->total_credit + 100;
-		$result = $new->total_credit;
+		$result = $updated->total_credit;
 		$this->assertEqual($expected, $result);
 	}
 }
