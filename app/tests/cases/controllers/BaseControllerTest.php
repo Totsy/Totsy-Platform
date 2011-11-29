745c48eb37ce8bf6b93620ec47486eae523eab7f
PoTa <pota@mosfet.hu>
2011-11-29 10:44:57 +0100

    Refactor tests (Base, User, BaseController, UsersController) to better adhere to syntax rules.

--- app/tests/cases/controllers/BaseControllerTest.php
+++ app/tests/cases/controllers/BaseControllerTest.php
@@ -30,7 +30,7 @@ class BaseControllerTest extends \lithium\test\Unit {
 		Session::Config($this->sessionConfig);
 	}
 
-	protected function fixture($short, $class) {
+	protected function _fixture($short, $class) {
 		if (!isset($this->fixtures[$class])) {
 			$this->fixtures[$class] = Fixture::load($class);
 		}
@@ -43,72 +43,99 @@ class BaseControllerTest extends \lithium\test\Unit {
 
 	public function testConstructAddsClasses() {
 		$controller = new BaseControllerMock();
-		$this->assertNotEqual(0, count($controller->classes()));
+		$expected = 0;
+		$result = count($controller->classes());
+		$this->assertNotEqual($expected, $result);
 	}
 
 	public function testInitWithoutUser() {
 		$request = new Request(array('params' => array('controller' => 'base', 'action' => 'index')));
 		$controller = new BaseControllerMock(compact('request'));
 		$set = $controller->renderData();
-		$this->assertTrue(array_key_exists('userInfo', $set), $set);
-		$this->assertNull($set['userInfo']);
-		$this->assertTrue(isset($set['cartCount']), $set);
+
+		$result = array_key_exists('userInfo', $set);
+		$message = $set;
+		$this->assertTrue($result, $message);
+
+		$result = $set['userInfo'];
+		$this->assertNull($result);
+
+		$result = isset($set['cartCount']);
+		$message = $set;
+		$this->assertTrue($result, $message);
 	}
 
 	public function testInitWithUser() {
-		$user = $this->fixture('user1', 'User');
+		$user = $this->_fixture('user1', 'User');
 		Session::write('userLogin', $user->data());
 		$request = new Request(array('params' => array('controller' => 'base', 'action' => 'index')));
 		$controller = new BaseControllerMock(compact('request'));
 		$set = $controller->renderData();
-		$this->assertTrue(isset($set['userInfo']), $set);
-		$this->assertEqual($set['userInfo']['_id'], $user->_id);
-		$this->assertTrue(isset($set['credit']), $set);
-		$this->assertEqual($set['credit'], number_format($user->credit, 0));
+
+		$result = isset($set['userInfo']);
+		$message = $set;
+		$this->assertTrue($result, $message);
+
+		$expected = $user->_id;
+		$result = $set['userInfo']['_id'];
+		$this->assertEqual($expected, $result);
+
+		$result = isset($set['credit']);
+		$message = $set;
+		$this->assertTrue($result, $message);
+
+		$expected = number_format($user->credit, 0);
+		$result = $set['credit'];
+		$this->assertEqual($expected, $result);
 	}
 
-	protected function checkEligible($method, $key, $expected, $user) {
+	protected function _checkEligible($method, $key, $expected, $user) {
 		Session::write('services', array());
 		$controller = new BaseControllerMock();
 		$controller->invokeMethod($method, array(array('_id' => $user->_id)));
-		$result = Session::read('services');
-		$this->assertTrue(isset($result[$key]), $result);
-		$this->assertEqual($expected, $result[$key]);
+		$session = Session::read('services');
+
+		$result = isset($session[$key]);
+		$message = $session;
+		$this->assertTrue($result, $message);
+
+		$result = $session[$key];
+		$this->assertEqual($expected, $result);
 	}
 
-	protected function checkFreeShippingEligible($expected, $attributes) {
-		$user = $this->fixture('user1', 'User');
+	protected function _checkFreeShippingEligible($expected, $attributes) {
+		$user = $this->_fixture('user1', 'User');
 		$user->save($attributes, array('validate' => false));
-		$this->checkEligible('freeShippingEligible', 'freeshipping', $expected, $user);
+		$this->_checkEligible('freeShippingEligible', 'freeshipping', $expected, $user);
 	}
 
 	public function testFreeShippingEligible() {
-		$this->checkFreeShippingEligible('eligible', array('created_date' => new MongoDate()));
+		$this->_checkFreeShippingEligible('eligible', array('created_date' => new MongoDate()));
 	}
 
 	public function testFreeShippingEligibleUsed() {
-		$this->checkFreeShippingEligible('used', array('created_date' => new MongoDate(), 'purchase_count' => 1));
+		$this->_checkFreeShippingEligible('used', array('created_date' => new MongoDate(), 'purchase_count' => 1));
 	}
 
 	public function testFreeShippingEligibleExpired() {
-		$this->checkFreeShippingEligible('expired', array('created_date' => new MongoDate(time() - 60 * 24 * 60 * 60)));
+		$this->_checkFreeShippingEligible('expired', array('created_date' => new MongoDate(time() - 60 * 24 * 60 * 60)));
 	}
 
-	protected function checkTenOffFiftyEligible($expected, $attributes, $order_attributes = null) {
-		$user = $this->fixture('user1', 'User');
+	protected function _checkTenOffFiftyEligible($expected, $attributes, $order_attributes = null) {
+		$user = $this->_fixture('user1', 'User');
 		$user->save($attributes, array('validate' => false));
 		if ($order_attributes) {
 			$order = Order::create($order_attributes + array('user_id' => $user->_id));
 			$order->save(null, array('validate' => false));
 		}
-		$this->checkEligible('tenOffFiftyEligible', '10off50', $expected, $user);
+		$this->_checkEligible('tenOffFiftyEligible', '10off50', $expected, $user);
 		if ($order_attributes) {
 			$order->delete();
 		}
 	}
 
 	public function testTenOffFiftyEligible() {
-		$this->checkTenOffFiftyEligible(
+		$this->_checkTenOffFiftyEligible(
 			'eligible',
 			array('created_date' => new MongoDate(), 'purchase_count' => 1),
 			array('date_created' => new MongoDate())
@@ -116,11 +143,11 @@ class BaseControllerTest extends \lithium\test\Unit {
 	}
 
 	public function testTenOffFiftyEligibleIneligible() {
-		$this->checkTenOffFiftyEligible('ineligible', array('created_date' => new MongoDate(), 'purchase_count' => 0));
+		$this->_checkTenOffFiftyEligible('ineligible', array('created_date' => new MongoDate(), 'purchase_count' => 0));
 	}
 
 	public function testTenOffFiftyEligibleExpired() {
-		$this->checkTenOffFiftyEligible(
+		$this->_checkTenOffFiftyEligible(
 			'expired',
 			array('created_date' => new MongoDate(time() - 60 * 24 * 60 * 60), 'purchase_count' => 1),
 			array('date_created' => new MongoDate())
@@ -130,8 +157,10 @@ class BaseControllerTest extends \lithium\test\Unit {
 	public function testWriteSession() {
 		$controller = new BaseControllerMock();
 		$controller->writeSession(array('foo' => 'bar'));
+
+		$expected = array('foo' => 'bar');
 		$result = Session::read('userLogin');
-		$this->assertEqual(array('foo' => 'bar'), $result);
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testCleanCC() {
@@ -139,8 +168,10 @@ class BaseControllerTest extends \lithium\test\Unit {
 		$request = new Request(array('params' => array('controller' => 'base', 'action' => 'index')));
 		$controller = new BaseControllerMock(compact('request'));
 		$controller->cleanCC();
+
+		$expected = array();
 		$result = Session::read('cc_infos');
-		$this->assertEqual(array(), $result);
+		$this->assertEqual($expected, $result);
 	}
 
 	public function testCleanCCDoesNotClean() {
@@ -148,8 +179,10 @@ class BaseControllerTest extends \lithium\test\Unit {
 		$request = new Request(array('params' => array('controller' => 'orders', 'action' => 'index')));
 		$controller = new BaseControllerMock(compact('request'));
 		$controller->cleanCC();
+
+		$expected = array('foo' => 'bar');
 		$result = Session::read('cc_infos');
-		$this->assertEqual(array('foo' => 'bar'), $result);
+		$this->assertEqual($expected, $result);
 	}
 }
 
--- app/tests/cases/models/BaseTest.php
+++ app/tests/cases/models/BaseTest.php
@@ -7,29 +7,50 @@ use MongoDate;
 
 class BaseTest extends \lithium\test\Unit {
 	public function testDates() {
-		$result = Base::dates('now');
-		$this->assertTrue($result instanceof MongoDate);
+		$date = Base::dates('now');
+		$result = $date instanceof MongoDate;
+		$this->assertTrue($result);
 	}
 
 	public function testGenerateToken() {
 		$token1 = Base::generateToken();
 		$token2 = Base::generateToken();
-		$this->assertTrue(is_string($token1), $token1);
-		$this->assertEqual(10, strlen($token1));
-		$this->assertNotEqual($token2, $token1);
+
+		$result = is_string($token1);
+		$message = $token1;
+		$this->assertTrue($result, $message);
+
+		$expected = 10;
+		$result = strlen($token1);
+		$this->assertEqual($expected, $result);
+
+		$expected = $token2;
+		$result = $token1;
+		$this->assertNotEqual($expected, $result);
 	}
 
 	public function testRandomString() {
 		$random1 = Base::randomString(8);
 		$random2 = Base::randomString(8);
-		$this->assertTrue(is_string($random1), $random1);
-		$this->assertEqual(8, strlen($random1));
-		$this->assertNotEqual($random2, $random1);
+
+		$result = is_string($random1);
+		$message = $random1;
+		$this->assertTrue($result, $message);
+
+		$expected = 8;
+		$result = strlen($random1);
+		$this->assertEqual($expected, $result);
+
+		$expected = $random2;
+		$result = $random1;
+		$this->assertNotEqual($expected, $result);
 	}
 
 	public function testRandomStringNoRepeatingCharacters() {
 		$random = Base::randomString(4, 'AB');
-		$this->assertTrue(in_array($random, array('ABAB', 'BABA')), $random);
+		$result = in_array($random, array('ABAB', 'BABA'));
+		$message = $random;
+		$this->assertTrue($result, $message);
 	}
 }
 
