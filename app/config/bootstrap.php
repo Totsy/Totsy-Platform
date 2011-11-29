1c185d08e5b7b6d88ea7357b6f2c983f79808f7e
David Persson <davidpersson@gmx.de>
2011-11-29 16:04:49 +0100

    Extract environment detection bootstrap.

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -32,34 +32,14 @@
 require __DIR__ . '/bootstrap/libraries.php';
 
 /**
- * This should go into its own file.
+ * Sets up environment detection.
  */
-
-use lithium\core\Environment;
-
-Environment::is(function($request) {
-	switch ($request->env('HTTP_HOST')) {
-		case 'totsy.com':
-		case 'www.totsy.com':
-		case 'totsystaging.com':
-		case 'www.totsystaging.com':
-		case 'newprod.totsy.com':
-		case '50.56.49.10':
-			return 'production';
-		case 'test.totsy.com':
-			return 'test';
-		case 'dev.totsy.com':
-			return 'development';
-		default:
-			return 'local';
-	}
-});
+require __DIR__ . '/bootstrap/environment.php';
 
 /**
  * Include this file if your application uses a database connection.
  */
 require __DIR__ . '/bootstrap/connections.php';
-
 
 /**
  * Error handling.
---
+++ app/config/bootstrap/environment.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Sets up environment detection.
+ */
+use lithium\core\Environment;
+
+Environment::is(function($request) {
+	switch ($request->env('HTTP_HOST')) {
+		case 'totsy.com':
+		case 'www.totsy.com':
+		case 'totsystaging.com':
+		case 'www.totsystaging.com':
+		case 'newprod.totsy.com':
+		case '50.56.49.10':
+		case 'web1-dc1.totsy.com':
+		case 'web2-dc1.totsy.com':
+		case 'web3-dc1.totsy.com':
+		case 'web4-dc1.totsy.com':
+		case 'web5-dc1.totsy.com':
+		case 'web6-dc1.totsy.com':
+		case 'web7-dc1.totsy.com':
+		case 'web8-dc1.totsy.com':
+		case 'www.totsystaging.com':
+			return 'production';
+		case 'test.totsy.com':
+			return 'test';
+		case 'dev.totsy.com':
+			return 'development';
+		default:
+			return 'local';
+	}
+});
+
+?>
