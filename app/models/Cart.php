57daae9ada0133714a3a77211c8eb8615c20fa95
Mariano Iglesias <mariano@cricava.com>
2011-12-01 16:21:00 -0300

    Fixing issue where Cart::reserved() was returning the wrong value

--- app/models/Cart.php
+++ app/models/Cart.php
@@ -315,7 +315,7 @@ class Cart extends Base {
 		if ($reserved) {
 			$carts = $reserved->data();
 			foreach ($carts as $cart) {
-				$total = $total + isset($cart['quantity']) ? $cart['quantity'] : 0;
+				$total += (isset($cart['quantity']) ? $cart['quantity'] : 0);
 			}
 		}
 		return $total;
