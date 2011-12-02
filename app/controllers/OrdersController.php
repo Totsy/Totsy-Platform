bbbcf2fb873b653165711bbd381980540205c11d
troyer <troyer@totsy.com>
2011-12-02 13:50:27 -0500

    dont apply credits to shipping/tax

--- app/controllers/OrdersController.php
+++ app/controllers/OrdersController.php
@@ -458,7 +458,7 @@ class OrdersController extends BaseController {
 			return $this->redirect(array('Orders::payment'));
 		}
 		#Check if Services
-		$servicesAvailable = false;
+		$serviceAvailable = false;
 		if(Session::check('service_available')) {
 			$serviceAvailable = Session::read('service_available');
 		}
