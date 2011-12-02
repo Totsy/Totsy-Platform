67dbe477ae10091a78caa427bc41f29ff28f8c72
Eric The Robot <erictherobot@gmail.com>
2011-12-02 14:06:47 -0500

    adding new mobile views and startup icons and splash page for mobile devices

---
+++ app/views/elements/mobile_helpNav.html.php
@@ -0,0 +1,15 @@
+<h2>Need Help?</h2>
+<hr />
+<ul data-role="listview" data-inset="true">
+			<li>
+			<a href="#" onclick="window.location.href='/tickets/add';return false;">Support</a></li>
+			<li>
+			<a href="#" onclick="window.location.href='/pages/faq';return false;">FAQ's</a></li>
+			<li>
+			<a href="#" onclick="window.location.href='/pages/privacy';return false;">Privacy Policy</a></li>
+			<li>
+			<a href="#" onclick="window.location.href='/pages/terms';return false;">Terms of Use</a></li>
+
+
+
+</ul>
