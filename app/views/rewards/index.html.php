92a175026fd268f5bdb3f60dde9b3c74a42d6b0e
eubiera <eubiera@totsy.com>
2011-12-02 13:47:52 -0500

    started implementation

--- app/views/rewards/index.html.php
+++ app/views/rewards/index.html.php
@@ -1,3 +1,17 @@
+<?php
+
+print_r($_SESSION);
+
+?>
+
+<script>
+
+
+
+</script>
+
+
+<img src="http://loyalty.500friends.com/api/enroll.gif?uuid=<YOUR ACCOUNT ID>&email=<CUSTOMER EMAIL>" style="position: absolute; left: -10px; visibility: hidden;"/>
 <div class="grid_16">
 	<br />
 	<h1 class="page-title gray">
