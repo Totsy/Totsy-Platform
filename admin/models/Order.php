89d767d92216a57c503d631d4871e136c1d90a34
troyer <troyer@totsy.com>
2011-12-19 10:41:52 -0500

    fix reauth old datas, order decrypt method, through CS

--- admin/models/Order.php
+++ admin/models/Order.php
@@ -915,16 +915,16 @@ class Order extends Base {
 			$cc_encrypt = $order['cc_payment'];
 			$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CFB);
 			$iv =  base64_decode($order['cc_payment']['vi']);
-			$key = md5($order['user_id']);
+			$key = $order['user_id'];
 			unset($cc_encrypt['vi']);
 			foreach	($cc_encrypt as $k => $cc_info) {
-				$crypt_info = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key.sha1($k), base64_decode($cc_info), MCRYPT_MODE_CFB, $iv);
+				$crypt_info = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($key . $k), base64_decode($cc_info), MCRYPT_MODE_CFB, $iv);
 				$creditCard[$k] = $crypt_info;
 			}
 		}
 		return $creditCard; 
 	}
-	
+
 	/**
 	 * Encrypt all credits card informations with MCRYPT and store it in the Session
 	 */
