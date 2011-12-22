d4057c26b079be8fb54dcde2a58de535cdfe2654
David Gowrie <dgowrie@totsty>
2011-12-22 17:43:29 -0500

    stripping out extraneous code (copied from index.html.php)

--- app/views/events/category.html.php
+++ app/views/events/category.html.php
@@ -40,6 +40,7 @@ CATEGORY VIEW
 		XXXXXXXXXXXXXXXXXXX
 		I AM AN EVENT
 		XXXXXXXXXXXXXXXXXXX
+		<?php echo $event['blurb']; ?>
 		</pre>
 	<?php
 		//print_r($event->eventItems);
@@ -49,7 +50,11 @@ CATEGORY VIEW
 			<div class="item" style="float:left; margin-right:7px; width:125px; height:188px; border:1px solid #ccc;">
 		<?php	
 		
+			echo "<!--";
+			echo "<pre>";
 			print_r($item);
+			echo "</pre>";
+			echo "-->";
 
 			echo "<br>";
 			echo "id is " . $item['_id'];
@@ -68,7 +73,7 @@ CATEGORY VIEW
 			
 			
 			//var is just the jpg filename, hardcoded to www.totsy bc dev often doesnt have images
-			echo "http://www.totsy.com/images/" . $item['primary_image'] . ".jpg";
+			echo "http://www.totsy.com/image/" . $item['primary_image'] . ".jpg";
 			echo "<br>";
 			echo "<br>";
 			echo "<br>";
@@ -98,156 +103,68 @@ CATEGORY VIEW
 		<?php if ($y == 3): ?>
 			<?php $y = 1; ?>
 		<?php endif ?>
-		<div class="p-container roundy_product_home">
-					<!-- this is where the items count was -->
-					<?php
-						if (!empty($event->images->splash_big_image)) {
-							$productImage = "/image/{$event->images->splash_big_image}.jpg";
-						} else {
-							$productImage = ($x <= 1) ? "/img/no-image-large.jpeg" : "/img/no-image-small.jpeg";
-						}
-					?>
-					<?php
-						if(empty($departments)) {
-							$url = $event->url;
-						} else {
-							$url = $event->url.'/?filter='.$departments;
-						}
-					?>
-					<?php if ($x <= 1): ?>
-						<?php echo $this->html->link(
-							$this->html->image("$productImage", array(
-							'title' => $event->name,
-							'alt' => $event->name,
-							'width' => '349',
-							'height' => '403',
-					'style' => 'margin:0px 0px -6px 0px;'
-							)), "sale/$url", array('escape'=> false));
-						?>
-					<?php else: ?>
-						<?php echo $this->html->link(
-							$this->html->image("$productImage", array(
-							'title' => $event->name,
-							'alt' => $event->name,
-							'width' => '228',
-							'height' => '266'
-						)), "sale/$url", array('escape'=> false));
-						 ?>
-					<?php endif ?>
-
-			<div class="splash-details">
+				<div class="p-container roundy_product_home">
+							<!-- this is where the items count was -->
+							<?php
+								if (!empty($event->images->splash_big_image)) {
+									$productImage = "/image/{$event->images->splash_big_image}.jpg";
+								} else {
+									$productImage = ($x <= 1) ? "/img/no-image-large.jpeg" : "/img/no-image-small.jpeg";
+								}
+							?>
+							<?php
+								if(empty($departments)) {
+									$url = $event->url;
+								} else {
+									$url = $event->url.'/?filter='.$departments;
+								}
+							?>
+							<?php if ($x <= 1): ?>
+								<?php echo $this->html->link(
+									$this->html->image("$productImage", array(
+									'title' => $event->name,
+									'alt' => $event->name,
+									'width' => '349',
+									'height' => '403',
+							'style' => 'margin:0px 0px -6px 0px;'
+									)), "sale/$url", array('escape'=> false));
+								?>
+							<?php else: ?>
+								<?php echo $this->html->link(
+									$this->html->image("$productImage", array(
+									'title' => $event->name,
+									'alt' => $event->name,
+									'width' => '228',
+									'height' => '266'
+								)), "sale/$url", array('escape'=> false));
+								 ?>
+							<?php endif ?>
+				
+					<div class="event-details">
 						<div class="table-cell left" style="display:block; padding:5px 5px 5px 10px;">
-						 <p style="padding:0px; margin:0px; font-size:15px; color:#fff; font-weight:normal; text-transform:none;"> <?php echo $event->name; ?></p>
-						 <p style="padding:0px; margin:-3px 0px 0px 0px; font-size:12px; color:#c7c7c7; font-weight:normal; font-style:italic; text-transform:none;"><span id="<?php echo "todaysplash$x"; ?>" title="<?php echo $date = $event->end_date->sec * 1000; ?>" class="counter end"></span>
+							 <p style="padding:0px; margin:0px; font-size:15px; color:#fff; font-weight:normal; text-transform:none;"> 
+							 	======== EVENT NAME ========
+							 <?php echo $event->name; ?></p>
+							 <p style="padding:0px; margin:-3px 0px 0px 0px; font-size:12px; color:#c7c7c7; font-weight:normal; font-style:italic; text-transform:none;"><span id="<?php echo "todaysplash$x"; ?>" title="<?php echo $date = $event->end_date->sec * 1000; ?>" class="counter end"></span>
 						</div>
-
+				
 						<div class="table-cell right">
 							<?php echo $this->html->link('Shop', 'sale/'.$event->url, array('class' => 'button small', 'style'=>'display:table-cell !important'));?>
 						</div>
-					</div>
-				</div>
-			</div>
+					</div><!-- /.event-details -->
+				</div><!-- /.p-container -->
+			</div><!-- /.gridX -->
 
-			<!-- @DG - no banners on the category landing page...
-			<?php if ($x == 1): ?>
-				<div id="banner_container" class="grid_5" style="height:405px; overflow:hidden;">
-					<div><a href="/users/invite"><img src="/img/invite_girl.png"></a></div>
-					<?php if(!empty($banner["img"])): ?>
-						<?php foreach($banner["img"] as $image): ?>
-							<div><?php if(!empty($image["url"])):?>
-								<a href="<?php echo $image["url"]?>"
-									<?php
-										if(array_key_exists('newPage', $image) && $image['newPage']) {
-											echo 'target="_blank"';
-										}
-									?>
-									>
-									<img src="/image/<?php echo $image["_id"]?>.jpeg" alt="" />
-								</a>
-								<?php else: ?>
-									<img src="/image/<?php echo $image["_id"]?>.jpeg" alt="" />
-								<?php endif ?>
-							</div>
-						<?php endforeach ?>
-					<?php endif ?>
-				</div>
-				<div class="clear"></div>
-			<?php endif ?>
-			-->
 		<?php $x++; ?>
 		<?php $y++; ?>
 	<?php endforeach ?>
 
 	<div style="margin-bottom:35px;" class="clear"></div>
 
-	<div class="container_16">
-	<div class="grid_16">
-		<h2 class="page-title gray">Upcoming Sales</h2>
-		<hr />
-	</div>
-		<?php $x = 0; ?>
-		<?php $y = 0; ?>
-		<?php foreach ($pendingEvents as $event): ?>
-			<?php if (($y == 0) || ($y == 2)): ?>
-				<div class="grid_4">
-			<?php endif ?>
-			<?php if ($y == 1): ?>
-				<div class="grid_4">
-			<?php endif ?>
-			<?php if ($y == 2): ?>
-				<?php $y = -1; ?>
-			<?php endif ?>
-					<div class="p-container roundy_product_home">
-						<?php
-							if (!empty($event->images->splash_small_image)) {
-								$productImage = "/image/{$event->images->splash_small_image}.jpg";
-							} else {
-								$productImage = "/img/no-image-small.jpeg";
-							}
-						?>
-						<?php echo $this->html->link(
-						$this->html->image("$productImage", array(
-							'title' => $event->name,
-							'alt' => $event->name,
-							'width' => '228',
-							'height' => '266'
-						)), 'sale/'.$event->url, array('escape'=> false));
-						 ?>
-				<div class="splash-details">
-						<div class="table-cell left" style="display:block; padding:5px 5px 5px 10px;">
-						 <p style="padding:0px; margin:0px; font-size:15px; color:#fff; font-weight:normal; text-transform:none;"> <?php echo $event->name; ?></p>
-						 <p style="padding:0px; margin:-3px 0px 0px 0px; font-size:12px; color:#c7c7c7; font-weight:normal; font-style:italic; text-transform:none;">
-						 <span id="<?php echo "futuresplash$x"; ?>" title="<?php echo $date = $event->start_date->sec * 1000; ?>" class="counter start"></span>
-						</div>
-
-						<div class="table-cell right">
-							<?php echo $this->html->link('View', 'sale/'.$event->url, array('class' => 'button small', 'style'=>'display:table-cell !important'));?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<?php $x++; ?>
-			<?php $y++; ?>
-	<?php endforeach ?>
-	</div>
-</div>
-</div>
-</div>
+</div><!-- a -->
+</div><!-- b -->
+</div><!-- c -->
 <div id="modal" style="background:#fff!important;"></div>
-<!--Javascript Output for Today's Events -->
-<?php if (!empty($todayJs)): ?>
-	<?php foreach ($todayJs as $value): ?>
-		<?php //echo $value ?>
-	<?php endforeach ?>
-<?php endif ?>
-
-<!--Javascript Output for Future Events-->
-<?php if (!empty($futureJs)): ?>
-	<?php foreach ($futureJs as $value): ?>
-		<?php //echo $value ?>
-	<?php endforeach ?>
-<?php endif ?>
 
 <script type="text/javascript">
 //<!--
