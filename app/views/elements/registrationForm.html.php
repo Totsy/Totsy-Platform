9ba32260f747d0d825c9dac911c474dc89b60f6c
Eric The Robot <erictherobot@gmail.com>
2011-12-26 11:13:08 -0500

    adding a hotfix for shorttags

--- app/views/elements/registrationForm.html.php
+++ app/views/elements/registrationForm.html.php
@@ -26,7 +26,7 @@
 	<div style="float:right">
 	<?php echo $this->form->text('email', array('class' => 'inputbox', 'style' => 'width:190px')); ?>
 	</div>
-	<?=$this->form->error('email'); ?>
+	<?php echo $this->form->error('email'); ?>
 
 	<div class="clear"></div>
 	<div style="width:70px; float:left">
@@ -40,8 +40,8 @@
 	<div style="float:right">
 		<?php echo $this->form->text('confirmemail', array('class' => 'inputbox', 'style' => 'width:190px')); ?>
 	</div>
-	<?=$this->form->error('confirmemail'); ?>
-	<?=$this->form->error('emailcheck'); ?>
+	<?php echo $this->form->error('confirmemail'); ?>
+	<?php echo $this->form->error('emailcheck'); ?>
 
 	<div class="clear"></div>
 
@@ -78,7 +78,7 @@
 
 	<input class="button fr" type="button" value="Join Now" onclick="return setIframe();" style="width:100px; height 28px; font-weight:important;">
 
-	<?=$this->form->error('terms'); ?>
+	<?php echo $this->form->error('terms'); ?>
 
 
 <?php echo $this->form->end(); ?>
