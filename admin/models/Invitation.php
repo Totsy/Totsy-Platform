28fee6a3da109abcc527263c0a52d0582bf84caa
Slavik <skosh@totsy.com>
2012-01-12 14:06:32 -0500

    fix invitation model ns

--- admin/models/Invitation.php
+++ admin/models/Invitation.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace admin\models;
+use admin\models\Base;
 
-class Invitation extends admin\models\Base {
+class Invitation extends Base {
 
 	protected $_meta = array('source' => 'invitations');
 	
