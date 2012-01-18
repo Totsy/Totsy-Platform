742eb3d83f1033e02b9d9183b497ec8da753a80f
David Gowrie <dgowrie@totsy.com>
2012-01-18 16:21:39 -0500

    Jeremy made requisite changes to get the status pages in the correct directories and ready to go

--- app/views/_error/500.html.php
+++ app/views/_error/500.html.php
@@ -2,23 +2,23 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Site Down For Maintenance - Totsy [503 Service Unavailable]</title>
-	<link rel="stylesheet" type="text/css" href="/css/base.css" />
-	<link rel="stylesheet" type="text/css" href="/css/960.css" />
+	<title>Woopsies - Totsy [500 Internal Server Error]</title>
+	<?php echo '<link rel="stylesheet" type="text/css" href="/css/base.css?' . filemtime(LITHIUM_APP_PATH . '/webroot/css/base.css') . '" />'; ?>
+	<?php echo '<link rel="stylesheet" type="text/css" href="/css/960.css?' . filemtime(LITHIUM_APP_PATH . '/webroot/css/960.css') . '" />'; ?>
 	<link href="/img/favicon.ico" title="Icon" type="image/x-icon" rel="icon" />
 	<link href="/img/favicon.ico" title="Icon" type="image/x-icon" rel="shortcut icon" />
 </head>
 
-<body class="app p-status p-status-503">
+<body class="app p-status p-status-500">
 
 	<div id="totsy" class="container_16 roundy glow">
-		<header class="group">
-			<h1><em>Totsy - </em>We're taking a scheduled time-out.</h1>
+		<header>
+			<h1><em>Totsy - </em>Our best people are on this!</h1>
 		</header>
 		<section>
-			<p>Totsy is down for some quick maintenance to improve your shopping experience. </p>
-			<p>We apologize for any inconvenience and will be back soon.</p>
-			<p>In the meantime, feel free to contact <a href="mailto:support@totsy.com" title="Email Totsy Support">support@totsy.com</a> if you'd like more information, or if you just miss us.</p>
+			<p>Unfortunately, our youngster got a little rambunctious and needed a time-out, so the site is down at the moment. <br/>(Growing pains are never easy, are they?)</p>
+			<p>We apologize for any inconvenience you may be experiencing. Please don't fret. We'll be back as quick as we can. We promise!</p>
+			<p>In the meantime, feel free to contact <a href="mailto:support@totsy.com" title="Email Totsy Support">support@totsy.com</a> if you'd like more information, or if you just miss us.</p>
 		</section>
 	
 	</div>
