43d2172dfe9f7587c3b5b9dcc9f289bf1af34da2
jonathan widro <jwidro@totsy.com>
2012-02-06 13:44:08 -0500

    html frame for splash

---
+++ app/views/events/splash.html.php
@@ -0,0 +1,18 @@
+<h1>JOJO MAMAN BEBE SPLASH TEST FRAMEWORK</h1>
+
+
+<table>
+
+<tr>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+</tr>
+<tr>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+<td width=300 height=200 style="background:#ff0000; border:1px solid #000000; color;#ffff00">YES</td>
+</tr>
+
+
+</table>
