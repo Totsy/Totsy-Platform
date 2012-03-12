9d83fcf8a0a7e32b745353bdca4156866c3d7080
Evan Ubiera <eubiera@totsy.com>
2012-03-12 12:23:38 -0400

    writing P3P headers for IE3

--- app/webroot/totsyfbtab/totsy_signuptab.php
+++ app/webroot/totsyfbtab/totsy_signuptab.php
@@ -1,5 +1,6 @@
 <?php 
 header("p3p: CP=\"ALL DSP COR PSAa PSDa OUR NOR ONL UNI COM NAV\"");
+//header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" 
