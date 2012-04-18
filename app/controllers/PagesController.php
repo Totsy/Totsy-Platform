53e30f347bd96d39466ae9fa2464e37b720f71cd
jonathan widro <jwidro@totsy.com>
2012-04-18 15:54:51 -0400

    earth day

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -28,12 +28,16 @@ class PagesController extends BaseController {
 		} elseif ($path[0] == 'btrendie' || $path[0] == "living_social"){
 			$this->_render['layout'] = 'blank';
 		}
-		$allowed = array('terms', 'faq', 'contact', 'privacy', 'aboutus', 'btrendie', 'moms', 'testimonials', 'being_green', 'press','affiliates','living_social', 'careers');
+		$allowed = array('earthday', 'terms', 'faq', 'contact', 'privacy', 'aboutus', 'btrendie', 'moms', 'testimonials', 'being_green', 'press','affiliates','living_social', 'careers');
 				
 		$userCheck = Session::read('userLogin');
 						
 		if (empty($userCheck) && !in_array($path[0], $allowed)) {
 			$this->redirect('/');
+		}
+				
+		if($path[0] == "earthday"){
+//		 	$this->_render['layout'] = 'earthday';
 		}
 				
 		if (in_array($path[0], $allowed) && $path[0] == "living_social") {
