d89dab060b0cf792b5530513618c1a32ad8cd579
Eric The Robot <erictherobot@gmail.com>
2012-04-30 10:01:20 -0400

    adding invite friends banner back.

--- app/views/events/index.html.php
+++ app/views/events/index.html.php
@@ -187,8 +187,7 @@
 			</div>
 	
 				<?php if ($x == 1): ?>
-					<div id="banner_container" class="grid_5" style="height:405px; overflow:hidden;">
-		<img src="/img/april_hp_Banner_v2.jpg">
+					<div id="banner_container" class="grid_5" style="height:405px; overflow:hidden;"><a href="/users/invite"><img src="/img/invite-friends-envelope-2.gif"></a>
 					</div>
 					
 <ul class="echoShare" data-productname='<?php echo $event->name ?>' data-productcap="" data-productdesc='<?php echo htmlspecialchars_decode(html_entity_decode(strip_tags($event->name))) ?>' data-imageclass="" data-producturl='<?php echo $_SERVER['HTTP_HOST']. '/sale/'.$event->url ?>'></ul> 
